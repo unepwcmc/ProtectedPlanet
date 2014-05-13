4c59c5065aab2d18c96f7b637a83ad96acd808c4
Miguel Torres <migtorres@gmail.com>
2014-05-13 14:30:34 +0100

    Simplify importer test

--- test/unit/carto_db_importer_test.rb
+++ test/unit/carto_db_importer_test.rb
@@ -5,20 +5,17 @@ class TestCartoDbImporter < ActiveSupport::TestCase
     file_mock = mock()
     File.expects(:open).with('an_file.zip', 'r').returns(file_mock)
 
-    response_mock = mock()
-    response_mock.expects(:body).returns("{\"item_queue_id\":\"12345\"}")
-    response_mock.expects(:response_code).returns(200)
+    response = Typhoeus::Response.new(code: 200, body: "{\"item_queue_id\":\"12345\"}")
     Typhoeus
       .expects(:post)
       .with("https://carbon-tool.cartodb.com/api/v1/imports/", params: {api_key: '1234'}, body: {file: file_mock})
-      .returns(response_mock)
+      .returns(response)
 
-    response_mock = mock()
-    response_mock.expects(:body).returns("{\"state\":\"complete\"}")
+    response = Typhoeus::Response.new(code: 200, body: "{\"state\":\"complete\"}")
     Typhoeus
       .expects(:get)
       .with("https://carbon-tool.cartodb.com/api/v1/imports/12345", params: {api_key: '1234'})
-      .returns(response_mock)
+      .returns(response)
 
     cartodb_importer = CartoDbImporter.new username: 'carbon-tool', api_key: '1234'
     response = cartodb_importer.import 'an_file.zip'
@@ -30,20 +27,17 @@ class TestCartoDbImporter < ActiveSupport::TestCase
     file_mock = mock()
     File.expects(:open).with('an_file.zip', 'r').returns(file_mock)
 
-    response_mock = mock()
-    response_mock.expects(:body).returns("{\"item_queue_id\":\"12345\"}")
-    response_mock.expects(:response_code).returns(200)
+    response = Typhoeus::Response.new(code: 200, body: "{\"item_queue_id\":\"12345\"}")
     Typhoeus
       .expects(:post)
       .with("https://carbon-tool.cartodb.com/api/v1/imports/", params: {api_key: '1234'}, body: {file: file_mock})
-      .returns(response_mock)
+      .returns(response)
 
-    response_mock = mock()
-    response_mock.expects(:body).returns("{\"state\":\"failure\"}")
+    response = Typhoeus::Response.new(code: 200, body: "{\"state\":\"failure\"}")
     Typhoeus
       .expects(:get)
       .with("https://carbon-tool.cartodb.com/api/v1/imports/12345", params: {api_key: '1234'})
-      .returns(response_mock)
+      .returns(response)
 
     cartodb_importer = CartoDbImporter.new username: 'carbon-tool', api_key: '1234'
     response = cartodb_importer.import 'an_file.zip'
@@ -55,12 +49,11 @@ class TestCartoDbImporter < ActiveSupport::TestCase
     file_mock = mock()
     File.expects(:open).with('an_file.zip', 'r').returns(file_mock)
 
-    response_mock = mock()
-    response_mock.expects(:response_code).returns(500)
+    response = Typhoeus::Response.new(code: 500)
     Typhoeus
       .expects(:post)
       .with("https://carbon-tool.cartodb.com/api/v1/imports/", params: {api_key: '1234'}, body: {file: file_mock})
-      .returns(response_mock)
+      .returns(response)
 
     cartodb_importer = CartoDbImporter.new username: 'carbon-tool', api_key: '1234'
     response = cartodb_importer.import 'an_file.zip'
