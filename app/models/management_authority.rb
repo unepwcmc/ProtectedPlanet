28a18c7d01e7d002468eb91cd394fb3ffa3eb37a
Adam Mulligan <adammulligan@gmail.com>
2014-05-27 16:29:54 +0100

    Adds ManagementAuthority model for Protected Areas

---
+++ app/models/management_authority.rb
@@ -0,0 +1,2 @@
+class ManagementAuthority < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140527151457) do
+ActiveRecord::Schema.define(version: 20140527152932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20140527151457) do
   end
 
   create_table "legal_statuses", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "management_authorities", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
