368b3588861befb45c5459f1e66df711c3aba454
Adam Mulligan <adammulligan@gmail.com>
2014-06-01 20:16:46 +0100

    Adds DB schema entry for postgis adapter

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529072242) do
+ActiveRecord::Schema.define(version: 20140601191435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "countries", force: true do |t|
     t.text     "name"
