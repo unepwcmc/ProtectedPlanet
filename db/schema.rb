0cb0c53df920a03598674dfa8965ad05be6c028a
Adam Mulligan <adammulligan@gmail.com>
2014-06-01 22:09:05 +0100

    Adds international criteria attribute to protected area

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601210739) do
+ActiveRecord::Schema.define(version: 20140601210855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20140601210739) do
     t.integer  "governance_id"
     t.text     "management_plan"
     t.integer  "management_authority_id"
+    t.string   "international_criteria"
   end
 
   add_index "protected_areas", ["wdpa_id"], :name => "index_protected_areas_on_wdpa_id", :unique => true
