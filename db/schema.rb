fec990373124577f1817a1ec7755224a8bf3087d
Adam Mulligan <adammulligan@gmail.com>
2014-06-02 10:28:49 +0100

    Removes foolish requirement that WDPA parent IDs be unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601211314) do
+ActiveRecord::Schema.define(version: 20140602092716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,7 +104,7 @@ ActiveRecord::Schema.define(version: 20140601211314) do
   end
 
   add_index "protected_areas", ["wdpa_id"], :name => "index_protected_areas_on_wdpa_id", :unique => true
-  add_index "protected_areas", ["wdpa_parent_id"], :name => "index_protected_areas_on_wdpa_parent_id", :unique => true
+  add_index "protected_areas", ["wdpa_parent_id"], :name => "index_protected_areas_on_wdpa_parent_id"
 
   create_table "protected_areas_sub_locations", id: false, force: true do |t|
     t.integer "protected_area_id"
