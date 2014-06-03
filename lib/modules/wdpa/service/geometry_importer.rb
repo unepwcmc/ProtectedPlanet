0d9606bb96e841f7d50a12d5b90b59925298e84c
Adam Mulligan <adammulligan@gmail.com>
2014-06-03 13:59:34 +0100

    Adds query error handling to geometry importer

--- lib/modules/wdpa/service/geometry_importer.rb
+++ lib/modules/wdpa/service/geometry_importer.rb
@@ -1,4 +1,6 @@
 class Wdpa::Service::GeometryImporter
+  DB = ActiveRecord::Base.connection
+
   def self.import protected_areas
     protected_areas.each do |protected_area_attributes|
       standard_attributes = Wdpa::DataStandard.attributes_from_standards_hash({
@@ -9,12 +11,15 @@ class Wdpa::Service::GeometryImporter
       wdpa_id = standard_attributes[:wdpa_id]
       geometry = standard_attributes[:the_geom]
 
-      db = ActiveRecord::Base.connection
-      db.execute("""
-        UPDATE protected_areas
-        SET the_geom = ST_GeomFromText(E'#{geometry}')
-        WHERE wdpa_id = #{wdpa_id}
-      """)
+      begin
+        DB.execute("""
+          UPDATE protected_areas
+          SET the_geom = ST_GeomFromText(E'#{geometry}')
+          WHERE wdpa_id = #{wdpa_id}
+        """)
+      rescue
+        return false
+      end
     end
 
     true
--- test/unit/wdpa/service/geometry_importer_test.rb
+++ test/unit/wdpa/service/geometry_importer_test.rb
@@ -43,7 +43,20 @@ class TestWdpaGeometryImporterService < ActiveSupport::TestCase
   end
 
   test '.import returns false if any update query fails' do
-    skip
+    FactoryGirl.create(:protected_area, wdpa_id: 988)
+    protected_area_attributes = [{
+      wdpaid: 987,
+      wkb_geometry: "\x00\x00\x00\x00\x01?\xF0\x00\x00\x00\x00\x00\x00?\xF0\x00\x00\x00\x00\x00\x00", # POINT(1, 1)
+    }]
+
+    ActiveRecord::Base.connection.
+      expects(:execute).
+      raises(ActiveRecord::StatementInvalid).
+      once
+
+    import_successful = Wdpa::Service::GeometryImporter.import(protected_area_attributes)
+
+    refute import_successful, "Expected import to fail"
   end
 
   test '.import returns false if the protected area to update does not exist' do
