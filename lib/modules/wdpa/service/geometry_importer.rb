fd9969aa7378fc7db568e596b95a3d36d2891798
Adam Mulligan <adammulligan@gmail.com>
2014-06-04 12:16:28 +0100

    Significant improvements to speed of geometry importer
    Uses the magic of SQL to copy geometries directly in postgres, rather
    than passing them through various layers of ruby.

--- lib/modules/wdpa/service/geometry_importer.rb
+++ lib/modules/wdpa/service/geometry_importer.rb
@@ -1,42 +1,18 @@
 class Wdpa::Service::GeometryImporter
   DB = ActiveRecord::Base.connection
 
-  def self.import protected_areas
-    protected_areas.each do |protected_area_attributes|
-      next if protected_area_exists protected_area_attributes[:wdpaid]
-
-      standard_attributes = Wdpa::DataStandard.attributes_from_standards_hash({
-        wdpaid: protected_area_attributes[:wdpaid],
-        wkb_geometry: protected_area_attributes[:wkb_geometry]
-      })
-
-      wdpa_id = standard_attributes[:wdpa_id]
-      geometry = standard_attributes[:the_geom]
-
-      begin
-        DB.execute("""
-          UPDATE protected_areas
-          SET the_geom = ST_GeomFromText(E'#{geometry}')
-          WHERE wdpa_id = #{wdpa_id}
-        """)
-      rescue
-        return false
-      end
+  def self.import table_name
+    begin
+      DB.execute("""
+        UPDATE protected_areas pa
+        SET the_geom = import.wkb_geometry
+        FROM #{table_name} import
+        WHERE pa.wdpa_id = import.wdpaid;
+      """)
+    rescue
+      return false
     end
 
     true
-  end
-
-  private
-
-  def self.protected_area_exists wdpa_id
-    existing_protected_area = ProtectedArea.
-      select('wdpa_id').
-      where(
-        wdpa_id: wdpa_id,
-        the_geom: nil
-      ).first
-
-    existing_protected_area.nil?
   end
 end
--- test/unit/wdpa/service/geometry_importer_test.rb
+++ test/unit/wdpa/service/geometry_importer_test.rb
@@ -1,99 +1,30 @@
 require 'test_helper'
 
 class TestWdpaGeometryImporterService < ActiveSupport::TestCase
-  test '.import imports the given geometries directly in to postgis if
-   the PA exists' do
-    protected_area = FactoryGirl.create(:protected_area, wdpa_id: 987)
-    protected_area_attributes = [{
-      wdpaid: 987,
-      wkb_geometry: "\x00\x00\x00\x00\x01?\xF0\x00\x00\x00\x00\x00\x00?\xF0\x00\x00\x00\x00\x00\x00" # POINT(1, 1)
-    }]
-
-    import_successul = Wdpa::Service::GeometryImporter.import(protected_area_attributes)
-
-    assert import_successul, "Expected the Protected Area to be imported successfully"
-
-    pa_has_valid_geometry = ProtectedArea.
-      select('ST_IsValid(the_geom) as geom_valid').
-      where(wdpa_id: protected_area.wdpa_id).
-      first.geom_valid
-
-    assert pa_has_valid_geometry, "Expected the Protected Area to have a valid geometry"
-  end
-
-  test '.import ignores all attributes except the_geom' do
-    protected_area = FactoryGirl.create(:protected_area, wdpa_id: 988)
-    protected_area_attributes = [{
-      wdpaid: protected_area.wdpa_id,
-      wkb_geometry: "\x00\x00\x00\x00\x01?\xF0\x00\x00\x00\x00\x00\x00?\xF0\x00\x00\x00\x00\x00\x00", # POINT(1, 1)
-      orig_name: 'Small PA'
-    }]
-
-    Wdpa::DataStandard.
-      expects(:attributes_from_standards_hash).
-      with(protected_area_attributes.first.except(:orig_name)).
-      returns({wdpa_id: 988, the_geom: 'POINT (1.0, 1.0)'}).
-      once
+  test '.import updates all the PA geometries with the geometries in the import table' do
+    table_name = 'wdpa_poly_apr2014'
 
     ActiveRecord::Base.connection.
       expects(:execute).
-      once
+      with("""
+        UPDATE protected_areas pa
+        SET the_geom = import.wkb_geometry
+        FROM #{table_name} import
+        WHERE pa.wdpa_id = import.wdpaid;
+      """).once
 
-    Wdpa::Service::GeometryImporter.import(protected_area_attributes)
+    import_successful = Wdpa::Service::GeometryImporter.import table_name
+    assert import_successful, "Expected the geometry import to be successful"
   end
 
   test '.import returns false if any update query fails' do
-    protected_area = FactoryGirl.create(:protected_area, wdpa_id: 988)
-    protected_area_attributes = [{
-      wdpaid: protected_area.wdpa_id,
-      wkb_geometry: "\x00\x00\x00\x00\x01?\xF0\x00\x00\x00\x00\x00\x00?\xF0\x00\x00\x00\x00\x00\x00", # POINT(1, 1)
-    }]
-
     ActiveRecord::Base.connection.
       expects(:execute).
       raises(ActiveRecord::StatementInvalid).
       once
 
-    import_successful = Wdpa::Service::GeometryImporter.import(protected_area_attributes)
+    import_successful = Wdpa::Service::GeometryImporter.import 'table_name'
 
     refute import_successful, "Expected import to fail"
-  end
-
-  test '.import ignores protected area that do not exist' do
-    protected_area_attributes = [{
-      wdpaid: 100
-    }]
-
-    ActiveRecord::Base.connection.
-      expects(:execute).never
-
-    Wdpa::Service::GeometryImporter.import(protected_area_attributes)
-  end
-
-  test '.import ignores PAs with geometries' do
-    old_geometry = "\x00\x00\x00\x00\x01?\xF0\x00\x00\x00\x00\x00\x00?\xF0\x00\x00\x00\x00\x00\x00" # POINT(1, 1)
-    new_geometry = "\x00\x00\x00\x00\x01?\xF0\x00\x00\x00\x00\x00\x00@\x00\x00\x00\x00\x00\x00\x00" # POINT(1, 2)
-
-    protected_area = FactoryGirl.create(:protected_area, wdpa_id: 987, the_geom: old_geometry)
-    protected_area_attributes = [{
-      wdpaid: 987,
-      wkb_geometry: new_geometry
-    }]
-
-    Wdpa::Service::GeometryImporter.import(protected_area_attributes)
-
-    protected_area.reload
-    assert_equal "POINT (1.0 1.0)", protected_area.the_geom.to_s
-  end
-
-  test '.import does not standardise the PA attributes before checking for the PA existence' do
-    protected_area_attributes = [{
-      wdpaid: 100,
-      wkb_geometry: "\x00\x00\x00\x00\x01?\xF0\x00\x00\x00\x00\x00\x00?\xF0\x00\x00\x00\x00\x00\x00", # POINT(1, 1)
-    }]
-
-    Wdpa::DataStandard.expects(:attributes_from_standards_hash).never
-
-    Wdpa::Service::GeometryImporter.import(protected_area_attributes)
   end
 end
