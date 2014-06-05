cdee8b667322804c630c0f7f86310f6de5450cf9
Adam Mulligan <adammulligan@gmail.com>
2014-06-05 08:18:01 +0100

    Once again removes geometry from data standard
    The data standard shouldn't handle the geometry, it should be handled
    directly through SQL.

--- test/unit/wdpa/data_standard_test.rb
+++ test/unit/wdpa/data_standard_test.rb
@@ -264,17 +264,6 @@ class TestWdpaDataStandard < ActiveSupport::TestCase
     assert_equal   designation_type, attributes[:designation].jurisdiction.name
   end
 
-  test '.attributes_from_standards_hash parses geometries to WKT' do
-    wkb_geom = "\x00\x00\x00\x00\x01?\xF0\x00\x00\x00\x00\x00\x00?\xF0\x00\x00\x00\x00\x00\x00"
-    wkt_geom = "POINT (1.0 1.0)"
-    attributes = Wdpa::DataStandard.attributes_from_standards_hash({wkb_geometry: wkb_geom})
-
-    the_geom = attributes[:the_geom]
-
-    assert_not_nil the_geom, "Expected the_geom to be returned"
-    assert_equal wkt_geom, the_geom
-  end
-
   test '.attributes_from_standards_hash ignores attributes not in the
    WDPA Standard' do
     attributes = Wdpa::DataStandard.attributes_from_standards_hash({awesomeness: 'Very Awesome'})
