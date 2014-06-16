56e7683ccdf569dc0e21294ded4974e23b98aee3
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-06-16 18:53:20 +0100

    add bounds calculation and showing to pa show page

--- app/models/protected_area.rb
+++ app/models/protected_area.rb
@@ -8,4 +8,15 @@ class ProtectedArea < ActiveRecord::Base
   belongs_to :management_authority
   belongs_to :no_take_status
   belongs_to :designation
+
+  def bounds
+    rgeo_factory = RGeo::Geos.factory srid: 4326
+    bounding_box = RGeo::Cartesian::BoundingBox.new rgeo_factory
+    bounding_box.add the_geom
+
+    [
+      [bounding_box.min_y, bounding_box.min_x],
+      [bounding_box.max_y, bounding_box.max_x]
+    ]
+  end
 end
