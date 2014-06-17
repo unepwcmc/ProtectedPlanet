6662896b9fd7d55a941dd71a043b653aa08f38f2
Adam Mulligan <adammulligan@gmail.com>
2014-06-17 14:44:37 +0100

    Removes unnecessary variable assignment in tests

--- test/integration/protected_area_show_test.rb
+++ test/integration/protected_area_show_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class ProtectedAreaShowTest < ActionDispatch::IntegrationTest
 
   test 'renders the Protected Area name' do
-    protected_area = FactoryGirl.create(:protected_area, name: 'Killbear', slug: 'killbear')
+    FactoryGirl.create(:protected_area, name: 'Killbear', slug: 'killbear')
 
     get '/killbear'
     assert_match /Killbear/, @response.body
   end
 
   test 'renders the Protected Area name given a WDPA ID' do
-    protected_area = FactoryGirl.create(:protected_area, wdpa_id: 1234, name: 'Killbear')
+    FactoryGirl.create(:protected_area, wdpa_id: 1234, name: 'Killbear')
 
     get '/1234'
     assert_match /Killbear/, @response.body
