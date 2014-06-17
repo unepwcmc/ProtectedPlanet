6d20ff622b8a7c7bdf4f1fb4ed589bf0901704c2
Adam Mulligan <adammulligan@gmail.com>
2014-06-17 18:29:01 +0100

    Panoramio photos are now rendered on the PA show page

--- test/integration/protected_area_show_test.rb
+++ test/integration/protected_area_show_test.rb
@@ -15,4 +15,19 @@ class ProtectedAreaShowTest < ActionDispatch::IntegrationTest
     get '/1234'
     assert_match /Killbear/, @response.body
   end
+
+  test 'renders the Images for the Protected Area' do
+    image = FactoryGirl.create(:image, url: 'http://images.com/image.jpg')
+    FactoryGirl.create(:protected_area, wdpa_id: 1234, images: [image])
+
+    get '/1234'
+
+    assert_select 'ul.protected_area_photos' do
+      assert_select 'li', 1 do |elements|
+        elements.each do |element|
+          assert_select element, 'img'
+        end
+      end
+    end
+  end
 end
