17b2f79ea8423abb8282865f6a7ea277f2d10a6b
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-06-17 19:56:35 +0100

    construction and saving of wikipedia url in worker

--- app/workers/wikipedia_summary_worker.rb
+++ app/workers/wikipedia_summary_worker.rb
@@ -1,13 +1,18 @@
 class WikipediaSummaryWorker
   include Sidekiq::Worker
+  sidekiq_options :retry => false
 
   def perform protected_area_id
     @protected_area = ProtectedArea.find protected_area_id
 
     articles = WikipediaArticle.search search_term
 
+    return false if articles.empty?
+
     wikipedia_article = articles.first
     wikipedia_article.summary = Wikipedia::Summary.fetch wikipedia_article
+    wikipedia_article.url = Wikipedia::URL.build wikipedia_article
+
     wikipedia_article.save!
 
     @protected_area.wikipedia_article = wikipedia_article
@@ -19,7 +24,7 @@ class WikipediaSummaryWorker
   def search_term
     [
       @protected_area.name,
-      @protected_area.designation.name
+      @protected_area.designation.try(:name)
     ].join ' '
   end
 end
