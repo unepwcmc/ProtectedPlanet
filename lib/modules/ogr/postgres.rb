9e278f616bdc5357d1bcc8898a188f2a9b20b837
Adam Mulligan <adammulligan@gmail.com>
2014-06-18 18:54:44 +0100

    Refactors OGR Postgres wrapper to allow for renaming tables on import

--- lib/modules/ogr/postgres.rb
+++ lib/modules/ogr/postgres.rb
@@ -1,16 +1,22 @@
 class Ogr::Postgres
+  WRONG_ARGUMENTS_MSG = 'Given new table name, but no original table name'
   DB_CONFIG = Rails.configuration.database_configuration[Rails.env]
 
-  def self.import file: , to: DB_CONFIG["database"]
+  def self.import file_path, original_table_name=nil, table_name=nil
+    if table_name && !original_table_name
+      raise ArgumentError, WRONG_ARGUMENTS_MSG
+    end
+
     ogr = self.new
-    ogr.import file: file, to: to
+    ogr.import file_path, original_table_name, table_name
 
     ogr
   end
 
-  def import(file: , to:)
-    @file_path = file
-    @database_name = to
+  def import file_path, original_table_name=nil, table_name=nil
+    @file_path = file_path
+    @original_table_name = original_table_name
+    @table_name = table_name
 
     system(ogr_command)
   end
--- test/unit/ogr/postgres_test.rb
+++ test/unit/ogr/postgres_test.rb
@@ -4,21 +4,55 @@ class TestOgrPostgres < ActiveSupport::TestCase
   test '.import runs the correct ogr2ogr command to import a geo
    database to postgres' do
     db_config = Rails.configuration.database_configuration[Rails.env]
-    db_name   = "my_little_database"
-
-    ogr_command = "ogr2ogr -overwrite -skipfailures -lco ENCODING=UTF-8 -f \"PostgreSQL\" PG:\" host=#{db_config["host"]} user=#{db_config["username"]} dbname=#{db_name}\" ./an/file"
-    Ogr::Postgres.any_instance.expects(:system).with(ogr_command).once
-
-    Ogr::Postgres.import file: './an/file', to: db_name
-  end
-
-  test '.import defaults to the database in the Rails config if no
-   database is given' do
-    db_config = Rails.configuration.database_configuration[Rails.env]
 
     ogr_command = "ogr2ogr -overwrite -skipfailures -lco ENCODING=UTF-8 -f \"PostgreSQL\" PG:\" host=#{db_config["host"]} user=#{db_config["username"]} dbname=#{db_config["database"]}\" ./an/file"
     Ogr::Postgres.any_instance.expects(:system).with(ogr_command).once
 
-    Ogr::Postgres.import file: './an/file'
+    Ogr::Postgres.import './an/file'
+  end
+
+  test '.import imports a specific table when specified' do
+    table_name = "my_first_table"
+    db_config = Rails.configuration.database_configuration[Rails.env]
+
+    ogr_command = """
+      ogr2ogr -overwrite -skipfailures -lco ENCODING=UTF-8
+      -f \"PostgreSQL\" PG:\" host=#{db_config["host"]}
+      user=#{db_config["username"]} dbname=#{db_config["database"]}\"
+      -sql \"SELECT * FROM #{table_name}\"
+      ./an/file
+    """.squish
+    Ogr::Postgres.any_instance.expects(:system).with(ogr_command).once
+
+    Ogr::Postgres.import './an/file', table_name
+  end
+
+  test '.import imports a given table to a specific new table when
+   specified' do
+    table_name = "my_first_table"
+    new_table_name = "my_second_first_table"
+    db_config = Rails.configuration.database_configuration[Rails.env]
+
+    ogr_command = """
+      ogr2ogr -overwrite -skipfailures -lco ENCODING=UTF-8
+      -f \"PostgreSQL\" PG:\" host=#{db_config["host"]}
+      user=#{db_config["username"]} dbname=#{db_config["database"]}\"
+      -sql \"SELECT * FROM #{table_name}\"
+      -nln #{new_table_name}
+      ./an/file
+    """.squish
+    Ogr::Postgres.any_instance.expects(:system).with(ogr_command).once
+
+    Ogr::Postgres.import './an/file', table_name, new_table_name
+  end
+
+  test '.import raises an error if given a new table name, but no
+   original table name' do
+    new_table_name = "my_second_first_table"
+    error_msg = 'Given new table name, but no original table name'
+
+    assert_raises ArgumentError, error_msg do
+      Ogr::Postgres.import './an/file', nil, new_table_name
+    end
   end
 end
