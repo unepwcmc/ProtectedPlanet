e856639d11f270f25bfe232d139aba0d81ace893
Miguel Torres <migtorres@gmail.com>
2014-06-18 10:23:38 +0100

    refactors stats

--- lib/modules/stats.rb
+++ lib/modules/stats.rb
@@ -1,4 +1,7 @@
 class Stats
+
+  IUCN_CATEGORIES = "'Ia', 'Ib', 'II', 'II', 'IV', 'V', 'VI'"
+
   def self.global_pa_count 
     ProtectedArea.count
   end
@@ -11,7 +14,7 @@ class Stats
 
   def self.global_pas_with_iucn_category
     ProtectedArea.joins(:iucn_category)
-                 .where("iucn_categories.name IN ('Ia', 'Ib', 'II', 'II', 'IV', 'V', 'VI')")
+                 .where("iucn_categories.name IN (#{IUCN_CATEGORIES})")
                  .count
   end
 
@@ -20,12 +23,8 @@ class Stats
   end
 
   def self.global_protected_areas_by_designation
-    designation_count = Designation.select("designations.name, count(*)").joins(:protected_areas).group("designations.id, designations.name")
-    result = {}
-    designation_count.each do |designation|
-      result.merge!(designation[:name] => designation[:count])
-    end
-    result
+    global_protected_areas = ProtectedArea.all
+    protected_areas_by_designation global_protected_areas 
   end
 
   def self.countries_providing_data
@@ -44,29 +43,37 @@ class Stats
 
   def self.country_pas_with_iucn_category iso
     ProtectedArea.joins(:iucn_category, :countries)
-                 .where("iucn_categories.name IN ('Ia', 'Ib', 'II', 'II', 'IV', 'V', 'VI') AND iso = '#{iso}'")
+                 .where("iucn_categories.name IN (#{IUCN_CATEGORIES}) AND iso = '#{iso}'")
                  .count
   end
 
   def self.country_designation_count iso
-    country_protected_areas = ProtectedArea.joins(:countries).where("countries.iso = '#{iso}'")
+    country_protected_areas = protected_areas_in_country(iso)
     country_protected_areas.select('designations.id').joins(:designation).group('designations.id').length
   end
 
   def self.country_protected_areas_by_designation iso
-    country_protected_areas = ProtectedArea.joins(:countries).where("countries.iso = '#{iso}'")
-    designation_count = country_protected_areas.select('designations.name, count(*)').joins(:designation).group('designations.name')
-    result = {}
-    designation_count.each do |designation|
-      result.merge!(designation[:name] => designation[:count])
-    end
-    result
+    country_protected_areas = protected_areas_in_country(iso)
+    protected_areas_by_designation(country_protected_areas)
   end 
 
   private
 
   def self.total_pas_in entity_code,type
     ProtectedArea.joins(type.to_sym).where("iso = '#{entity_code}'").count
+  end
+
+  def self.protected_areas_in_country iso
+    ProtectedArea.joins(:countries).where("countries.iso = '#{iso}'")
+  end
+
+  def self.protected_areas_by_designation protected_areas
+    designation_count = protected_areas.select('designations.name, count(*)').joins(:designation).group('designations.name')
+    result = {}
+    designation_count.each do |designation|
+      result.merge!(designation[:name] => designation[:count])
+    end
+    result
   end
 end
 
