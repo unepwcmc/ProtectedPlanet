f2dc465f9c54341c5b859b141fae834cdbc9c151
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-06-18 11:08:08 +0100

    fix failing tests for pa show page

--- test/factories/protected_areas.rb
+++ test/factories/protected_areas.rb
@@ -2,5 +2,10 @@
 
 FactoryGirl.define do
   factory :protected_area do
+    legal_status_updated_at Date.new(2014,1,1)
+    association :designation, factory: :designation, name: 'My designation'
+    association :iucn_category, factory: :iucn_category, name: 'My IUCN category'
+    association :legal_status, factory: :legal_status, name: 'My legal status'
+    association :governance, factory: :governance, name: 'My governance'
   end
 end
