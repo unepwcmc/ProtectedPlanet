cac732f4521e89315afb75156459871cbfd33903
Adam Mulligan <adammulligan@gmail.com>
2014-06-19 17:29:41 +0100

    Adds Download KML file generator for all PAs

--- lib/modules/download/kml.rb
+++ lib/modules/download/kml.rb
@@ -1,2 +1,32 @@
 class Download::Kml
+  QUERY = "SELECT * FROM #{Wdpa::Release::IMPORT_VIEW_NAME}"
+
+  def self.generate zip_path
+    download_kml = new zip_path
+    download_kml.generate
+
+    download_kml
+  end
+
+  def initialize zip_path
+    @zip_path = zip_path
+  end
+
+  def generate
+    export_success = Ogr::Postgres.export :kml, kml_path, QUERY
+    if export_success
+      return system("zip -j #{@zip_path} #{kml_path}")
+    end
+  end
+
+  private
+
+  def kml_path
+    "#{path_without_extension}.kml"
+  end
+
+  def path_without_extension
+    filename_without_extension = File.basename(@zip_path, File.extname(@zip_path))
+    File.join(File.dirname(@zip_path), filename_without_extension)
+  end
 end
---
+++ test/unit/download/kml_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class DownloadKmlTest < ActiveSupport::TestCase
+  test '#generate, given a path, calls ogr2ogr with the path, a query,
+   and the specific driver' do
+    zip_file_path = './all-kml.zip'
+    kml_file_path = './all-kml.kml'
+    query = "SELECT * FROM #{Wdpa::Release::IMPORT_VIEW_NAME}"
+
+    Download::Kml.
+      any_instance.
+      expects(:system).
+      with("zip -j #{zip_file_path} #{kml_file_path}").
+      returns(true)
+    Ogr::Postgres.expects(:export).with(:kml, kml_file_path, query).returns(true)
+
+    assert Download::Kml.generate(zip_file_path),
+      "Expected #generate to return true on success"
+  end
+end
