0baab1ab2f8c7317f02d0aa632e36a3e0ac12e07
Miguel Torres <migtorres@gmail.com>
2014-06-19 15:50:36 +0100

    adds number of pas per designation in a region

--- lib/modules/stats.rb
+++ lib/modules/stats.rb
@@ -72,6 +72,25 @@ class Stats
     result[0]["count"].to_i
   end 
 
+  def self.region_protected_areas_by_designation iso
+    sql = """SELECT ds.name, count(1) FROM regions rg 
+                  RIGHT JOIN countries ct ON region_id = rg.id
+                  JOIN countries_protected_areas cpa ON country_id = ct.id
+                  JOIN protected_areas pa ON protected_area_id = pa.id
+                  JOIN designations ds ON designation_id = ds.id
+                  WHERE rg.iso =? 
+                  GROUP BY ds.id""".squish
+    sql_sanitized = ActiveRecord::Base.__send__(:sanitize_sql, [sql, iso], '')
+    result = DB.execute(sql_sanitized)
+    result_hash = {}
+    result.each do |designation|
+      result_hash.merge!(designation["name"] => designation["count"].to_i)
+    end
+    result_hash
+  end 
+
+
+
   #Countries
 
   def self.country_total_pas iso
