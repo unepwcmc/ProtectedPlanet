f6ccc77eb8e0f337421f23abaf239051a35cf3d2
Miguel Torres <migtorres@gmail.com>
2014-06-20 00:14:41 +0100

    returns regions tests deleted before

--- test/unit/stats_test.rb
+++ test/unit/stats_test.rb
@@ -50,10 +50,84 @@ class StatsTest < ActiveSupport::TestCase
     assert_equal 2, Stats::Global.countries_providing_data
   end
 
+    test '.number of pas in one region' do
+    region_1 = FactoryGirl.create(:region, name: 'Africasia', iso: 'AFS')
+    region_2 = FactoryGirl.create(:region, name: 'Eurociania', iso: 'EOC')
+    country_1 = FactoryGirl.create(:country, region: region_1)
+    country_2 = FactoryGirl.create(:country, region: region_2)
+    country_3 = FactoryGirl.create(:country, region: region_1)
+    FactoryGirl.create(:protected_area, countries: [country_1], wdpa_id: 1)
+    FactoryGirl.create(:protected_area, countries: [country_2], wdpa_id: 2)
+    FactoryGirl.create(:protected_area, countries: [country_3], wdpa_id: 3)
+    assert_equal 2, Stats::Regional.total_pas('AFS')
+  end
 
+  test '.percentage cover of protected areas in region' do
+    region = FactoryGirl.create(:region, iso: 'BANANA')
+    FactoryGirl.create(:regional_statistic, region: region, :percentage_cover_pas => 50)
+    assert_equal 50, Stats::Regional.percentage_cover_pas('BANANA')
+  end
 
+  test '.protected areas with IUCN category per region' do
+    iucn_category_1 = FactoryGirl.create(:iucn_category, name: 'Ib')
+    iucn_category_2 = FactoryGirl.create(:iucn_category, name: 'V')
+    no_iucn_category = FactoryGirl.create(:iucn_category, name: 'Pepe')
+    region_1 = FactoryGirl.create(:region, name: 'Americasia', iso: 'AMA')
+    region_2 = FactoryGirl.create(:region, name: 'Oceanafrica', iso: 'OAF')
+    country_1 = FactoryGirl.create(:country, region: region_1)
+    country_2 = FactoryGirl.create(:country, region: region_2)
+    country_3 = FactoryGirl.create(:country, region: region_1)
 
-  #Countries
+    FactoryGirl.create(:protected_area, iucn_category: iucn_category_1, countries: [country_1] )
+    FactoryGirl.create(:protected_area, iucn_category: iucn_category_2, countries: [country_2])
+    FactoryGirl.create(:protected_area, iucn_category: no_iucn_category, countries: [country_2])
+    FactoryGirl.create(:protected_area, iucn_category: iucn_category_2, countries: [country_3])
+    assert_equal 2, Stats::Regional.pas_with_iucn_category('AMA')
+  end
+
+  test '.number of types of designations per region' do
+    designation_1 = FactoryGirl.create(:designation, name: 'Lionel Messi')
+    designation_2 = FactoryGirl.create(:designation, name: 'Robin Van Persie')
+    designation_3 = FactoryGirl.create(:designation, name: 'Cristiano Ronaldo')
+    region_1 = FactoryGirl.create(:region, name: 'Afronesia', iso: 'AFE')
+    region_2 = FactoryGirl.create(:region, name: 'Eurarctica', iso: 'EUA')
+    country_1 = FactoryGirl.create(:country, region: region_1)
+    country_2 = FactoryGirl.create(:country, region: region_2)
+    country_3 = FactoryGirl.create(:country, region: region_1)
+    FactoryGirl.create(:protected_area, designation: designation_1, countries: [country_1])
+    FactoryGirl.create(:protected_area, designation: designation_1, countries: [country_1])
+    FactoryGirl.create(:protected_area, designation: designation_2, countries: [country_2])
+    FactoryGirl.create(:protected_area, designation: designation_3, countries: [country_3])
+    assert_equal 2, Stats::Regional.designation_count('AFE')
+  end
+
+  test '.total protected areas by designation in a region' do
+    designation_1 = FactoryGirl.create(:designation, name: 'Lionel Messi')
+    designation_2 = FactoryGirl.create(:designation, name: 'Robin Van Persie')
+    designation_3 = FactoryGirl.create(:designation, name: 'Cristiano Ronaldo')
+    region_1 = FactoryGirl.create(:region, name: 'Afronesia', iso: 'AFE')
+    region_2 = FactoryGirl.create(:region, name: 'Eurarctica', iso: 'EUA')
+    country_1 = FactoryGirl.create(:country, region: region_1)
+    country_2 = FactoryGirl.create(:country, region: region_2)
+    country_3 = FactoryGirl.create(:country, region: region_1)
+    FactoryGirl.create(:protected_area, designation: designation_1, countries: [country_1])
+    FactoryGirl.create(:protected_area, designation: designation_1, countries: [country_1])
+    FactoryGirl.create(:protected_area, designation: designation_2, countries: [country_2])
+    FactoryGirl.create(:protected_area, designation: designation_3, countries: [country_3])
+    assert_equal ({'Cristiano Ronaldo' => 1, 'Lionel Messi' => 2}), Stats::Regional.protected_areas_by_designation('AFE')
+  end
+
+  test '.number of countries providing data by region' do
+    region_1 = FactoryGirl.create(:region, name: 'Afronesia', iso: 'AFE')
+    region_2 = FactoryGirl.create(:region, name: 'Eurarctica', iso: 'EUA')
+    country_1 = FactoryGirl.create(:country, region: region_1)
+    country_2 = FactoryGirl.create(:country, region: region_2)
+    country_3 = FactoryGirl.create(:country, region: region_1)
+    FactoryGirl.create(:protected_area, countries: [country_1], wdpa_id: 1)
+    FactoryGirl.create(:protected_area, countries: [country_2], wdpa_id: 2)
+    FactoryGirl.create(:protected_area, countries: [country_3], wdpa_id: 3)
+    assert_equal 2, Stats::Regional.countries_providing_data('AFE')
+  end
 
 
   test '.number of pas in one country' do
