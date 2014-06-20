1e8f9521e87b2f46dc01492ac9a4d1c7fb6e2ffc
Miguel Torres <migtorres@gmail.com>
2014-06-20 12:01:30 +0100

    adds integration tests for regional and country stats

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do  
 
   get 'stats/global', to: 'stats/global#index'
-  get 'stats/regional/:iso', to: 'stats/global#show'
+  get 'stats/regional/:iso', to: 'stats/regional#show'
+  get 'stats/country/:iso', to: 'stats/country#show'
   get '/search', to: 'search#index'
   get '/:id', to: 'protected_areas#show'
 end
--- test/integration/stats_show_test.rb
+++ test/integration/stats_show_test.rb
@@ -2,13 +2,22 @@ require 'test_helper'
 
 class StatsShowTest < ActionDispatch::IntegrationTest
 
-  test 'renders the Global stats' do
+  test 'renders global stats' do
     
     get '/stats/global'
     assert page.has_content?, @response.body
   end
 
+  test 'renders regional stats' do
+    region_1 = FactoryGirl.create(:region, name: 'Americasia', iso: 'AMA')
+    get '/stats/regional/AMA'
+    assert page.has_content?, @response.body
+  end
 
-
+  test 'renders country stats' do
+    region_1 = FactoryGirl.create(:region, name: 'Orange Emirate', iso: 'PUM')
+    get '/stats/country/PUM'
+    assert page.has_content?, @response.body
+  end
 
 end
