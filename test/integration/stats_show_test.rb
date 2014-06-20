e82e5775b95e227868d239b1149e68e1e79761db
Miguel Torres <migtorres@gmail.com>
2014-06-20 12:12:22 +0100

    fixed stats show region call

--- test/integration/stats_show_test.rb
+++ test/integration/stats_show_test.rb
@@ -9,13 +9,13 @@ class StatsShowTest < ActionDispatch::IntegrationTest
   end
 
   test 'renders regional stats' do
-    region_1 = FactoryGirl.create(:region, name: 'Americasia', iso: 'AMA')
+    FactoryGirl.create(:region, name: 'Americasia', iso: 'AMA')
     get '/stats/regional/AMA'
     assert page.has_content?, @response.body
   end
 
   test 'renders country stats' do
-    region_1 = FactoryGirl.create(:region, name: 'Orange Emirate', iso: 'PUM')
+    FactoryGirl.create(:country, name: 'Orange Emirate', iso: 'PUM')
     get '/stats/country/PUM'
     assert page.has_content?, @response.body
   end
