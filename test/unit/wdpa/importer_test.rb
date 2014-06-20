1f0bb288fd3d1b5bb8d39092aebdb46a6dc03bf9
Adam Mulligan <adammulligan@gmail.com>
2014-06-20 11:24:31 +0100

    Runs download generation on import

--- test/unit/wdpa/importer_test.rb
+++ test/unit/wdpa/importer_test.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class TestWdpaImporter < ActiveSupport::TestCase
   test '.import creates a WDPA Release, imports the protected areas from
-   it and cleans up' do
+   it, generates downloads for the imported PAs and cleans up' do
     wdpa_release = mock()
     wdpa_release.stubs(:clean_up)
 
     Wdpa::Release.expects(:download).returns(wdpa_release)
     Wdpa::ProtectedAreaImporter.expects(:import).with(wdpa_release)
+    Wdpa::DownloadGenerator.expects(:generate)
 
     Wdpa::Importer.import
   end
