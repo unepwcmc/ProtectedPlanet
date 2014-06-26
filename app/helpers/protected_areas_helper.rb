8bd0296e2274a198a746bf8d8f038ce6c2b42843
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-06-26 14:01:36 +0100

    remove unused PA helper

--- app/helpers/protected_areas_helper.rb
+++ app/helpers/protected_areas_helper.rb
@@ -1,10 +1,2 @@
 module ProtectedAreasHelper
-  def link_to_download protected_area, type
-    url_for(
-      controller: 'downloads',
-      action: 'show',
-      id: protected_area.countries.first.iso_3,
-      type: type
-    )
-  end
 end
