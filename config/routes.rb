34ccbca1a4061d1f11159a0c121456d7778be651
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-06-26 14:05:25 +0100

    prefer iso_3 to id for param in downloads route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get '/', to: 'home#index'
 
-  get '/downloads/:id', to: 'downloads#show'
+  get '/downloads/:iso_3', to: 'downloads#show'
 
   get '/search', to: 'search#index'
   get '/:id', to: 'protected_areas#show'
