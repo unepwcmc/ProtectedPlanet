62a27d62b99600ad3cf3eadd7a1b931243a74fdc
Adam Mulligan <adammulligan@gmail.com>
2014-06-26 13:37:29 +0100

    Removes PA coverage stat as we can't calculate that yet

--- app/controllers/stats/country_controller.rb
+++ app/controllers/stats/country_controller.rb
@@ -1,12 +1,11 @@
 class Stats::CountryController < ApplicationController
   def show
     iso = params[:iso]
-    @country =  ::Country.where("iso = ?", iso).first
+
+    @country =  Country.where(iso: iso).first
     @number_of_pas = Stats::Country.total_pas iso
     @pas_with_iucn_category = Stats::Country.pas_with_iucn_category iso
-    #@territory_covered_by_pas = Stats::Country.percentage_cover_pas iso
     @number_of_designations = Stats::Country.designation_count iso
     @designations_by_frequency = Stats::Country.protected_areas_by_designation iso
-    
   end
 end
