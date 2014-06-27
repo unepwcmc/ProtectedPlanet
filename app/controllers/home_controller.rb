5abbca451441e2e07c194931be5bfa19b1c5b1e7
Adam Mulligan <adammulligan@gmail.com>
2014-06-27 15:03:00 +0100

    Adds a couple extra stats to home page so it doesn't look so sparse

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @number_of_pas = Stats::Global.pa_count
     @number_of_designations = Stats::Global.designation_count
+    @pas_with_iucn_category = Stats::Global.pas_with_iucn_category
+    @countries_providing_data = Stats::Global.countries_providing_data
   end
 end
