309b61e8b695ef314eb11511c4d30c3f38888b0a
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-07-03 12:08:06 +0100

    add caching of home and pa show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def enable_caching
+    expires_in Rails.application.secrets.cache_max_age, public: true
+  end
 end
