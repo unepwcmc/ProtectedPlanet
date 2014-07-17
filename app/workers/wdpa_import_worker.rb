190cd423d3c2fad13e23b2adf2d6e7c19876525b
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-07-17 18:47:25 +0100

    remove responsibilities from WdpaImportWorker

--- app/workers/wdpa_import_worker.rb
+++ app/workers/wdpa_import_worker.rb
@@ -1,52 +1,14 @@
 class WdpaImportWorker
-  @@REDIS_PREFIX = Rails.application.secrets.redis['wdpa_imports_prefix']
-  @@LOCKING_KEY  = "#{@@REDIS_PREFIX}:locking_key"
-
   include Sidekiq::Worker
   sidekiq_options :retry => false
 
   def perform
-    return unless lock_import
+    return unless ImportTools.create_import
 
     begin
       Wdpa::Importer.import
-      log_success
     rescue => e
-      log_failure(e)
-    ensure
-      unlock_import
-    end
-  end
-
-  private
-
-  def lock_import
-    Sidekiq.redis{|client| client.setnx(@@LOCKING_KEY, Time.now.to_i)}
-  end
-
-  def unlock_import
-    Sidekiq.redis{|client| client.del(@@LOCKING_KEY)}
-  end
-
-  def log_success
-    success_key = "#{@@REDIS_PREFIX}:success"
-    Sidekiq.redis do |client|
-      client.zadd(
-        success_key,
-        Time.now.to_i,
-        {message: 'Imported successfully'}.to_json
-      )
-    end
-  end
-
-  def log_failure error
-    failure_key = "#{@@REDIS_PREFIX}:failure"
-    Sidekiq.redis do |client|
-      client.zadd(
-        failure_key,
-        Time.now.to_i,
-        {message: error.message, backtrace: error.backtrace}.to_json
-      )
+      # ImportTools.abort_import
     end
   end
 end
--- test/unit/workers/wdpa_import_worker_test.rb
+++ test/unit/workers/wdpa_import_worker_test.rb
@@ -2,18 +2,15 @@ require 'test_helper'
 
 class WdpaImportWorkerTest < ActiveSupport::TestCase
   test '.perform stops immediately if there is another import ongoing' do
+    ImportTools.stubs(:create_import).returns(false)
     Wdpa::Importer.expects(:import).never
-    Sidekiq.expects(:redis).yields(stub_everything()).returns(nil)
 
     Sidekiq::Testing.inline! { WdpaImportWorker.perform_async }
   end
 
   test '.perform calls the WdpaImporter and unlocks redis after the process' do
+    ImportTools.stubs(:create_import).returns(true)
     Wdpa::Importer.expects(:import)
-
-    redis_mock = stub_everything()
-    redis_mock.expects(:del)
-    Sidekiq.expects(:redis).yields(redis_mock).returns(true).at_least_once
 
     Sidekiq::Testing.inline! { WdpaImportWorker.perform_async }
   end
