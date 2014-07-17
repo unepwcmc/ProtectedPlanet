6f10a1806271091706e048a69f23e1cefec998a3
Adam Mulligan <adammulligan@gmail.com>
2014-07-17 14:42:54 +0100

    Compacts migrations to reduce number required to db setup
    Move indexes for country geometries in to migrations

--- db/migrate/20140704154012_add_columns_to_country_statistic.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToCountryStatistic < ActiveRecord::Migration
-  def change
-    add_column :country_statistics, :percentage_region, :decimal
-    add_column :country_statistics, :percentage_land, :decimal
-    add_column :country_statistics, :percentage_water, :decimal
-    add_column :country_statistics, :percentage_coast, :decimal
-  end
-end
