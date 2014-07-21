1dc3e2136a3e92294508b73423749cd05ee414d3
Adam Mulligan <adammulligan@gmail.com>
2014-07-21 11:38:12 +0100

    Geometry dissolver buffers point geometries inline
    This way we don't need to (we can't anyway) modify the standard_points
    table.

--- test/unit/geospatial/country_geometry_populator_test.rb
+++ test/unit/geospatial/country_geometry_populator_test.rb
@@ -11,28 +11,31 @@ class CountryGeometryPopulatorTest < ActiveSupport::TestCase
       FROM (
        SELECT ST_UNION(the_geom) as the_geom
        FROM (
-         SELECT  iso3, wkb_geometry the_geom FROM standard_polygons pol
-           WHERE pol.iso3 = 'FAK'
-            AND st_isvalid(pol.wkb_geometry)
-            AND pol.marine = '1'
-            AND pol.status NOT IN ('Proposed', 'Not Reported')
+         SELECT  iso3, wkb_geometry the_geom FROM standard_polygons polygon
+           WHERE polygon.iso3 = 'FAK'
+            AND ST_IsValid(polygon.wkb_geometry)
+            AND polygon.marine = '1'
+            AND polygon.status NOT IN ('Proposed', 'Not Reported')
 
          UNION
 
-         SELECT iso3, buffer_geom the_geom FROM standard_points poi
-           WHERE poi.iso3 = 'FAK'
-            AND st_isvalid(poi.buffer_geom)
-            AND poi.marine = '1'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
+         SELECT iso3, the_geom FROM (
+           SELECT iso3, ST_Buffer(wkb_geometry::geography, |/( rep_area*1000000 / pi() ))::geometry the_geom
+           FROM standard_points point
+           WHERE point.iso3 = 'FAK'
+             AND point.marine = '1'
+             AND point.status NOT IN ('Proposed', 'Not Reported')
+         ) AS c
+         WHERE ST_IsValid(the_geom)
 
          UNION
 
-         SELECT c.iso_3, ST_Makevalid(ST_Intersection(c.land_geom,s.wkb_geometry)) FROM standard_polygons s
-           INNER JOIN countries c ON ST_Intersects(c.land_geom,s.wkb_geometry)
-           WHERE s.iso3 LIKE '%,%'
-            AND c.iso_3 = 'FAK'
-            AND poi.marine = '1'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
+         SELECT country.iso_3, ST_Makevalid(ST_Intersection(country.land_geom, polygon.wkb_geometry)) FROM standard_polygons polygon
+           INNER JOIN countries country ON ST_Intersects(country.land_geom, polygon.wkb_geometry)
+           WHERE polygon.iso3 LIKE '%,%'
+            AND country.iso_3 = 'FAK'
+            AND polygon.marine = '1'
+            AND polygon.status NOT IN ('Proposed', 'Not Reported')
         ) b
       ) a
       WHERE iso_3 = 'FAK'
@@ -44,107 +47,34 @@ class CountryGeometryPopulatorTest < ActiveSupport::TestCase
       FROM (
        SELECT ST_UNION(the_geom) as the_geom
        FROM (
-         SELECT  iso3, wkb_geometry the_geom FROM standard_polygons pol
-           WHERE pol.iso3 = 'FAK'
-            AND st_isvalid(pol.wkb_geometry)
-            AND pol.marine = '0'
-            AND pol.status NOT IN ('Proposed', 'Not Reported')
+         SELECT  iso3, wkb_geometry the_geom FROM standard_polygons polygon
+           WHERE polygon.iso3 = 'FAK'
+            AND ST_IsValid(polygon.wkb_geometry)
+            AND polygon.marine = '0'
+            AND polygon.status NOT IN ('Proposed', 'Not Reported')
 
          UNION
 
-         SELECT iso3, buffer_geom the_geom FROM standard_points poi
-           WHERE poi.iso3 = 'FAK'
-            AND st_isvalid(poi.buffer_geom)
-            AND poi.marine = '0'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
+         SELECT iso3, the_geom FROM (
+           SELECT iso3, ST_Buffer(wkb_geometry::geography, |/( rep_area*1000000 / pi() ))::geometry the_geom
+           FROM standard_points point
+           WHERE point.iso3 = 'FAK'
+             AND point.marine = '0'
+             AND point.status NOT IN ('Proposed', 'Not Reported')
+         ) AS c
+         WHERE ST_IsValid(the_geom)
 
          UNION
 
-         SELECT c.iso_3, ST_Makevalid(ST_Intersection(c.land_geom,s.wkb_geometry)) FROM standard_polygons s
-           INNER JOIN countries c ON ST_Intersects(c.land_geom,s.wkb_geometry)
-           WHERE s.iso3 LIKE '%,%'
-            AND c.iso_3 = 'FAK'
-            AND poi.marine = '0'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
+         SELECT country.iso_3, ST_Makevalid(ST_Intersection(country.land_geom, polygon.wkb_geometry)) FROM standard_polygons polygon
+           INNER JOIN countries country ON ST_Intersects(country.land_geom, polygon.wkb_geometry)
+           WHERE polygon.iso3 LIKE '%,%'
+            AND country.iso_3 = 'FAK'
+            AND polygon.marine = '0'
+            AND polygon.status NOT IN ('Proposed', 'Not Reported')
         ) b
       ) a
       WHERE iso_3 = 'FAK'
-    """.squish
-
-    ActiveRecord::Base.connection.expects(:execute).with(marine_query)
-    ActiveRecord::Base.connection.expects(:execute).with(land_query)
-
-    Geospatial::CountryGeometryPopulator.populate_dissolved_geometries country
-  end
-
-  test '#dissolve, given a country defined as "complex", simplifies the
-   terrestrial geometry' do
-    country = FactoryGirl.build(:country, iso_3: 'AUS')
-
-    marine_query = """
-      UPDATE countries
-      SET marine_pas_geom = a.the_geom
-      FROM (
-       SELECT ST_UNION(the_geom) as the_geom
-       FROM (
-         SELECT  iso3, wkb_geometry the_geom FROM standard_polygons pol
-           WHERE pol.iso3 = 'AUS'
-            AND st_isvalid(pol.wkb_geometry)
-            AND pol.marine = '1'
-            AND pol.status NOT IN ('Proposed', 'Not Reported')
-
-         UNION
-
-         SELECT iso3, buffer_geom the_geom FROM standard_points poi
-           WHERE poi.iso3 = 'AUS'
-            AND st_isvalid(poi.buffer_geom)
-            AND poi.marine = '1'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
-
-         UNION
-
-         SELECT c.iso_3, ST_Makevalid(ST_Intersection(c.land_geom,s.wkb_geometry)) FROM standard_polygons s
-           INNER JOIN countries c ON ST_Intersects(c.land_geom,s.wkb_geometry)
-           WHERE s.iso3 LIKE '%,%'
-            AND c.iso_3 = 'AUS'
-            AND poi.marine = '1'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
-        ) b
-      ) a
-      WHERE iso_3 = 'AUS'
-    """.squish
-
-    land_query = """
-      UPDATE countries
-      SET land_pas_geom = a.the_geom
-      FROM (
-       SELECT ST_UNION(the_geom) as the_geom
-       FROM (
-         SELECT  iso3, ST_Makevalid(ST_Buffer(ST_Simplify(wkb_geometry,0.005),0.00000001)) the_geom FROM standard_polygons pol
-           WHERE pol.iso3 = 'AUS'
-            AND st_isvalid(pol.wkb_geometry)
-            AND pol.marine = '0'
-            AND pol.status NOT IN ('Proposed', 'Not Reported')
-
-         UNION
-
-         SELECT iso3, buffer_geom the_geom FROM standard_points poi
-           WHERE poi.iso3 = 'AUS'
-            AND st_isvalid(poi.buffer_geom)
-            AND poi.marine = '0'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
-
-         UNION
-
-         SELECT c.iso_3, ST_Makevalid(ST_Intersection(c.land_geom,s.wkb_geometry)) FROM standard_polygons s
-           INNER JOIN countries c ON ST_Intersects(c.land_geom,s.wkb_geometry)
-           WHERE s.iso3 LIKE '%,%'
-            AND c.iso_3 = 'AUS'
-            AND poi.marine = '0'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
-        ) b
-      ) a
-      WHERE iso_3 = 'AUS'
     """.squish
 
     ActiveRecord::Base.connection.expects(:execute).with(marine_query)
@@ -163,28 +93,31 @@ class CountryGeometryPopulatorTest < ActiveSupport::TestCase
       FROM (
        SELECT ST_UNION(the_geom) as the_geom
        FROM (
-         SELECT  iso3, ST_Makevalid(ST_Buffer(ST_Simplify(wkb_geometry,0.005),0.00000001)) the_geom FROM standard_polygons pol
-           WHERE pol.iso3 = 'GBR'
-            AND st_isvalid(pol.wkb_geometry)
-            AND pol.marine = '1'
-            AND pol.status NOT IN ('Proposed', 'Not Reported')
+         SELECT  iso3, ST_Makevalid(ST_Buffer(ST_Simplify(wkb_geometry,0.005),0.00000001)) the_geom FROM standard_polygons polygon
+           WHERE polygon.iso3 = 'GBR'
+            AND ST_IsValid(polygon.wkb_geometry)
+            AND polygon.marine = '1'
+            AND polygon.status NOT IN ('Proposed', 'Not Reported')
 
          UNION
 
-         SELECT iso3, buffer_geom the_geom FROM standard_points poi
-           WHERE poi.iso3 = 'GBR'
-            AND st_isvalid(poi.buffer_geom)
-            AND poi.marine = '1'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
+         SELECT iso3, the_geom FROM (
+           SELECT iso3, ST_Buffer(wkb_geometry::geography, |/( rep_area*1000000 / pi() ))::geometry the_geom
+           FROM standard_points point
+           WHERE point.iso3 = 'GBR'
+             AND point.marine = '1'
+             AND point.status NOT IN ('Proposed', 'Not Reported')
+         ) AS c
+         WHERE ST_IsValid(the_geom)
 
          UNION
 
-         SELECT c.iso_3, ST_Makevalid(ST_Intersection(c.land_geom,s.wkb_geometry)) FROM standard_polygons s
-           INNER JOIN countries c ON ST_Intersects(c.land_geom,s.wkb_geometry)
-           WHERE s.iso3 LIKE '%,%'
-            AND c.iso_3 = 'GBR'
-            AND poi.marine = '1'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
+         SELECT country.iso_3, ST_Makevalid(ST_Intersection(country.land_geom, polygon.wkb_geometry)) FROM standard_polygons polygon
+           INNER JOIN countries country ON ST_Intersects(country.land_geom, polygon.wkb_geometry)
+           WHERE polygon.iso3 LIKE '%,%'
+            AND country.iso_3 = 'GBR'
+            AND polygon.marine = '1'
+            AND polygon.status NOT IN ('Proposed', 'Not Reported')
         ) b
       ) a
       WHERE iso_3 = 'GBR'
@@ -196,28 +129,31 @@ class CountryGeometryPopulatorTest < ActiveSupport::TestCase
       FROM (
        SELECT ST_UNION(the_geom) as the_geom
        FROM (
-         SELECT  iso3, ST_Makevalid(ST_Buffer(ST_Simplify(wkb_geometry,0.005),0.00000001)) the_geom FROM standard_polygons pol
-           WHERE pol.iso3 = 'GBR'
-            AND st_isvalid(pol.wkb_geometry)
-            AND pol.marine = '0'
-            AND pol.status NOT IN ('Proposed', 'Not Reported')
+         SELECT  iso3, ST_Makevalid(ST_Buffer(ST_Simplify(wkb_geometry,0.005),0.00000001)) the_geom FROM standard_polygons polygon
+           WHERE polygon.iso3 = 'GBR'
+            AND ST_IsValid(polygon.wkb_geometry)
+            AND polygon.marine = '0'
+            AND polygon.status NOT IN ('Proposed', 'Not Reported')
 
          UNION
 
-         SELECT iso3, buffer_geom the_geom FROM standard_points poi
-           WHERE poi.iso3 = 'GBR'
-            AND st_isvalid(poi.buffer_geom)
-            AND poi.marine = '0'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
+         SELECT iso3, the_geom FROM (
+           SELECT iso3, ST_Buffer(wkb_geometry::geography, |/( rep_area*1000000 / pi() ))::geometry the_geom
+           FROM standard_points point
+           WHERE point.iso3 = 'GBR'
+             AND point.marine = '0'
+             AND point.status NOT IN ('Proposed', 'Not Reported')
+         ) AS c
+         WHERE ST_IsValid(the_geom)
 
          UNION
 
-         SELECT c.iso_3, ST_Makevalid(ST_Intersection(c.land_geom,s.wkb_geometry)) FROM standard_polygons s
-           INNER JOIN countries c ON ST_Intersects(c.land_geom,s.wkb_geometry)
-           WHERE s.iso3 LIKE '%,%'
-            AND c.iso_3 = 'GBR'
-            AND poi.marine = '0'
-            AND poi.status NOT IN ('Proposed', 'Not Reported')
+         SELECT country.iso_3, ST_Makevalid(ST_Intersection(country.land_geom, polygon.wkb_geometry)) FROM standard_polygons polygon
+           INNER JOIN countries country ON ST_Intersects(country.land_geom, polygon.wkb_geometry)
+           WHERE polygon.iso3 LIKE '%,%'
+            AND country.iso_3 = 'GBR'
+            AND polygon.marine = '0'
+            AND polygon.status NOT IN ('Proposed', 'Not Reported')
         ) b
       ) a
       WHERE iso_3 = 'GBR'
