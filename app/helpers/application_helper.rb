baf1e0cf1ec249ad7a969cdfc34f83450fb81e85
Adam Mulligan <adammulligan@gmail.com>
2014-07-22 10:39:48 +0100

    Replaces rounding helper with something more sane

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,4 @@ module ApplicationHelper
   def commaify number
     number_with_delimiter(number, delimeter: ',')
   end
-
-  def round_to_first_non_null_digit number
-    if number
-      /^-{0,1}[0-9]+\.*0*[1-9]{0,1}/.match(number.to_s).to_s
-    else
-      ''
-    end
-  end
 end
