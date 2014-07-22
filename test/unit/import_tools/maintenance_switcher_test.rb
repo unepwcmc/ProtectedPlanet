ce6dcb5a3d42b33c5382a3458b3599074e837450
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-07-22 18:53:37 +0100

    add ImportTools::MaintenanceSwitcher

---
+++ test/unit/import_tools/maintenance_switcher_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class ImportToolsMaintenanceSwitcherTest < ActiveSupport::TestCase
+  test '#on creates a new instance of MaintenanceSwitcher' do
+    ImportTools::MaintenanceSwitcher.expects(:new).returns(stub_everything)
+    ImportTools::MaintenanceSwitcher.on
+  end
+
+  test '#off creates a new instance of MaintenanceSwitcher' do
+    ImportTools::MaintenanceSwitcher.expects(:new).returns(stub_everything)
+    ImportTools::MaintenanceSwitcher.off
+  end
+
+  test '#on does an HTTP call to the maintenance url' do
+    key = Rails.application.secrets.maintenance_mode_key
+
+    fake_url = 'http://example.com/maintenance'
+    ImportTools::MaintenanceSwitcher.any_instance.stubs(:url_for).returns(fake_url)
+
+    HTTParty.expects(:put).with(
+      fake_url,
+      query: {maintenance_mode_on: true},
+      headers: {'Authorization' => key}
+    )
+    ImportTools::MaintenanceSwitcher.on
+  end
+
+  test '#off does an HTTP call to the maintenance url' do
+    key = Rails.application.secrets.maintenance_mode_key
+
+    fake_url = 'http://example.com/maintenance'
+    ImportTools::MaintenanceSwitcher.any_instance.stubs(:url_for).returns(fake_url)
+
+    HTTParty.expects(:put).with(
+      fake_url,
+      query: {maintenance_mode_on: false},
+      headers: {'Authorization' => key}
+    )
+    ImportTools::MaintenanceSwitcher.off
+  end
+end
