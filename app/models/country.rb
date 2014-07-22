bda3807273e04e55ac95e10611c7e322d1a8cd2f
Adam Mulligan <adammulligan@gmail.com>
2014-07-22 15:32:35 +0100

    Removes whitespace

--- app/models/country.rb
+++ app/models/country.rb
@@ -8,7 +8,7 @@ class Country < ActiveRecord::Base
     rgeo_factory = RGeo::Geos.factory srid: 4326
     bounds = RGeo::Cartesian::BoundingBox.new rgeo_factory
     bounds.add bounding_box
-    
+
     [
       [bounds.min_y, bounds.min_x],
       [bounds.max_y, bounds.max_x]
