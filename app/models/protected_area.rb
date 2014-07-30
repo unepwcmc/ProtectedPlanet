dc928c6f3c9e8eff615a4403c45f20ae53dc5764
Adam Mulligan <adammulligan@gmail.com>
2014-07-30 10:08:50 +0100

    Includes elastic search model callbacks
    ES Index will be created when PAs are created, i.e. during the import.

--- app/models/protected_area.rb
+++ app/models/protected_area.rb
@@ -1,5 +1,6 @@
 class ProtectedArea < ActiveRecord::Base
   include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
 
   has_and_belongs_to_many :countries
   has_and_belongs_to_many :sub_locations
