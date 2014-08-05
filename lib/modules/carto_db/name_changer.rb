ef0797fc6a8a31e7f73874025005bd845ef42c6e
Miguel Torres <migtorres@gmail.com>
2014-08-05 13:03:33 +0100

    adds delete from current table

---
+++ lib/modules/carto_db/name_changer.rb
@@ -0,0 +1,36 @@
+class CartoDb::NameChanger
+  include HTTParty
+  default_timeout 1800
+
+  def initialize username, api_key
+    self.class.base_uri "https://#{username}.cartodb.com/api/v2/sql"
+    @options = { query: { api_key: api_key } }
+  end
+
+  def delete_current current_table
+    query = delete_query(current_table)
+    full_transaction = transaction(query)
+
+    @options[:query][:q] = full_transaction
+    response = self.class.get('/', @options)
+
+    return response.code == 200 ? true : false
+
+
+  end
+
+  private
+
+  def delete_query table
+    """DELETE FROM #{table};""".squish
+  end
+
+  def transaction query
+    """BEGIN;
+       #{query}
+       COMMIT;""".squish
+  end
+
+
+
+end
