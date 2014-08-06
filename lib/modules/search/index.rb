77c1bb93cff4d5e5f38a64ae659529218ed8e76b
Adam Mulligan <adammulligan@gmail.com>
2014-08-06 13:54:37 +0100

    Removes index logging
    Upsets test output

--- lib/modules/search/index.rb
+++ lib/modules/search/index.rb
@@ -20,18 +20,12 @@ class Search::Index
   def documents
     documents = []
 
-    index = 1;
-    total = @model_enumerable.first.class.count
-
     @model_enumerable.find_in_batches.each do |group|
       group.each do |model_instance|
-        STDOUT.write "\r#{index}/#{total}";
         documents << index_header(model_instance)
         documents << model_instance.as_indexed_json
-        index += 1
       end
     end
-    puts
 
     documents
   end
