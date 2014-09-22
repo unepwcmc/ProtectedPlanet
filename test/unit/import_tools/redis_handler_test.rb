a8aae3bc0ced0e635cc3e566278ee622198d3901
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-09-22 19:15:40 +0100

    replace Sidekiq's redis with $redis in codebase

--- test/unit/import_tools/redis_handler_test.rb
+++ test/unit/import_tools/redis_handler_test.rb
@@ -5,16 +5,12 @@ class ImportToolsRedisHandlerTest < ActiveSupport::TestCase
     @redis_handler = ImportTools::RedisHandler.new
   end
 
-  test '.new sets the redis instance variable' do
-    assert_not_nil @redis_handler.redis
-  end
-
   test '.lock calls the setnx redis command with the given arguments' do
     current_key = 'test_key'
     @redis_handler.stubs(:current_key).returns(current_key)
 
     value = 'test_value'
-    @redis_handler.redis.expects(:setnx).with(current_key, value)
+    $redis.expects(:setnx).with(current_key, value)
 
     @redis_handler.lock(value)
   end
@@ -22,7 +18,7 @@ class ImportToolsRedisHandlerTest < ActiveSupport::TestCase
   test '.unlock calls the setnx redis command with the given arguments' do
     current_key = 'test_key'
     @redis_handler.stubs(:current_key).returns(current_key)
-    @redis_handler.redis.expects(:del).with(current_key)
+    $redis.expects(:del).with(current_key)
 
     @redis_handler.unlock
   end
@@ -30,12 +26,12 @@ class ImportToolsRedisHandlerTest < ActiveSupport::TestCase
   test '.current_id gets the id of the current import from redis' do
     expected_key = "#{Rails.application.secrets.redis['wdpa_imports_prefix']}:current"
 
-    @redis_handler.redis.expects(:get).with(expected_key)
+    $redis.expects(:get).with(expected_key)
     @redis_handler.current_id
   end
 
   test '.increase_property_and_compare calls redis commands in a redis transaction' do
-    @redis_handler.redis.expects(:multi).yields.returns([])
+    $redis.expects(:multi).yields.returns([])
     @redis_handler.increase_property_and_compare(123, :test_key_1, :test_key_2)
   end
 
@@ -44,14 +40,14 @@ class ImportToolsRedisHandlerTest < ActiveSupport::TestCase
     key_1, key_2 = [:key_1, :key_2]
     value_1, value_2 = [123, 123]
 
-    @redis_handler.redis.stubs(:multi).returns([value_1, value_2])
+    $redis.stubs(:multi).returns([value_1, value_2])
 
     assert @redis_handler.increase_property_and_compare(id, key_1, key_2)
   end
 
   test '.add_to_previous_ids calls redis zadd with the given id' do
     id = 123
-    @redis_handler.redis.expects(:zadd).with(anything, id, id.to_s)
+    $redis.expects(:zadd).with(anything, id, id.to_s)
 
     @redis_handler.add_to_previous_ids(id)
   end
