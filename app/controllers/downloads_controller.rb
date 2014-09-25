4d22d588c8a14568f3425b91ac28372c5bb31f3f
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-09-25 14:09:14 +0100

    add creation of downloads in DownloadsController

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -5,4 +5,14 @@ class DownloadsController < ApplicationController
 
     redirect_to Download.link_to(country_iso_3, type)
   end
+
+  def create
+    search = Search.download(params[:q], {filters: filters})
+    render json: {token: search.token}
+  end
+
+  private
+  def filters
+    params.permit(Search::ALLOWED_FILTERS)
+  end
 end
