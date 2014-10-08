3020c6952c6a197085c37d7affe3b8960b8e13f5
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-08 14:33:39 +0100

    fix import_tools test

--- test/unit/import_tools_test.rb
+++ test/unit/import_tools_test.rb
@@ -8,7 +8,7 @@ class ImportToolsTest < ActiveSupport::TestCase
 
   test '#current_import returns an Import with the token returned from Redis' do
     ImportTools::RedisHandler.any_instance.stubs(:current_token).returns(123)
-    ImportTools::Import.expects(:find).with(123)
+    ImportTools::Import.expects(:new).with(123)
 
     ImportTools.current_import
   end
@@ -22,7 +22,7 @@ class ImportToolsTest < ActiveSupport::TestCase
 
   test '#last_import returns an instance of Import with the token of the last import' do
     ImportTools::RedisHandler.any_instance.expects(:previous_imports).returns([1, 2, 3])
-    ImportTools::Import.expects(:find).with(3)
+    ImportTools::Import.expects(:new).with(3)
 
     ImportTools.last_import
   end
