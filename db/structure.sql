bf7c339b5ad63997d179b4df64174b5266882d2d
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-20 15:05:51 +0100

    💩:

--- db/structure.sql
+++ db/structure.sql
@@ -928,7 +928,47 @@ CREATE MATERIALIZED VIEW tsvector_search_documents AS
 
 
 --
--- Name: wikipedia_articles; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+-- Name: users; Type: TABLE; Schema: public; Owner: -; Tablespace:
+--
+
+CREATE TABLE users (
+    id integer NOT NULL,
+    email character varying(255) DEFAULT ''::character varying NOT NULL,
+    encrypted_password character varying(255) DEFAULT ''::character varying NOT NULL,
+    reset_password_token character varying(255),
+    reset_password_sent_at timestamp without time zone,
+    remember_created_at timestamp without time zone,
+    sign_in_count integer DEFAULT 0 NOT NULL,
+    current_sign_in_at timestamp without time zone,
+    last_sign_in_at timestamp without time zone,
+    current_sign_in_ip character varying(255),
+    last_sign_in_ip character varying(255),
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
+
+
+--
+-- Name: wikipedia_articles; Type: TABLE; Schema: public; Owner: -; Tablespace:
 --
 
 CREATE TABLE wikipedia_articles (
@@ -1097,6 +1137,13 @@ ALTER TABLE ONLY sub_locations ALTER COLUMN id SET DEFAULT nextval('sub_location
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
+ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+
+--
+-- Name: ogc_fid; Type: DEFAULT; Schema: public; Owner: -
+--
+
 ALTER TABLE ONLY wikipedia_articles ALTER COLUMN id SET DEFAULT nextval('wikipedia_articles_id_seq'::regclass);
 
 
@@ -1253,7 +1300,15 @@ ALTER TABLE ONLY sub_locations
 
 
 --
--- Name: wikipedia_articles_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace:
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: wikipedia_articles_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace:
 --
 
 ALTER TABLE ONLY wikipedia_articles
@@ -1648,6 +1703,8 @@ INSERT INTO schema_migrations (version) VALUES ('20140721122852');
 INSERT INTO schema_migrations (version) VALUES ('20140725144859');
 
 INSERT INTO schema_migrations (version) VALUES ('20140725145539');
+
+INSERT INTO schema_migrations (version) VALUES ('20141020132951');
 
 INSERT INTO schema_migrations (version) VALUES ('20141020135233');
 
