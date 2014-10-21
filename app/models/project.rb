d2d69d77b8afda9ce294feb926b42ab6b9c985a7
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-21 13:19:40 +0100

    add PolymorphicGroup module to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
+  include PolymorphicGroup
+
   belongs_to :user
 
   has_many :project_items
@@ -6,8 +8,5 @@ class Project < ActiveRecord::Base
   has_many :protected_areas, through: :project_items, source: :item, source_type: 'ProtectedArea'
   has_many :countries, through: :project_items, source: :item, source_type: 'Country'
   has_many :regions, through: :project_items, source: :item, source_type: 'Region'
-
-  def items
-    protected_areas + countries + regions
-  end
+  polymorphic_group :items, [:protected_areas, :countries, :regions]
 end
