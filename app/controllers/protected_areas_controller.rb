644691599a2bfe16ba515cb64844bb3d18afc24e
Adam Mulligan <adammulligan@fastmail.fm>
2014-10-22 19:53:40 +0100

    Render correct 404 page

--- app/controllers/protected_areas_controller.rb
+++ app/controllers/protected_areas_controller.rb
@@ -19,6 +19,6 @@ class ProtectedAreasController < ApplicationController
   private
 
   def render_404
-    render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found
+    render :file => "#{Rails.root}/public/404.html", :layout => false, :status => :not_found
   end
 end
