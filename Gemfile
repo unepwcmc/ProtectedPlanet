83950adcf105c747b8956a1929bc3af7b216275f
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-10-23 16:30:21 +0100

    Reinstalls Devise
    Devise was removed by Andrea (accidentally, maybe?) due to merging a revert
    commit from master.

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ gem 'turnout', '~> 2.0.0'
 
 gem 'sinatra', '>= 1.3.0', :require => nil
 
+gem 'devise', '~> 3.4.0'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root to: 'home#index'
   get '/', to: 'home#index'
 
