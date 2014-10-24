28d884b598bbedfb10838c312390d360a9b1f482
Miguel Torres <migtorres@gmail.com>
2014-10-24 10:36:11 +0100

    adds test to api json

--- test/models/protected_area_test.rb
+++ test/models/protected_area_test.rb
@@ -105,4 +105,69 @@ class ProtectedAreaTest < ActiveSupport::TestCase
     FactoryGirl.create(:protected_area, iucn_category: no_iucn_category)
     assert_equal 2, ProtectedArea.with_valid_iucn_categories.count
   end
+
+  test '.as_api_feeder returns the PA as JSON with requested attributes' do
+
+  region = FactoryGirl.create(:region, id: 987, name: 'North Manmerica')
+  country = FactoryGirl.create(:country, id: 123, iso3: 'MB', name: 'Manboneland', region: region)
+  sub_location = FactoryGirl.create(:sub_location, english_name: 'Manboneland City')
+
+  iucn_category = FactoryGirl.create(:iucn_category, id: 456, name: 'IA')
+  designation = FactoryGirl.create(:designation, id: 654, name: 'National')
+  governance = FactoryGirl.create(:governance, id: 111, name: 'Bone Man')
+  legal_status = FactoryGirl.create(:legal_status, id: 987, name: 'Proposed')
+  jurisdiction = FactoryGirl.create(:jurisdiction, id: 2, name: 'International')
+
+
+  pa = FactoryGirl.create(:protected_area,
+    name: 'Manbone', countries: [country], sub_locations: [sub_location],
+    original_name: 'Manboné', iucn_category: iucn_category, 
+    designation: designation, governance: governance, 
+    legal_status: legal_status, jurisdiction: jurisdiction, 
+    legal_status_updated_at: '2007-01-01', marine: true, wdpa_id: 555999, 
+    reported_area: 10.2)
+
+    expected_json = {
+      "id" => pa.id,
+      "wdpa_id" => 555999,
+      "name" => 'Manbone',
+      "original_name" => "Manboné",
+      "marine" => true,
+      "sub_locations" => [
+        {
+          "english_name" => "Manboneland City"
+        }
+      ],
+      "countries_for_index" => [
+        {
+          "id" => 123,
+          "name" => "Manboneland",
+          "iso3" => "MB",
+          "region_for_index" => {
+            "id" => 987,
+            "name" => "North Manmerica"
+          }
+        }
+      ],
+      "iucn_category" => {
+        "id" => 456,
+        "name" => "IA"
+      },
+      "designation" => {
+        "id" => 654,
+        "name" => "National"
+      },
+      "legal_status" => {
+        "id" => 987,
+        "name" => "Proposed"
+      },
+      "jurisdiction" => {
+        "id" => 2,
+        "name" => "International"
+      },
+      "legal_status_updated_at" => "2007-01-01"
+    }
+
+    assert_equal expected_json, pa.as_api_feeder
+  end
 end
