29a4a3484a9916d0b798715cd2dd98456d078d28
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-27 10:00:20 +0000

    add wdpa_ids for saved_searches

--- test/unit/workers/project_downloads_generator_test.rb
+++ test/unit/workers/project_downloads_generator_test.rb
@@ -4,8 +4,13 @@ class ProjectDownloadsGeneratorWorkerTest < ActiveSupport::TestCase
     pa = FactoryGirl.create(:protected_area)
     country = FactoryGirl.create(:country)
     region = FactoryGirl.create(:region)
+    saved_search = FactoryGirl.create(:saved_search, results_ids: ["1", "2"])
+
     project = FactoryGirl.create(:project,
-      protected_areas: [pa], countries: [country], regions: [region]
+      protected_areas: [pa],
+      countries: [country],
+      regions: [region],
+      saved_searches: [saved_search]
     )
 
     wdpa_ids = project.items.flat_map(&:wdpa_ids).uniq
