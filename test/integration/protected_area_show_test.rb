5e38089cad1ad8b9da7ce523e8607f5f969f020d
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-10-28 14:39:30 +0000

    remove unneded test for PAs and wikipedia summaries

--- test/integration/protected_area_show_test.rb
+++ test/integration/protected_area_show_test.rb
@@ -19,20 +19,6 @@ class ProtectedAreaShowTest < ActionDispatch::IntegrationTest
     assert_match /Killbear/, @response.body
   end
 
-  test 'renders the Wikipedia summary' do
-    wikipedia_article = FactoryGirl.create(
-      :wikipedia_article,
-      summary: 'Summary text',
-      image_url: 'http://url.com/image.jpg',
-      url: 'http://url.com/article',
-      protected_area: @protected_area
-    )
-
-    get "/#{@protected_area.slug}"
-    assert_match Regexp.new(wikipedia_article.summary), @response.body
-    assert_match Regexp.new(wikipedia_article.url), @response.body
-  end
-
   test 'renders the Images for the Protected Area' do
     image = FactoryGirl.create(:image, url: 'http://images.com/image.jpg')
     @protected_area = FactoryGirl.create(
