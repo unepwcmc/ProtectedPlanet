a11f934c6fff1e424273c79a3b93137b4942d434
Adam Mulligan <adammulligan@fastmail.fm>
2014-10-30 16:15:01 +0000

    Fix tests for search

--- test/unit/search/filter_test.rb
+++ test/unit/search/filter_test.rb
@@ -6,17 +6,23 @@ class SearchFilterTest < ActiveSupport::TestCase
     filters = Search::Filter.from_params({region: 3})
 
     expected_filters = [{
-      "nested" => {
-        "path" => "countries_for_index.region_for_index",
-        "filter" => {
-           "bool" => {
-              "must" => {
-                "term" => {
-                  "countries_for_index.region_for_index.id" => 3
+      "bool"=>{
+        "should"=>[
+          {
+            "nested"=>{
+              "path"=>"countries_for_index.region_for_index",
+              "filter"=>{
+                "bool"=>{
+                  "must"=>{
+                    "term"=>{
+                      "countries_for_index.region_for_index.id"=>3
+                    }
+                  }
                 }
               }
             }
-        }
+          }
+        ]
       }
     }]
 
@@ -26,12 +32,7 @@ class SearchFilterTest < ActiveSupport::TestCase
   test '#from_params, given a type Filter, returns the filter query as a
    hash' do
     filter = Search::Filter.from_params({type: 'country'})
-
-    expected_filters = [{
-      "type" => {
-        "value" => 'country'
-      }
-    }]
+    expected_filters = [{"bool"=>{"should"=>[{"type"=>{"value"=>"country"}}]}}]
 
     assert_equal expected_filters, filter
   end
@@ -39,12 +40,7 @@ class SearchFilterTest < ActiveSupport::TestCase
   test '#from_params, given a hash of search params, returns the filter query
    as a hash' do
     filters = Search::Filter.from_params(type: 'protected_area')
-
-    expected_filters = [{
-      "type" => {
-        "value" => 'protected_area'
-      }
-    }]
+    expected_filters = [{"bool"=>{"should"=>[{"type"=>{"value"=>"protected_area"}}]}}]
 
     assert_equal expected_filters, filters
   end
@@ -54,17 +50,23 @@ class SearchFilterTest < ActiveSupport::TestCase
     filters = Search::Filter.from_params(region: '3')
 
     expected_filters = [{
-      "nested" => {
-        "path" => "countries_for_index.region_for_index",
-        "filter" => {
-           "bool" => {
-              "must" => {
-                "term" => {
-                  "countries_for_index.region_for_index.id" => 3
+      "bool"=>{
+        "should"=>[
+          {
+            "nested"=>{
+              "path"=>"countries_for_index.region_for_index",
+              "filter"=>{
+                "bool"=>{
+                  "must"=>{
+                    "term"=>{
+                      "countries_for_index.region_for_index.id"=>3
+                    }
+                  }
                 }
               }
             }
-        }
+          }
+        ]
       }
     }]
 
@@ -75,12 +77,18 @@ class SearchFilterTest < ActiveSupport::TestCase
     filters = Search::Filter.from_params(location: [1,2])
 
     expected_filters = [{
-      "geo_distance" => {
-        "distance" => "2000km",
-        "protected_area.coordinates" => {
-          "lat" => 1,
-          "lon" => 2
-        }
+      "bool"=>{
+        "should"=>[
+          {
+            "geo_distance"=>{
+              "distance"=>"2000km",
+              "protected_area.coordinates"=>{
+                "lat"=>1,
+                "lon"=>2
+              }
+            }
+          }
+        ]
       }
     }]
 
@@ -94,33 +102,39 @@ class SearchFilterTest < ActiveSupport::TestCase
     )
 
     expected_filters = [{
-      "nested" => {
-        "path" => "iucn_category",
-        "filter" => {
-         "bool" => {
-            "must" => {
-              "term" => {
-                "iucn_category.id" => 1
+      "bool"=>{
+        "should"=>[
+          {
+            "nested"=>{
+              "path"=>"iucn_category",
+              "filter"=>{
+                "bool"=>{
+                  "must"=>{
+                    "term"=>{
+                      "iucn_category.id"=>1
+                    }
+                  }
+                }
+              }
+            }
+          },
+          {
+            "nested"=>{
+              "path"=>"iucn_category",
+              "filter"=>{
+                "bool"=>{
+                  "must"=>{
+                    "term"=>{
+                      "iucn_category.id"=>2
+                    }
+                  }
+                }
               }
             }
           }
-        }
-      }
-    }, {
-      "nested" => {
-        "path" => "iucn_category",
-        "filter" => {
-         "bool" => {
-            "must" => {
-              "term" => {
-                "iucn_category.id" => 2
-              }
-            }
-          }
-        }
+        ]
       }
     }]
-
 
     assert_equal expected_filters, filters
   end
