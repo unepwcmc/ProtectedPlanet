60960d89346049cca1af76745c577b7e0ede1b30
Adam Mulligan <adammulligan@fastmail.fm>
2014-10-31 08:19:32 +0000

    Removes old data from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,5 @@ class HomeController < ApplicationController
   after_filter :enable_caching
 
   def index
-    @number_of_pas = ProtectedArea.count
-    @number_of_designations = Designation.count
-    @pas_with_iucn_category = ProtectedArea.with_valid_iucn_categories.count
-    @countries_providing_data = Country.data_providers.count
   end
 end
