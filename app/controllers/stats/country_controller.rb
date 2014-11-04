d27f600b5b4066c20890c2d2cc1657368c979e2d
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-11-04 16:17:41 +0000

    Added basic countries comparison

--- app/controllers/stats/country_controller.rb
+++ app/controllers/stats/country_controller.rb
@@ -6,7 +6,18 @@ class Stats::CountryController < ApplicationController
     @number_of_pas = @country.protected_areas.count
     @pas_with_iucn_category = @country.protected_areas_with_iucn_categories.count
     @number_of_designations = @country.designations.count
+  end
 
-    @focus = 'country'
+  def new_comparison
+    @country = Country.where(iso: params[:iso]).first
+    @presenter = StatisticPresenter.new @country
+  end
+
+  def compare
+    @country = Country.where(iso: params[:iso]).first
+    @presenter = StatisticPresenter.new @country
+
+    @comparison_country = Country.where(iso: params[:compare_iso]).first
+    @comparison_presenter = StatisticPresenter.new @comparison_country
   end
 end
