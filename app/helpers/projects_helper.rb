72a51e45bef02380731d09c33368cc06fcba838c
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-11-07 14:01:43 +0000

    Added paths to projects with two helper methods

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -9,4 +9,19 @@ module ProjectsHelper
       classname
     end
   end
+
+  def show_item_path item
+    case item.class.name
+    when 'SavedSearch'
+      search_path(q: item.name)
+    when 'Country'
+      country_stats_path(item)
+    when 'Region'
+      regional_stats_path(item)
+    when 'ProtectedArea'
+      protected_area_path(item)
+    else
+      root_path
+    end
+  end  
 end
