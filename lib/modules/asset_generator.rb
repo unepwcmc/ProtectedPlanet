7f0857875be93037412053c7cb9c1d1d7bf74bf9
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-11-07 15:28:57 +0000

    don't show pa images anymore

--- lib/modules/asset_generator.rb
+++ lib/modules/asset_generator.rb
@@ -2,21 +2,12 @@ module AssetGenerator
   class AssetGenerationFailedError < StandardError; end;
 
   def self.protected_area_tile protected_area, opts={size: {x: 128, y: 256}}
-    tile_url = if protected_area.images.any?
-      panoramio_url protected_area.images.first.url
-    else
-      mapbox_url protected_area.geojson, opts[:size]
-    end
+    tile_url = mapbox_url protected_area.geojson, opts[:size]
 
     request_tile tile_url[:host], tile_url[:path]
   end
 
   private
-
-  def self.panoramio_url url
-    uri = URI(url)
-    {host: uri.host, path: uri.path}
-  end
 
   def self.mapbox_url geojson, size
     access_token = Rails.application.secrets.mapbox['access_token']
