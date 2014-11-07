65bed700d13c0265a1ab570134c95005d7a1efbd
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-11-07 13:50:43 +0000

    Added new assets, updated PA show page and menu, updated project page with styling fixes, added delete projects ability

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,12 @@
 module ProjectsHelper
+  def class_to_human classname
+    case classname.name
+    when 'SavedSearch'
+      'Search'
+    when 'ProtectedArea'
+      'Protected Area'
+    else
+      classname
+    end
+  end
 end
