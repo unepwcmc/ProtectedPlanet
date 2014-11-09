96eff0dda5a87975f685dfcedde26dce248c8784
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-11-09 05:24:17 +0000

    adapt Project and ProjectDownloadGenerator to send updates

--- app/workers/project_downloads_generator.rb
+++ app/workers/project_downloads_generator.rb
@@ -6,6 +6,23 @@ class ProjectDownloadsGenerator
     project = Project.find project_id
     wdpa_ids = Set.new project.items.flat_map(&:wdpa_ids)
 
-    Download.generate "project_#{project.id}_all", wdpa_ids.to_a
+    while_generating project.id do
+      Download.generate "project_#{project.id}_all", wdpa_ids.to_a
+      links id
+    end
+  end
+
+  private
+
+  def while_generating id
+    $redis.set("projects:#{id}:all", 'generating')
+    $redis.set("projects:#{id}:all", yield)
+  end
+
+
+  def links id
+    ['csv', 'shp', 'kml'].each_with_object({}) do |type, hash|
+      hash[type] = Download.link_to "project_#{id}_all", type
+    end.to_json
   end
 end
