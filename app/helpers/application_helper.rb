d503fa09a52ffcae18cd77b933678a55e336cfa8
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-11-09 05:23:18 +0000

    better cover helpers, now in application_helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,35 @@
 module ApplicationHelper
+  COVER_HELPERS = {
+    ProtectedArea => :protected_area_cover,
+    Country => :country_cover,
+    Region => :region_cover,
+    SavedSearch => :saved_search_cover
+  }
+
   def commaify number
     number_with_delimiter(number, delimeter: ',')
+  end
+
+  def cover item
+    send(COVER_HELPERS[item.class], item)
+  end
+
+  def protected_area_cover protected_area
+    version = Rails.application.secrets.mapbox['version']
+    image_params = {id: protected_area.id, version: version, size: {x: 256, y: 128}}
+
+    image_tag(tiles_path(image_params), alt: protected_area.name)
+  end
+
+  def country_cover country
+    image_tag("search-placeholder-country.png", alt: country.name)
+  end
+
+  def region_cover region
+    image_tag("search-placeholder-region.png", alt: region.name)
+  end
+
+  def saved_search_cover saved_search
+    image_tag("projects-saved-searches.png", alt: saved_search.name)
   end
 end
