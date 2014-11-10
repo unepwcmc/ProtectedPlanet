144428afc8650135e965ee99eed55c204d9675b6
Adam Mulligan <adammulligan@fastmail.fm>
2014-11-10 14:08:23 +0000

    Improves appearance of Country and PA page
    sorry for that inline SQL query

--- app/controllers/stats/country_controller.rb
+++ app/controllers/stats/country_controller.rb
@@ -1,23 +1,21 @@
 class Stats::CountryController < ApplicationController
-  def show
-    @country = Country.where(iso: params[:iso]).first
-    @presenter = StatisticPresenter.new @country
+  before_filter :load_vars
 
-    @number_of_pas = @country.protected_areas.count
-    @pas_with_iucn_category = @country.protected_areas_with_iucn_categories.count
-    @number_of_designations = @country.designations.count
+  def show
   end
 
   def new_comparison
-    @country = Country.where(iso: params[:iso]).first
-    @presenter = StatisticPresenter.new @country
   end
 
   def compare
-    @country = Country.where(iso: params[:iso]).first
-    @presenter = StatisticPresenter.new @country
-
     @comparison_country = Country.where(iso: params[:compare_iso]).first
     @comparison_presenter = StatisticPresenter.new @comparison_country
+  end
+
+  private
+
+  def load_vars
+    @country = Country.where(iso: params[:iso]).first
+    @presenter = StatisticPresenter.new @country
   end
 end
