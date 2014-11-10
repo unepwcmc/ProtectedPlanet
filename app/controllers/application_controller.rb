6d1d1370dfe7f94e9a4d5b5248a83a85de462631
Adam Mulligan <adammulligan@fastmail.fm>
2014-11-10 14:14:58 +0000

    Redirect to previous page on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,34 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :store_location
+
   def enable_caching
     expires_in Rails.application.secrets.cache_max_age, public: true
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+  private
+
+  NO_REDIRECT = [
+    "/users/sign_in",
+    "/users/sign_up",
+    "/users/password/new",
+    "/users/password/edit",
+    "/users/confirmation",
+    "/users/sign_out"
+  ]
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get?
+
+    if (!NO_REDIRECT.include?(request.path) && !request.xhr?)
+      puts "STORING LOCATION"
+      session[:previous_url] = request.fullpath
+    end
   end
 end
