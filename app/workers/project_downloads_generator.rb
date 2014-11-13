8a885cdc0d7a7d7a8eda05b867682d872babf872
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-11-13 01:58:49 +0100

    fix links in downloads email

--- app/workers/project_downloads_generator.rb
+++ app/workers/project_downloads_generator.rb
@@ -3,26 +3,30 @@ class ProjectDownloadsGenerator
   sidekiq_options retry: false
 
   def perform project_id
-    project = Project.find project_id
-    wdpa_ids = Set.new project.items.flat_map(&:wdpa_ids)
+    @project = Project.find project_id
+    wdpa_ids = Set.new @project.items.flat_map(&:wdpa_ids)
 
-    while_generating project.id do
-      Download.generate "project_#{project.id}_all", {wdpa_ids: wdpa_ids.to_a}
-      {status: 'completed', links: links(project.id)}.to_json
+    while_generating @project.id do
+      Download.generate "project_#{@project.id}_all", {wdpa_ids: wdpa_ids.to_a}
+      {status: 'completed', links: links(@project.id)}.to_json
     end
   end
 
   private
 
-  def while_generating id
-    $redis.set("projects:#{id}:all", {status: 'generating'}.to_json)
-    $redis.set("projects:#{id}:all", yield)
+  def filename
+    "project_#{@project.id}_all"
+  end
+
+  def while_generating
+    $redis.set("projects:#{@project.id}:all", {status: 'generating'}.to_json)
+    $redis.set("projects:#{@project.id}:all", yield)
   end
 
 
   def links id
     ['csv', 'shp', 'kml'].each_with_object({}) do |type, hash|
-      hash[type] = Download.link_to "project_#{id}_all", type
+      hash[type] = Download.link_to filename, type
     end.to_json
   end
 end
