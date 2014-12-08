9a2b062086a35c39cd45746d47d726a87ba32901
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-12-08 18:35:45 +0000

    update views and controllers with new structure

--- test/integration/api/search_test.rb
+++ test/integration/api/search_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class SearchTest < ActionDispatch::IntegrationTest
   test 'GET /search/points?q=manbone returns an array of lat/lng points
    for the results of the given search' do
-    expected_attributes = {
-      "id" => 1234567,
-      "name" => 'Manbone National Swimming Pool',
-      "wdpa_id" => 55512345,
-      "the_geom_latitude" => '0',
-      "the_geom_longitude" => '-1'
-    }
-
-    pa = FactoryGirl.create(:protected_area, expected_attributes)
-
     results_object = {
       "hits" => {
         "hits" => [{
           "_type" => "protected_area",
           "_source" => {
-            "id" => pa.id
+            "id" => 123,
+            "name" => 'Manbone National Swimming Pool',
+            "wdpa_id" => 55512345,
+            "coordinates" => ['0', '-1']
           }
         }]
       }
+    }
+
+    expected_attributes = {
+      "id" => 123,
+      "name" => 'Manbone National Swimming Pool',
+      "wdpa_id" => 55512345,
+      "coordinates" => ['0', '-1']
     }
 
     search_mock = mock().tap { |m| m.stubs(:search).returns(results_object) }
