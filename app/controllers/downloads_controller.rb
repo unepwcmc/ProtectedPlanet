62e6046c6cfca899d4d9df9844992af2eacec047
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-12-08 03:54:38 +0000

    use new structure in controllers

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,52 +1,24 @@
 class DownloadsController < ApplicationController
   def show
-    type = params[:type]
-
-    json = if params[:domain] == 'general'
-      country_iso_3 = params[:id]
-      {link: Download.link_to(country_iso_3, type)}
-    elsif params[:domain] == 'project'
-      project = Project.find(params[:id])
-      project.download_info
-    end
-
-    render json: json
+    redirect_to Download.link_to(params['id'], params['type'])
   end
 
   def create
-    search = Search.download(params[:q], {filters: filters})
-    set_email(search)
-
-    render json: {token: search.token}
-  end
-
-  def update
-    search = Search.find(params[:id])
-    set_email(search)
-
-    render(search ? {json: search.properties} : {status: 404})
+    render json: Download.request(params)
   end
 
   def poll
-    if params[:domain] == 'search'
-      search = Search.find(params[:token])
-      render(search ? {json: search.properties} : {status: 404})
-    elsif params[:domain] == 'project'
-      project = Project.find(params[:token])
-      render(project ? {json: project.download_info} : {status: 404})
-    end
+    render json: Download.poll(params)
+  end
+
+  def update
+    params.merge!({'email' => user_email})
+    render status: 200, json: Download.set_email(params)
   end
 
   private
 
-  def set_email search
-    email = params[:email] || current_user.try(:email)
-    if email.present?
-      search.properties['user_email'] = email
-    end
-  end
-
-  def filters
-    params.stringify_keys.slice(*Search::ALLOWED_FILTERS)
+  def user_email
+    params[:email] || current_user.try(:email)
   end
 end
