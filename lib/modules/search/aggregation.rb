9ab7bfc53ddf712c5fc8dac2ec73f0fcaf64ea4e
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2014-12-16 15:50:26 +0000

    add parse method to aggregation.rb

--- lib/modules/search/aggregation.rb
+++ lib/modules/search/aggregation.rb
@@ -1,8 +1,36 @@
 class Search::Aggregation
+  AGGREGATORS = {
+    'boolean' => Search::Aggregators::Boolean,
+    'model'   => Search::Aggregators::Model
+  }
+
   TEMPLATE_DIRECTORY = File.join(File.dirname(__FILE__), 'templates')
   TEMPLATE = File.read(File.join(TEMPLATE_DIRECTORY, 'aggregations.json'))
 
+  def self.parse raw_aggregations
+    {}.tap do |aggregations|
+      raw_aggregations.each do |name, hash|
+        aggregator = aggregator_for(name)
+        config = configuration[name]
+
+        aggregations[name] = aggregator.build(name, hash, config)
+      end
+    end
+  end
+
   def self.all
-    JSON.parse(TEMPLATE)
+    @@json ||= JSON.parse(TEMPLATE)
+  end
+
+  def self.configuration
+    Search.configuration['aggregations']
+  end
+
+  def self.aggregator_for name
+    AGGREGATORS[configuration[name]['type']]
+  end
+
+  def self.configuration_for name
+    configuration[name]
   end
 end
