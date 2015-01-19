fb41e2c75566a8d6d915a139147d8722cf0502f5
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-01-19 14:17:09 +0000

    remove generation of downloads for regions

--- lib/modules/wdpa/download_generator.rb
+++ lib/modules/wdpa/download_generator.rb
@@ -5,9 +5,5 @@ class Wdpa::DownloadGenerator
     Country.pluck(:iso_3).each do |country_iso_3|
       DownloadWorkers::General.perform_async :country, country_iso_3, for_import: true
     end
-
-    Region.pluck(:iso).each do |region_iso|
-      DownloadWorkers::General.perform_async :region, region_iso, for_import: true
-    end
   end
 end
