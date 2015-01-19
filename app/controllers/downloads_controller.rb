5e5f3dd58da4c84982c3e4c00319805570a13802
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-01-19 14:16:23 +0000

    skip authentication token verification for downloads_controller

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,4 +1,6 @@
 class DownloadsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def show
     redirect_to Download.link_to(params['id'], params['type'])
   end
