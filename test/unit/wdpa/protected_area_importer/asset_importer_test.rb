869780ef9c241ee2740fab9b5d9e4465fd8de85d
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-05-28 15:15:02 +0100

    remove ImageWorker from AssetImporter

--- test/unit/wdpa/protected_area_importer/asset_importer_test.rb
+++ test/unit/wdpa/protected_area_importer/asset_importer_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 require 'sidekiq/testing'
 
 class TestWdpaAssetImporterService < ActiveSupport::TestCase
-  test '.import, given a WDPA release, enqueues a WikipediaSummaryWorker and
-   ImageWorker job for new PAs' do
+  test '.import, given a WDPA release, enqueues a WikipediaSummaryWorker job for new PAs' do
     pa = FactoryGirl.create(:protected_area)
 
     Sidekiq::Testing.fake!
@@ -12,15 +11,10 @@ class TestWdpaAssetImporterService < ActiveSupport::TestCase
     Wdpa::ProtectedAreaImporter::AssetImporter.import
 
     assert_equal 1, ImportWorkers::WikipediaSummaryWorker.jobs.size
-    assert_equal 1, ImportWorkers::ImageWorker.jobs.size
 
     assert_equal(
       pa.id,
       ImportWorkers::WikipediaSummaryWorker.jobs.first['args'].first
-    )
-    assert_equal(
-      pa.id,
-      ImportWorkers::ImageWorker.jobs.first['args'].first
     )
 
     Sidekiq::Worker.clear_all
