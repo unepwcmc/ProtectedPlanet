987f9f21c1ba4a1c9fddbca763f6744cc7041ace
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-06-08 10:57:31 +0100

    add Download::Queries module

---
+++ lib/modules/download/queries.rb
@@ -0,0 +1,49 @@
+module Download
+  module Queries
+    POINTS_COLUMNS = [
+      :wkb_geometry, :wdpaid, :wdpa_pid,
+      :pa_def, :name, :orig_name,
+      :desig, :desig_eng, :desig_type,
+      :iucn_cat, :int_crit, :marine,
+      :rep_m_area, :rep_area, :no_take,
+      :no_tk_area, :status, :status_yr,
+      :gov_type, :own_type, :mang_auth,
+      :mang_plan, :verif, :metadataid,
+      :sub_loc, :parent_iso3, :iso3
+    ]
+
+    POLYGONS_COLUMNS = POINTS_COLUMNS.clone
+      .insert(13, :gis_m_area).insert(15, :gis_area)
+
+    def self.for_points extra_columns={}
+      aliased_columns = POINTS_COLUMNS.map { |column|
+        %{#{column} AS "#{column.upcase}"}
+      }
+
+      extra_columns.each { |(position, name)|
+        aliased_columns.insert(position, name)
+      }
+
+      {select: "#{aliased_columns.join(',')}", from: 'standard_points'}
+    end
+
+    def self.for_polygons
+      aliased_columns = POLYGONS_COLUMNS.map { |column|
+        %{#{column} AS "#{column.upcase}"}
+      }.join(',')
+
+      {select: "#{aliased_columns}", from: 'standard_polygons', where: ''}
+    end
+
+    def self.mixed
+      points = for_points({13 => %{'' AS "GIS_M_AREA"}, 15 => %{'' AS "GIS_AREA"}})
+      """
+        SELECT 'Polygon' as TYPE, #{for_polygons[:select]}
+        FROM #{for_polygons[:from]}
+        UNION ALL
+        SELECT 'Point' as TYPE, #{points[:select]}
+        FROM #{points[:from]}
+      """.squish
+    end
+  end
+end
