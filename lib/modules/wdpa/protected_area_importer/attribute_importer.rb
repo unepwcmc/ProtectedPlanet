64f5969019b7eb2f4660c9fa4eb0c61fe44e5115
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-07-05 19:07:18 +0100

    remove check for existing PA during import

--- lib/modules/wdpa/protected_area_importer/attribute_importer.rb
+++ lib/modules/wdpa/protected_area_importer/attribute_importer.rb
@@ -1,17 +1,16 @@
 class Wdpa::ProtectedAreaImporter::AttributeImporter
   def self.import wdpa_release
-    protected_areas = wdpa_release.protected_areas.map(&:symbolize_keys)
+    protected_areas = wdpa_release.protected_areas
 
     protected_areas.each do |protected_area_attributes|
-      unless protected_area_exists(protected_area_attributes[:wdpaid])
-        protected_area_attributes = remove_geometry protected_area_attributes
-        standardised_attributes = Wdpa::DataStandard.attributes_from_standards_hash(
-          protected_area_attributes
-        )
+      protected_area_attributes = protected_area_attributes.symbolize_keys
 
-        protected_area = ProtectedArea.create(standardised_attributes)
-        return false unless protected_area
-      end
+      protected_area_attributes = remove_geometry protected_area_attributes
+      standardised_attributes = Wdpa::DataStandard.attributes_from_standards_hash(
+        protected_area_attributes
+      )
+
+      ProtectedArea.create(standardised_attributes)
     end
 
     return true
@@ -23,12 +22,5 @@ class Wdpa::ProtectedAreaImporter::AttributeImporter
     attributes.select do |key, hash|
       Wdpa::DataStandard.standard_geometry_attributes[key].nil?
     end
-  end
-
-  def self.protected_area_exists wdpa_id
-    ProtectedArea.
-      select('wdpa_id').
-      where(wdpa_id: wdpa_id).
-      count > 0
   end
 end
--- test/unit/wdpa/protected_area_importer/attribute_importer_test.rb
+++ test/unit/wdpa/protected_area_importer/attribute_importer_test.rb
@@ -23,24 +23,6 @@ class TestWdpaAttributeImporterService < ActiveSupport::TestCase
     assert_not_nil ProtectedArea.where(wdpa_id: 4321).first
   end
 
-  test '.import does not standardise the attributes if the PA already
-   exists' do
-    pa = FactoryGirl.create(:protected_area, wdpa_id: 8765)
-    pa_attributes = [{
-      wdpaid: pa.wdpa_id
-    }]
-
-    wdpa_release = Wdpa::Release.new
-    wdpa_release.expects(:protected_areas).returns(pa_attributes)
-
-    Wdpa::DataStandard.expects(:attributes_from_standards_hash).never
-
-    imported = Wdpa::ProtectedAreaImporter::AttributeImporter.import(wdpa_release)
-
-    assert imported, "Expected importer to return true on success"
-    assert_equal 1, ProtectedArea.count
-  end
-
   test '.import works with stringified keys' do
     pa_attributes = [{
       "wdpaid" => 1234,
