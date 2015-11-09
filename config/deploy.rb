a0b817f57b1c5ad5203072bab5aa6e5a59b35300
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2015-11-09 17:23:59 +0000

    add bower:install rake task during capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -101,5 +101,16 @@ namespace :maintenance do
       end
     end
   end
-
 end
+
+namespace :bower do
+  desc 'Install bower packages'
+  task :install do
+    on roles(:web) do
+      within release_path do
+        execute :rake, 'bower:install CI=true'
+      end
+    end
+  end
+end
+before 'deploy:compile_assets', 'bower:install'
