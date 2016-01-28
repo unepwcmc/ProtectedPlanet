d0fe41e661df22e604fc50c2305f49831fed77bc
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-01-28 15:36:48 +0000

    add deploy task to git clone submodules

--- config/deploy.rb
+++ config/deploy.rb
@@ -114,3 +114,16 @@ namespace :bower do
   end
 end
 before 'deploy:compile_assets', 'bower:install'
+
+namespace :git do
+  desc 'Copy repo to releases'
+  task create_release: :'git:update' do
+    on roles(:all) do
+      with fetch(:git_environmental_variables) do
+        within repo_path do
+          execute :git, :clone, '-b', fetch(:branch), '--recursive', '.', release_path
+        end
+      end
+    end
+  end
+end
