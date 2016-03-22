519841d8646f2ca86d085c119ff1c7d9fa935ca7
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-22 11:51:17 +0000

    import protected areas in bulks

--- lib/modules/wdpa/protected_area_importer/attribute_importer.rb
+++ lib/modules/wdpa/protected_area_importer/attribute_importer.rb
@@ -1,21 +1,19 @@
 class Wdpa::ProtectedAreaImporter::AttributeImporter
   def self.import wdpa_release
-    imported_wdpa_ids = {}
-    protected_areas = wdpa_release.protected_areas
+    wdpa_release.protected_areas_in_bulk(100) do |protected_areas|
+      protected_areas.each do |protected_area_attributes|
+        protected_area_attributes = protected_area_attributes.symbolize_keys
 
-    protected_areas.each do |protected_area_attributes|
-      protected_area_attributes = protected_area_attributes.symbolize_keys
+        protected_area_attributes = remove_geometry protected_area_attributes
+        standardised_attributes = Wdpa::DataStandard.attributes_from_standards_hash(
+          protected_area_attributes
+        )
 
-      protected_area_attributes = remove_geometry protected_area_attributes
-      standardised_attributes = Wdpa::DataStandard.attributes_from_standards_hash(
-        protected_area_attributes
-      )
-
-      next if standardised_attributes.nil?
-      next if imported_wdpa_ids[standardised_attributes[:wdpa_id]]
-
-      imported_wdpa_ids[standardised_attributes[:wdpa_id]] = true
-      ProtectedArea.create(standardised_attributes)
+        next if standardised_attributes.nil?
+        unless ProtectedArea.create(standardised_attributes)
+          Bystander.log("Protected Area with WDPAID = #{standardised_attributes[:wdpa_id]} couldn't be imported")
+        end
+      end
     end
 
     return true
--- test/unit/wdpa/protected_area_importer/attribute_importer_test.rb
+++ test/unit/wdpa/protected_area_importer/attribute_importer_test.rb
@@ -12,7 +12,7 @@ class TestWdpaAttributeImporterService < ActiveSupport::TestCase
     }]
 
     wdpa_release = Wdpa::Release.new
-    wdpa_release.expects(:protected_areas).returns(pa_attributes)
+    wdpa_release.expects(:protected_areas_in_bulk).yields(pa_attributes)
 
     imported = Wdpa::ProtectedAreaImporter::AttributeImporter.import(wdpa_release)
 
@@ -21,26 +21,6 @@ class TestWdpaAttributeImporterService < ActiveSupport::TestCase
 
     assert_not_nil ProtectedArea.where(wdpa_id: 1234).first
     assert_not_nil ProtectedArea.where(wdpa_id: 4321).first
-  end
-
-  test '.import does not standardise the attributes if the PA already
-   exists' do
-    pa_attributes = [{
-      wdpaid: 123
-    }, {
-      wdpaid: 123
-    }]
-
-    wdpa_release = Wdpa::Release.new
-    wdpa_release.expects(:protected_areas).returns(pa_attributes)
-
-    Wdpa::DataStandard.expects(:attributes_from_standards_hash).twice
-      .returns({wdpa_id: 123}, {wdpa_id: 123})
-
-    ProtectedArea.expects(:create).once
-
-    imported = Wdpa::ProtectedAreaImporter::AttributeImporter.import(wdpa_release)
-    assert imported, "Expected importer to return true on success"
   end
 
   test '.import works with stringified keys' do
@@ -53,7 +33,7 @@ class TestWdpaAttributeImporterService < ActiveSupport::TestCase
     }]
 
     wdpa_release = Wdpa::Release.new
-    wdpa_release.expects(:protected_areas).returns(pa_attributes)
+    wdpa_release.expects(:protected_areas_in_bulk).yields(pa_attributes)
 
     imported = Wdpa::ProtectedAreaImporter::AttributeImporter.import(wdpa_release)
 
@@ -77,7 +57,7 @@ class TestWdpaAttributeImporterService < ActiveSupport::TestCase
       returns({wdpa_id: 123})
 
     wdpa_release = Wdpa::Release.new
-    wdpa_release.expects(:protected_areas).returns(pa_attributes)
+    wdpa_release.expects(:protected_areas_in_bulk).yields(pa_attributes)
 
     imported = Wdpa::ProtectedAreaImporter::AttributeImporter.import(wdpa_release)
 
