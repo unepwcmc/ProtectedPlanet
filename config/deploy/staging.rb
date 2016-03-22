6b960e41888a640fdff92cedce66992c3e315da6
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-22 12:26:32 +0000

    update sidekiq template for linode

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,6 @@ server "web.pp-staging.linode.protectedplanet.net", user: 'wcmc', roles: %w{web}
 server "util.pp-staging.linode.protectedplanet.net", user: 'wcmc', roles: %w{util}
 server "db.pp-staging.linode.protectedplanet.net", user: 'wcmc', roles: %w{db}, :no_release => true
 
-
 set :application, "protectedplanet"
 set :server_name, "web.pp-staging.linode.protectedplanet.net"
 set :sudo_user, "wcmc"
