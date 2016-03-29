0758334713c9cbc9765fae1ac755d86550ea0a62
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-29 15:43:21 +0100

    remove transaction from AttributeImporter

--- lib/modules/wdpa/protected_area_importer/attribute_importer.rb
+++ lib/modules/wdpa/protected_area_importer/attribute_importer.rb
@@ -4,21 +4,19 @@ class Wdpa::ProtectedAreaImporter::AttributeImporter
 
     wdpa_release.protected_areas_in_bulk(100) do |protected_areas|
       protected_areas.each do |protected_area_attributes|
-        ActiveRecord::Base.transaction do
-          protected_area_attributes = protected_area_attributes.symbolize_keys
-          next if imported_wdpa_ids[protected_area_attributes[:wdpaid]]
+        protected_area_attributes = protected_area_attributes.symbolize_keys
+        next if imported_wdpa_ids[protected_area_attributes[:wdpaid]]
 
-          protected_area_attributes = remove_geometry protected_area_attributes
-          standardised_attributes = Wdpa::DataStandard.attributes_from_standards_hash(
-            protected_area_attributes
-          )
+        protected_area_attributes = remove_geometry protected_area_attributes
+        standardised_attributes = Wdpa::DataStandard.attributes_from_standards_hash(
+          protected_area_attributes
+        )
 
-          next if standardised_attributes.nil?
-          if ProtectedArea.create(standardised_attributes)
-            imported_wdpa_ids[protected_area_attributes[:wdpaid]] = true
-          else
-            Bystander.log("Protected Area with WDPAID = #{standardised_attributes[:wdpa_id]} couldn't be imported")
-          end
+        next if standardised_attributes.nil?
+        if ProtectedArea.create(standardised_attributes)
+          imported_wdpa_ids[protected_area_attributes[:wdpaid]] = true
+        else
+          Bystander.log("Protected Area with WDPAID = #{standardised_attributes[:wdpa_id]} couldn't be imported")
         end
       end
     end
