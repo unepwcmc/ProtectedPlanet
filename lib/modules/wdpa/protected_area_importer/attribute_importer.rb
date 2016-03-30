20080ed358c0da329ba522eeaaf5c9f09fe22069
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-03-30 13:44:16 +0100

    remove imported pas hash in attribute importer

--- lib/modules/wdpa/protected_area_importer/attribute_importer.rb
+++ lib/modules/wdpa/protected_area_importer/attribute_importer.rb
@@ -1,39 +1,39 @@
 class Wdpa::ProtectedAreaImporter::AttributeImporter
   def self.import wdpa_release
-    imported_wdpa_ids = {}
-
-    wdpa_release.protected_areas_in_bulk(500) do |protected_areas|
-      Bystander.log("Batch: [#{protected_areas.map{|pa| pa["wdpaid"] }.join(",")}] ")
-
-      protected_areas.each do |protected_area_attributes|
-        protected_area_attributes = protected_area_attributes.symbolize_keys
-        if imported_wdpa_ids[protected_area_attributes[:wdpaid]]
-          next
-        end
-
-        protected_area_attributes = remove_geometry protected_area_attributes
-        standardised_attributes = Wdpa::DataStandard.attributes_from_standards_hash(
-          protected_area_attributes
-        )
-
-        if standardised_attributes.nil?
-          Bystander.log("Protected Area with WDPAID = #{protected_area_attributes[:wdpaid]} was skipped")
-          next
-        end
-
-        if pa = ProtectedArea.create(standardised_attributes)
-          imported_wdpa_ids[protected_area_attributes[:wdpaid]] = true
-          ImportWorkers::WikipediaSummaryWorker.perform_async pa.id
-        else
-          Bystander.log("Protected Area with WDPAID = #{standardised_attributes[:wdpa_id]} couldn't be imported")
-        end
-      end
+    wdpa_release.protected_areas_in_bulk(100) do |protected_areas|
+      import_bulk(protected_areas)
     end
 
     return true
   end
 
   private
+
+  def import_bulk(protected_areas)
+    protected_areas.each do |protected_area_attributes|
+      protected_area_attributes = protected_area_attributes.symbolize_keys
+
+      protected_area_attributes = remove_geometry protected_area_attributes
+      standardised_attributes = Wdpa::DataStandard.attributes_from_standards_hash(
+        protected_area_attributes
+      )
+
+      if standardised_attributes.nil?
+        Bystander.log("Protected Area with WDPAID = #{protected_area_attributes[:wdpaid]} was skipped")
+        next
+      end
+
+      begin
+        pa = ProtectedArea.create!(standardised_attributes)
+        ImportWorkers::WikipediaSummaryWorker.perform_async pa.id
+      rescue => err
+        Bystander.log("""
+          PA with WDPAID #{protected_area_attributes[:wdpaid]} was not imported because:
+          > #{err.message}
+        """)
+      end
+    end
+  end
 
   def self.remove_geometry attributes
     attributes.select do |key, hash|
