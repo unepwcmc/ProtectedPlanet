48a745265ddabc04d41774606964a72346ac74cf
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-10-11 19:06:05 +0100

    add service worker for app install

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -29,3 +29,14 @@ function touchHoverFix() {
     par.removeChild(el);
     setTimeout(function() {par.insertBefore(el, next);}, 10);
 }
+
+// try to install a service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
