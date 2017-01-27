670ede878d28b8dfe2bb2665d6309162f16f5d21
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-01-27 13:26:11 +0000

    update search for regions page in homepage controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   after_filter :enable_caching
 
   def index
-    @regions_page = Comfy::Cms::Page.find_by_label("UNEP Regions")
+    @regions_page = Comfy::Cms::Page.find_by_slug("unep-regions")
   end
 end
