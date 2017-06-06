b885d15f09462db4cc28b9851c387f6205347361
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-06-06 14:54:54 +0100

    add Wdpa::NetworkImporter

---
+++ lib/modules/wdpa/network_importer.rb
@@ -0,0 +1,45 @@
+module Wdpa::NetworkImporter
+  TRANSBOUNDARY_SITES_CSV = "#{Rails.root}/lib/data/seeds/transboundary_sites.csv".freeze
+  extend self
+
+  def import
+    ActiveRecord::Base.transaction do
+      Network.all.each(&:destroy)
+
+      csv = CSV.read(TRANSBOUNDARY_SITES_CSV)
+      csv.shift # remove headers
+
+      networks_by_wdpa_id = {}
+      pas_cache = {}
+
+      pa_couples = csv.map { |row| [row[0], row[1]] }.map(&:sort).uniq
+
+      pa_couples.each do |wdpa1, wdpa2|
+        next if wdpa1 == wdpa2
+
+        pa1 = (pas_cache[wdpa1] ||= ProtectedArea.find_by(wdpa_id: wdpa1))
+        pa2 = (pas_cache[wdpa2] ||= ProtectedArea.find_by(wdpa_id: wdpa2))
+
+        next unless pa1 && pa2
+
+        if network = networks_by_wdpa_id[wdpa1]
+          network.protected_areas << pa2
+          networks_by_wdpa_id[wdpa2] = network
+
+        elsif network = networks_by_wdpa_id[wdpa2]
+          network.protected_areas << pa1
+          networks_by_wdpa_id[wdpa1] = network
+
+        else
+          network = Network.create(name: "Transboundary sites")
+
+          network.protected_areas << pa1
+          network.protected_areas << pa2
+
+          networks_by_wdpa_id[wdpa1] = network
+          networks_by_wdpa_id[wdpa2] = network
+        end
+      end
+    end
+  end
+end
