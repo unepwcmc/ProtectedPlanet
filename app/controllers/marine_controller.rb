bd44d248aa305406953e7a9ce066b4279e080ca4
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-08-15 12:11:01 +0100

    Add pledges section to the marine page and build responsive table with dummy data

--- app/controllers/marine_controller.rb
+++ app/controllers/marine_controller.rb
@@ -1,5 +1,13 @@
 class MarineController < ApplicationController
   
   def index
+    @designations = [
+      {
+        name: "Northern Bering Sea",
+        country: "United States of America",
+        size: "2,105,050km²",
+        date: "2016"
+      }
+    ]
   end
 end
