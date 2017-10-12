a73b004f3ef9cc3da91c1459485bf8215caf388c
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-10-12 16:24:12 +0100

    Save some indentation levels

--- lib/modules/wdpa/overseas_territories_importer.rb
+++ lib/modules/wdpa/overseas_territories_importer.rb
@@ -11,17 +11,13 @@ module Wdpa::OverseasTerritoriesImporter
         parent_country = Country.find_by_iso_3(parent_iso)
         child_isos = child_isos.split(';')
 
-        unless parent_country.nil?
-          child_isos.each do |child_iso|
-            child_country = Country.find_by_iso_3(child_iso)
+        next if parent_country.nil?
+        child_isos.each do |child_iso|
+          child_country = Country.find_by_iso_3(child_iso)
 
-            unless child_country.nil?
-              unless parent_country.children.map(&:iso_3).include?(child_iso)
-                parent_country.children << child_country
-                puts "Added #{child_country.iso_3} to parent #{parent_country.iso_3}"
-              end
-            end
-          end
+          next if child_country.nil? || parent_country.children.map(&:iso_3).include?(child_iso)
+          parent_country.children << child_country
+          puts "Added #{child_country.iso_3} to parent #{parent_country.iso_3}"
         end
       end
     end
