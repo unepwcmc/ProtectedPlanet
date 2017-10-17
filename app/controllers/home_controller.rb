aef5368f8013481527c78916d0228e8d87185e19
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-10-17 12:09:15 +0100

    Refactor thematic areas so that it is DRY and tweak layout to work in IE11

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,56 @@ class HomeController < ApplicationController
 
   def index
     @regions_page = Comfy::Cms::Page.find_by_slug("unep-regions")
+
+    @thematicAreas = [
+      {
+        title: "WDPA",
+        content: "The World Database on Protected Areas (WDPA) is the most
+                  comprehensive global database on terrestrial and marine protected
+                  areas.",
+        image: "wdpa.jpg",
+        url: Rails.root.join("/c" + @wdpa_page.full_path)
+      },
+      {
+        title: "Marine Protected Areas",
+        content: "Explore the World's marine protected areas",
+        image: "marine.png",
+        url: marine_path
+      },
+      {
+        title: "ICCAs",
+        content: "The ICCA Registry is an online platform for indigenous
+          peoples’ and community conserved territories and areas, where
+          communities themselves provide data, case studies, maps, photos
+          and stories.",
+        image: "icca.jpg",
+        url: "http://www.iccaregistry.org"
+      },
+      {
+        title: "PAME",
+        content: "PAME is a global database, comprising many thousands of
+        assessments of how well a protected area is being managed –
+        primarily the extent to which it is
+        protecting values and achieving goals and objectives.",
+        image: "pame.jpg",
+        url: Rails.root.join("/c" + @pame_page.full_path)
+      },
+      {
+        title: "PARCC",
+        content: "The PARCC project's main
+          objective was to assess the vulnerability of West African protected
+          areas to climate change and help design more resilient protected area networks.",
+        image: "parcc.jpg",
+        url: "http://parcc.protectedplanet.net/"
+      },
+      {
+        title: "Green List",
+        content: "The IUCN Green List is a new global standard for protected areas.
+          The list recognizes success in achieving conservation outcomes and
+          measures progress in effective management of protected areas.",
+        image: "green-list.jpg",
+        url: Rails.root.join("/c" + @green_list_page.full_path)
+      }
+    ]
   end
 end
