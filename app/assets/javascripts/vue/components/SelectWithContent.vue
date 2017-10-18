276fe344544045017fb3bc4d48dc3b55000f19d9
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-10-18 15:43:04 +0100

    Finish building the dropdown template and add new thumbnail size

--- app/assets/javascripts/vue/components/SelectWithContent.vue
+++ app/assets/javascripts/vue/components/SelectWithContent.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>
-  hello
+  <div class="select-with-content">
+    <select v-model="selected">
+      <option :value="null">Select...</option>
+      <option v-for="option in options" :value="option">{{ option.title }}</option>
+    </select>
 
-  {{ options }}
+    <div v-if="selected" class="select-with-content__content">
+      <img v-if="selected.image" :src="selected.image" :alt="'Visual results for ' + selected.title">
+
+      <div v-if="selected.text" v-html="selected.text" class="select-with-content__text"></div>
+
+      <a v-if="selected.url" :href="selected.url" title="View the protected area" class="button--grey">View the protected area</a>
+    </div>
   </div>
 </template>
 
@@ -16,8 +25,10 @@
       }
     },
 
-    methods: {
-
+    data: function() {
+      return {
+        selected: null
+      }
     }
   }
 </script>
