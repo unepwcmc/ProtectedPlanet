569b17dabcfa76b8be91e112f370e7a67e303c68
Andrew Potter <hello@drewpotter.com>
2017-11-14 14:05:01 +0000

    Fixed test by adjusting json

--- test/models/country_test.rb
+++ test/models/country_test.rb
@@ -152,11 +152,13 @@ class CountryTest < ActiveSupport::TestCase
     expected_groups = [{
       'governance_id' => governance_1.id.to_s,
       'governance_name' => governance_1.name,
+      'governance_type' => nil,
       'count' => '2',
       'percentage' => '40.00'
     }, {
       'governance_id' => governance_2.id.to_s,
       'governance_name' => governance_2.name,
+      'governance_type' => nil,
       'count' => '3',
       'percentage' => '60.00'
     }]
