234ccbad08db80ed84a445019b07970be143f4f4
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-11-17 13:22:33 +0000

    Add temp sharing icons to test on staging

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,6 +55,14 @@ module ApplicationHelper
     end
   end
 
+  def url_encode (text)
+    ERB::Util.url_encode(text)
+  end
+
+  def encoded_page_url
+    url_encode(request.original_url)
+  end
+
   DEFAULT_SEO_DESC = """
     Protected Planet is the online interface for the
     World Database on Protected Areas (WDPA), and the most comprehensive
