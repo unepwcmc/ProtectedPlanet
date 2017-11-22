69fe7ac03596c53071510392e3979d1bc984d8cb
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-11-22 14:40:23 +0000

    Complete functionality for the quote sharing tool

--- app/assets/javascripts/vue/components/TwitterShare.vue
+++ app/assets/javascripts/vue/components/TwitterShare.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    TWitter Share
+  <div class="twitter-share" :style="styleObject" :class="{ 'twitter-share--active' : isActive }">
+    <span class="twitter-share__title">Share quote via</span>
+    <a 
+      title="Share highlighted quote on Twitter"
+      target="_blank"
+      :href="twitterUrl"
+      class="social--share social--twitter">
+    </a>
+
+    <a 
+      title="Share highlighted quote by Email"
+      target="_self"
+      :href="emailUrl"
+      class="social--share social--email">
+    </a>
   </div>
 </template>
 
@@ -10,20 +23,86 @@
 
     data: function () {
       return {
-        watchingMouseUp: false
+        charLimit: 140,
+        watchingMouseUp: false,
+        quote: '',
+        url: '',
+        isActive: false,
+        styleObject: {
+          top: 0,
+          left: 0,
+        }
       }
     },
 
     mounted: function () {
-      document.addEventListener("selectionchange", function(e) {
-        if (!this.watchingMouseUp) {
-          this.watchingMouseUp = true
-          document.addEventListener("mouseup", function(e) {
-            console.log('mouseup', window.getSelection().toString())
-            this.watchingMouseUp = false
-          }, false)
+      this.getPageUrl()
+      this.addListeners()
+    },
+
+    computed: {
+      twitterUrl: function () {
+        const quoteLimit = this.charLimit - 6 // the 6 is for "" ... and a space 
+        let quote = this.quote
+
+        if (quote.length > quoteLimit) {
+          quote = '"' + quote.substring(0, quoteLimit) + '..." '
         }
-      }, false)
-    }
+
+        return 'https://twitter.com/intent/tweet/?text=' + quote + '&amp;url=' + this.url 
+      },
+
+      emailUrl: function () {
+        return 'mailto:?subject=Marine%20Protected%20Planet%20website&amp;body=' + this.quote + '%0D%0A%0D%0A' + this.url
+      }
+    },
+
+    methods: {
+      getPageUrl: function () {
+        const href = window.location.href
+
+        this.url = href.split('#')[0]
+      },
+
+      addListeners: function () {
+        const that = this
+
+        // listen for text being selected
+        document.addEventListener("selectionchange", function() {
+
+          // add flag to make sure that the mouse up listener is only added once 
+          // whilst the user is highlighting text
+          if (!that.watchingMouseUp) {
+            that.watchingMouseUp = true
+            
+            // listen for when the user stops highlighting text
+            document.addEventListener("mouseup", function(e) {
+
+              // get selected text
+              const selected = window.getSelection().toString()
+
+              // remove flag
+              that.watchingMouseUp = false
+
+              // only update the quote and show the share box if the selected text isn't blank
+              // otherwise hide the twitter share pop up
+              if (selected.length > 0) {
+                const textPosition = window.getSelection().getRangeAt(0).getBoundingClientRect()
+                const top = window.pageYOffset + textPosition.top - 5
+                const left = textPosition.left + ( .5 * textPosition.width)
+
+                that.isActive = true
+                that.styleObject.top = top + 'px'
+                that.styleObject.left = left + 'px'
+                that.quote = selected  
+
+              } else {
+                that.isActive = false
+              }
+            }, { once: true })
+          }
+        }, false)
+      }
+    } 
   }
 </script>
