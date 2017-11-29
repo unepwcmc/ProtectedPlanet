7ed6d656f0516bfbed8d1c5fcb25c26267498ca9
Andrew Potter <hello@drewpotter.com>
2017-11-29 16:03:43 +0000

    Added some tests for the marine stats importer

--- lib/modules/wdpa/marine_stats_importer.rb
+++ lib/modules/wdpa/marine_stats_importer.rb
@@ -6,10 +6,13 @@ module Wdpa::MarineStatsImporter
   end
 
   def self.import_stats path
+    stats = {}
     CSV.foreach(path, headers: true) do |row|
       value = row["value"].tr(',', '')
+      stats[row["type"]] = value
       $redis.hmset('wdpa_marine_stats', row["type"], value)
     end
+    stats
   end
 
   def self.stats_csv_path
