a5591380af2f1e3872d0760bce292f64045fb155
Andrew Potter <hello@drewpotter.com>
2017-12-21 16:03:30 +0000

    Merged the iucn_categories partial

--- app/models/region.rb
+++ app/models/region.rb
@@ -53,6 +53,32 @@ class Region < ActiveRecord::Base
     }
   end
 
+  def protected_areas_per_iucn_category
+    region_data = Hash.new { |hash, key| hash[key] = Hash.new }
+    processed_data = []
+    total_region_count = []
+
+    countries.each do |country|
+      country.protected_areas_per_iucn_category.each do |protected_area|
+        region_pa_category = region_data[protected_area["iucn_category_name"]]
+        region_pa_category["count"] ||= 0
+        region_pa_category["count"] += protected_area["count"].to_i
+        total_region_count << protected_area["count"].to_i
+        region_pa_category["percentage"] ||= 0
+        region_pa_category["percentage"] += protected_area["percentage"].to_f
+      end
+    end
+
+    processed_data = region_data.map{ |key,value| {
+          "iucn_category_name" => key,
+          "count" => value["count"],
+          "percentage" => 100 * value["count"] / total_region_count.reduce(0, :+)
+        }
+    }
+
+    processed_data
+  end
+
   def sources_per_jurisdiction
     ActiveRecord::Base.connection.execute("""
       SELECT jurisdictions.name, COUNT(DISTINCT protected_areas_sources.source_id)
