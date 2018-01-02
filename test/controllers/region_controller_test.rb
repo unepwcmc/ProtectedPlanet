2fe34456fe8f4f77445b035536bede83cf451992
Andrew Potter <hello@drewpotter.com>
2018-01-02 11:50:44 +0000

    Added simple test for the region controller

---
+++ test/controllers/region_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class RegionControllerTest < ActionController::TestCase
+
+  test ".show action returns 200" do
+
+    region = FactoryGirl.create(:region, iso: 'EU')
+    FactoryGirl.create(:regional_statistic, region: region, pa_area: 100)
+
+    country = FactoryGirl.create(:country, name: 'Belgium', iso_3: 'BEL', region: region)
+
+    FactoryGirl.create(:country_statistic,
+      country: country,
+      pa_area: 100,
+      land_area: 50,
+      pa_land_area: 50,
+      percentage_pa_marine_cover: 50,
+      pa_marine_area: 50,
+      marine_area: 50,
+      percentage_pa_land_cover: 50,
+      polygons_count: 100,
+      points_count: 100
+    )
+
+    get :show, iso: 'EU'
+    assert_response :success
+  end
+
+end
