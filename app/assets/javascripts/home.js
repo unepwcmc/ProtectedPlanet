98fe3a3bb29ff09e948492ed32d4b8066613aaee
Will Kocur <will.kocur@unep-wcmc.org>
2019-01-30 12:00:09 +0000

    teeny changes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,5 @@
 //= require 'vue'
 //= require base
-//= require 'vue'
 //= require 'modules/home/base'
 //= require 'vue/components/carousel/carousel-helpers'
 //= require vue/components/carousel/Carousel
