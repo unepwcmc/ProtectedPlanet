277a26457c056ff6710992783152eaa65d7f765d
stacytalbot <stacymtalbot@gmail.com>
2019-02-07 09:57:54 +0000

    Deploy master to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, :staging
-set :branch, 'banner'
+set :branch, 'master'
 
 server "stage.protectedplanet.net", user: 'wcmc', roles: %w{web}
 server "util.pp-staging.linode.protectedplanet.net", user: 'wcmc', roles: %w{util}
