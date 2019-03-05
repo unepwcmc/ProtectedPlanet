a777da5a6f7facceecd1ee5ea1eeb77ee38f1222
lucacug <luca.cugliari@unep-wcmc.org>
2019-03-05 15:01:45 +0000

    check if PA exist before creat ing a story link

--- lib/modules/wdpa/story_map_link_list_importer.rb
+++ lib/modules/wdpa/story_map_link_list_importer.rb
@@ -8,8 +8,14 @@ module Wdpa::StoryMapLinkListImporter
       csv.shift # remove headers
 
       csv.each do |row|
-        StoryMapLink.where(wdpa_id: row[0], link: row[1])
-                    .first_or_create
+        wdpa_id = Integer(row[0]) rescue false
+        unless ProtectedArea.find_by_wdpa_id(wdpa_id).blank?
+          StoryMapLink.where(wdpa_id: wdpa_id, link: row[1])
+                      .first_or_create
+          puts "Added this link #{row[1]} to #{wdpa_id}"
+        else
+          puts "Protected Area with wdpa_id #{row[0]} doesn't exist"
+        end
       end
     end
   end
