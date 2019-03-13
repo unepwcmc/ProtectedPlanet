077e77f5b81d3f46977cb81c9bf7d4ba3d1cb29b
lucacug <luca.cugliari@unep-wcmc.org>
2019-03-13 16:37:21 +0000

    add month to filter

--- app/presenters/cms_presenter.rb
+++ app/presenters/cms_presenter.rb
@@ -18,6 +18,10 @@ class CmsPresenter
     resources.sort_by(&:created_at).map(&:created_at).group_by(&:year)
   end
 
+  def self.all_by_month resources
+    resources.group_by { |u| u.created_at.strftime('%Y-%m') }
+  end
+
   private
 
   def collect_older_versions page, collection=[]
