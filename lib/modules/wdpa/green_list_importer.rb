949dff300571a498ff3eaa282bb54f4b3280d925
lucacug <luca.cugliari@unep-wcmc.org>
2019-04-18 16:59:30 +0100

    refactor green list to deal with the two new attributes

--- lib/modules/wdpa/green_list_importer.rb
+++ lib/modules/wdpa/green_list_importer.rb
@@ -9,9 +9,10 @@ module Wdpa::GreenListImporter
       csv = CSV.read(GREEN_LIST_SITES_CSV)
       csv.shift # remove headers
 
-      sites = csv.map { |row| row }.flatten.uniq
+      # sites = csv.map { |row| row }.flatten.uniq
 
-      sites.each do |site|
+      csv.each do |row|
+        site, status, date = row[0], row[1], row[2]
         wdpa_id = Integer(site) rescue false
         if wdpa_id
           pa = ProtectedArea.find_by_wdpa_id(wdpa_id)
@@ -20,9 +21,13 @@ module Wdpa::GreenListImporter
         end
 
         unless pa.blank?
-          pa.is_green_list = true
-          pa.save
-          puts "Marked #{site} as a green list site"
+          # pa.is_green_list = true
+          # pa.save
+          pa.update_attributes(is_green_list: true,
+                               green_list_status: status,
+                               green_list_status_date: date)
+          # puts "Marked #{site} as a green list site"
+          puts "Updated green list site #{site}"
         end
       end
     end
