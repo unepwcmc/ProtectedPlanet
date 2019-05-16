b7a1afe6756048a775975f5f40be278c651d1164
stacytalbot <stacymtalbot@gmail.com>
2019-05-16 15:27:22 +0100

    Fix up js error on the home page

---
+++ app/javascript/components/charts/chart-bar/ChartBarStacked.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+  import ScrollMagic from 'scrollmagic'
+
+  export default {
+    name: "chart-bar-stacked",
+
+    mounted () {
+      this.scrollMagicHandlers()
+    },
+
+    methods: {
+      scrollMagicHandlers () {
+        const marineScrollMagic = new ScrollMagic.Controller()
+
+        new ScrollMagic.Scene({ triggerElement: '.sm-infographic', reverse: false })
+          .setClassToggle('.sm-infographic .infographic__bar--pa', 'infographic__bar--pa--animate')
+          .addTo(marineScrollMagic)
+      }
+    }
+  }
+</script>
