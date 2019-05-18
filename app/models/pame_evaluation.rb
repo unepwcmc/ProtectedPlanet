f1f833f7cc4f51af8f616474f907a17fbedb1a95
Andrew Potter <hello@drewpotter.com>
2019-05-18 19:30:21 +0100

    Modifications to the pame importer to handle restricted protected areas

--- app/models/pame_evaluation.rb
+++ app/models/pame_evaluation.rb
@@ -3,5 +3,5 @@ class PameEvaluation < ActiveRecord::Base
   belongs_to :pame_source
   has_and_belongs_to_many :countries
 
-  validates :methodology, :year, :protected_area, :metadata_id, :url, presence: true
+  validates :methodology, :year, :metadata_id, :url, presence: true
 end
--- lib/modules/wdpa/pame_importer.rb
+++ lib/modules/wdpa/pame_importer.rb
@@ -1,11 +1,11 @@
 require 'csv'
 
 module Wdpa::PameImporter
-  PAME_EVALUATIONS = "#{Rails.root}/lib/data/seeds/pame_data-2019-03-29.csv".freeze
+  PAME_EVALUATIONS = "#{Rails.root}/lib/data/seeds/pame_data-2019-04-25.csv".freeze
 
   def self.import
     puts "Importing PAME evaluations..."
-    missing_pas = []
+    delete_evaluations = []
 
     CSV.foreach(PAME_EVALUATIONS, headers: true) do |row|
       wdpa_id         = row[1].to_i
@@ -14,6 +14,8 @@ module Wdpa::PameImporter
       protected_area  = ProtectedArea.find_by_wdpa_id(wdpa_id) || nil
       metadata_id     = row[6].to_i
       url             = row[5]
+      restricted      = row[13] == "TRUE" ? true : false
+      iso3s           = row[2]
       pame_source     = PameSource.where({
         data_title: row[9],
         resp_party: row[10],
@@ -27,16 +29,16 @@ module Wdpa::PameImporter
           ps.language   = row[12]
         end
 
-      if protected_area.nil?
-        missing_pas << wdpa_id
-      else
-        PameEvaluation.where({
+      # If PameEvaluation doesn’t have a PA and is restricted then it's restricted.
+      if (protected_area.nil? && restricted) || (protected_area.present?)
+        pame_evaluation = PameEvaluation.where({
           protected_area: protected_area,
           methodology: methodology,
           year: year,
           metadata_id: metadata_id,
           url: url,
-          pame_source: pame_source
+          pame_source: pame_source,
+          restricted: restricted
         }).first_or_create do |pe|
           # If the record doesn't exist, create it...
           pe.protected_area = protected_area
@@ -45,12 +47,22 @@ module Wdpa::PameImporter
           pe.metadata_id    = metadata_id
           pe.url            = url
           pe.pame_source    = pame_source
+          pe.restricted     = restricted
         end
+        if protected_area.nil? && restricted
+          countries = []
+          iso3s.split(",").each do |iso3|
+            country = Country.find_by(iso_3: iso3)
+            pame_evaluation.countries << country unless pame_evaluation.countries.include? country
+          end
+        end
+      elsif protected_area.nil? && !restricted # If PameEvaluation doesn’t have a PA and isn’t restricted it should be deleted.
+        delete_evaluations << wdpa_id
       end
     end
 
     puts "Import finished!"
-    puts "Missing Protected Areas: #{missing_pas.count}"
-    puts missing_pas.join(",")
+    puts "Please delete the following: #{delete_evaluations.count}"
+    puts delete_evaluations.join(",")
   end
 end
