468a0ec95983e8e9b652375fc6808c679a6de619
Stuart Watson <stuart.watson@unep-wcmc.org>
2019-08-05 14:37:19 +0100

    Removed file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,7 @@
 set :stage, :staging
 set :branch, "devise_fix"
 
-server 'new-web.pp-staging.linode.protectedplanet.net', user: 'wcmc', roles: %w{app db web}
-
+server 'new-web.pp-staging.linode.protectedplanet.net', roles: %w{web app db}
 
 set :application, "protectedplanet"
 set :server_name, "protectedplanet"
