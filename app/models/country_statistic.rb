ed8b120d31b6502dccad14caf768f2e1bbf41e2b
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-08-26 11:11:54 +0100

    Add draft global percentage coverage

--- app/models/country_statistic.rb
+++ app/models/country_statistic.rb
@@ -28,4 +28,16 @@ class CountryStatistic < ActiveRecord::Base
   def overseas_percentage
     (overseas_total_protected_marine_area / overseas_total_marine_area) * 100
   end
+
+  [:land, :marine].each do |type|
+    field_name = "percentage_pa_#{type}_cover"
+    define_singleton_method("global_#{field_name}") do
+      stats = where.not(country_id: nil)
+      sum =
+        stats.map(&field_name.to_sym).inject(0) do |_sum, x|
+          _sum + (x || 0)
+        end
+      (sum / Country.count).round(2)
+    end
+  end
 end
