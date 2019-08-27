aa2d7af450b07570e70216aab2c42386fdafac52
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-08-27 14:54:44 +0100

    Make aichi11_targets model a singleton

--- app/models/aichi11_target.rb
+++ app/models/aichi11_target.rb
@@ -1,3 +1,19 @@
 class Aichi11Target < ActiveRecord::Base
- belongs_to :country
+  validates_inclusion_of :singleton_guard, :in => [0]
+
+  def self.instance
+    first || import
+  end
+
+  private
+
+  def self.import
+    CSV.foreach(aichi11_target_csv_path, headers: true) do |row|
+      return create({}.merge(row))
+    end
+  end
+
+  def self.aichi11_target_csv_path
+    Rails.root.join('lib/data/seeds/aichi11_targets.csv')
+  end
 end
