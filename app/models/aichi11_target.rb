76ef9709f4572bb8a436ac6d2b7d3a9ef8395d05
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-09-06 12:04:21 +0100

    Make stats constant a method to overcome issues with instance being called in constant

--- app/models/aichi11_target.rb
+++ app/models/aichi11_target.rb
@@ -5,30 +5,6 @@ class Aichi11Target < ActiveRecord::Base
     first || import
   end
 
-  STATS = {
-    coverage: {
-      name: 'Coverage',
-      terrestrial: {
-        value: CountryStatistic.global_percentage_pa_land_cover,
-        target: instance.coverage_terrestrial
-      },
-      marine: {
-        value: CountryStatistic.global_percentage_pa_marine_cover,
-        target: instance.coverage_marine
-      }
-    },
-    effectively_managed: {
-      name: 'Effectively managed',
-      terrestrial: {
-        value: PameStatistic.global_pame_percentage_pa_land_cover,
-        target: instance.effectively_managed_terrestrial
-      },
-      marine: {
-        value: PameStatistic.global_pame_percentage_pa_marine_cover,
-        target: instance.effectively_managed_marine
-      }
-    }
-  }.freeze
   TERRESTRIAL = {
     title: 'Terrestrial',
     colour: 'terrestrial'
@@ -47,7 +23,7 @@ class Aichi11Target < ActiveRecord::Base
   }.freeze
   def self.get_global_stats
     global_stats = Stats::CountryStatisticsApi.get_global_stats
-    STATS.each do |name, attributes|
+    stats.each do |name, attributes|
       json = { title: attributes[:name], charts: [] }
       terrestrial_chart = DEFAULT_CHART_JSON.merge(**TERRESTRIAL, **attributes[:terrestrial])
       marine_chart = DEFAULT_CHART_JSON.merge(**MARINE, **attributes[:marine])
@@ -67,5 +43,32 @@ class Aichi11Target < ActiveRecord::Base
 
   def self.aichi11_target_csv_path
     Rails.root.join('lib/data/seeds/aichi11_targets.csv')
+  end
+
+  def self.stats
+    {
+      coverage: {
+        name: 'Coverage',
+        terrestrial: {
+          value: CountryStatistic.global_percentage_pa_land_cover,
+          target: instance.coverage_terrestrial
+        },
+        marine: {
+          value: CountryStatistic.global_percentage_pa_marine_cover,
+          target: instance.coverage_marine
+        }
+      },
+      effectively_managed: {
+        name: 'Effectively managed',
+        terrestrial: {
+          value: PameStatistic.global_pame_percentage_pa_land_cover,
+          target: instance.effectively_managed_terrestrial
+        },
+        marine: {
+          value: PameStatistic.global_pame_percentage_pa_marine_cover,
+          target: instance.effectively_managed_marine
+        }
+      }
+    }
   end
 end
