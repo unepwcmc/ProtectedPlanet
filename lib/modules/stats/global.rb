1e27b01959496a943e8ff7a915ed63ef236e98d2
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-09-24 13:44:53 +0100

    Add comment clarifying on global stats calculation

--- lib/modules/stats/global.rb
+++ lib/modules/stats/global.rb
@@ -36,6 +36,8 @@ class Stats::Global
   end
 
   def self.calculate_stats_for(klass, field_name)
+    # Statistics with no country id belong to ABNJ and ATA
+    # which should be included in the global stat calculation
     stats = klass.all
     stats.map(&field_name.to_sym).inject(0) do |_sum, x|
       _sum + (x || 0)
