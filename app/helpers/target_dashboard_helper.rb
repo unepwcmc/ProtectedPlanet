6f3e279f36817bc613547f896b7fa2f0859bc1cb
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-10-02 14:47:24 +0100

    Fix missing tooltip text. Make sure to compare strings

--- app/helpers/target_dashboard_helper.rb
+++ app/helpers/target_dashboard_helper.rb
@@ -1,6 +1,6 @@
 module TargetDashboardHelper
   def getTooltipText id
-    tooltip = t("thematic_area.target_11_dashboard.tooltips").select { |_tooltip| _tooltip[:id] == id }.first
+    tooltip = t("thematic_area.target_11_dashboard.tooltips").select { |_tooltip| _tooltip[:id] == id.to_s }.first
 
     tooltip ? tooltip[:text] : ''
   end
