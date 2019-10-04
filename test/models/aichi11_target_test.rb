0c87f8899c2052ff2d1623722200fc3e2cbb1633
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2019-10-04 15:52:49 +0200

    Test Aichi11Target singleton is populated correctly

--- test/models/aichi11_target_test.rb
+++ test/models/aichi11_target_test.rb
@@ -6,4 +6,20 @@ class Aichi11TargetTest < ActiveSupport::TestCase
 
     assert_raise(ActiveRecord::RecordNotUnique) { FactoryGirl.create(:aichi11_target) }
   end
+
+  test 'it imports and saves stats from API' do
+    data = { importance_global: 12.15 }
+    Stats::CountryStatisticsApi.stubs(:global_stats_for_import).returns(data)
+
+    assert_equal Aichi11Target.instance.importance_global, 12.15
+  end
+
+  test 'it reads targets from file' do
+    Stats::CountryStatisticsApi.stubs(:global_stats_for_import).returns({})
+
+    Aichi11Target.expects(:aichi11_target_csv_path).returns(
+      Rails.root.join('lib/data/seeds/aichi11_targets.csv')
+    )
+    assert_equal(Aichi11Target.instance.importance_global, 17)
+  end
 end
