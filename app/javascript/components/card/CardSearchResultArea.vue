25721b3f5d987bea0e9edbe6f4af4211992d62b1
stacytalbot <stacymtalbot@gmail.com>
2020-02-14 17:52:22 +0000

    Start to build the vue components for the wdpa search results page

---
+++ app/javascript/components/card/CardSearchResultArea.vue
@@ -0,0 +1,62 @@
+<template>
+    <a 
+      class="card__link"
+      :href="url"
+      title=""
+    >
+      <i class="card__icon--region-large" />
+      
+      <img 
+        v-if="image" 
+        :src="image" 
+        alt=""
+        class="card__image"
+      >
+
+    <div class="card__content">
+      <h3 
+        v-html="title"
+        class="card__title"
+      />
+      <p
+        v-html="areas"
+      />
+    </div>
+
+    <div class="card__groups">
+      <p class="card__group flex flex-v-center">
+        <i class="card__icon--region" />
+        <span>Europe</span>
+      </p>
+      <p class="card__group flex flex-v-center">
+        <i class="card__icon--flag" />
+        <span>France</span>
+      </p>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'CardSearchResultArea',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: false
+    },
+    areas: {
+      type: String,
+      required: false
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ app/javascript/components/search/SearchResultsAreas.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="search--results-areas">
+    <!-- <p class="search__total">({{ totalItems }} {{ resultsText }})</p> -->
+
+    <div class="cards--search-results-areas">
+      <card-search-result-area
+        v-for="result, index in results.regions"
+        :key="index"
+        :image="result.image"
+        :summary="result.summary"
+        :title="result.title"
+        :url="result.url"
+      />
+    </div>
+
+    <!-- <pagination 
+      v-on:change-page="changePage" 
+      :currentPage="currentPage"
+      :pageItemsEnd="pageItemsEnd" 
+      :pageItemsStart="pageItemsStart" 
+      :noResultsText="noResultsText"
+      :totalItems="totalItems"
+    /> -->
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { setCsrfToken } from '../../helpers/request-helpers'
+import CardSearchResultArea from '../card/CardSearchResultArea.vue'
+import Pagination from '../pagination/Pagination.vue'
+
+export default {
+  name: 'SearchResultsAreas',
+
+  components: { CardSearchResultArea, Pagination },
+
+  props: {
+    // endpoint: {
+    //   type: String,
+    //   required: true
+    // },
+    // items_per_page: {
+    //   type: Number,
+    //   default: 15
+    // },
+    // noResultsText: {
+    //   type: String,
+    //   required: true
+    // },
+    // resultsText: {
+    //   type: String,
+    //   required: true
+    // }
+  },
+
+  data () {
+    return {
+      categoryId: '',
+      categories: [], // [ String ]
+      currentPage: 0,
+      // defaultCategory: this.categories[0].id,
+      defaultPage: 1,
+      pageItemsStart: 0,
+      pageItemsEnd: 0,
+      requestedPage: 0,
+      // results: {}, // { regions: [{ title: String, url: String}], countries: [{ areas: String, region: String, title: String, url: String}], sites: [{ areas: String, country: String, image: String, region: String, title: String, url: String}] }
+      searchTerm: '',
+      totalItems: 0,
+      results: { 
+        regions: [
+          {
+            title: 'Asia & Pacific',
+            url: 'url to page'
+          }
+        ],
+        countries: [
+          {
+            areas: 5908,
+            region: 'America',
+            title: 'United States of America',
+            url: 'url to page'
+          },
+          {
+            areas: 508,
+            regions: 'Europe',
+            title: 'United Kingdom',
+            url: 'url to page'
+          },
+          {
+            areas: 508,
+            regions: 'Europe',
+            title: 'United Kingdom',
+            url: 'url to page'
+          },
+          {
+            areas: 508,
+            regions: 'Europe',
+            title: 'United Kingdom',
+            url: 'url to page'
+          }
+        ],
+        sites: [
+          {
+            country: 'France',
+            image: 'url to generated map of PA location',
+            region: 'Europe',
+            title: 'Avenc De Fra Rafel',
+            url: 'url to page'
+          }
+        ]
+      }
+    }
+  },
+
+  created () {
+    // this.categoryId = this.defaultCategory
+    // this.requestedPage = this.defaultPage
+    // this.ajaxSubmission()
+  },
+
+  computed: {
+    hasResults () {
+      return this.results.length > 0
+    }
+  },
+
+  methods: {
+    ajaxSubmission () {
+      let data = {
+        params: {
+          category_id: this.categoryId,
+          items_per_page: this.itemsPerPage,
+          requested_page: this.requestedPage,
+          searchTerm: this.searchTerm
+        }
+      }
+
+      axios.post(this.endpoint, data)
+        .then(response => {
+          this.updateProperties(response.data)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
+
+    changePage (newPage) {
+      this.categoryId = this.defaultCategory
+      this.requestedPage = newPage
+    },
+
+    updateCategory (categoryId) {
+      this.categoryId = categoryId
+      this.requestedPage = this.defaultPage
+    },
+
+    updateProperties (data) {
+      this.categories = data.categories
+      this.currentPage = data.current_page
+      this.pageItemsStart = data.page_items_start
+      this.pageItemsEnd = data.page_items_end
+      this.results = data.results
+      this.searchTerm = data.search_term
+      this.totalItems = data.total_items
+    },
+  }
+}
+</script>
