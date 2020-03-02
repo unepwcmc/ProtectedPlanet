1b87d365e828aec9e899b043ac18ed607ba5f521
stacytalbot <stacymtalbot@gmail.com>
2020-03-02 11:58:46 +0000

    Style up the list in the stats card

--- app/controllers/region_controller.rb
+++ app/controllers/region_controller.rb
@@ -3,6 +3,7 @@ class RegionController < ApplicationController
 
   def show
     @iucn_categories = @region.protected_areas_per_iucn_category
+    @governance_types = @region.protected_areas_per_governance
 
     @sources = [
       {
