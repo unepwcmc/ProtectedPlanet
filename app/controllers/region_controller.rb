8dc8e50ddac08e98792b787a816a3a7671a3c2df
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-03-30 16:13:13 +0100

    Refactor controllers and presenters

--- app/controllers/region_controller.rb
+++ app/controllers/region_controller.rb
@@ -2,40 +2,11 @@ class RegionController < ApplicationController
   before_action :load_vars
 
   def show
-    presenter = RegionPresenter.new @region
-
-    @designations = [
-      {
-        title: 'National designations',
-        total: get_designation_total('National'),
-        has_jurisdiction: has_jurisdiction('National'),
-        jurisdictions: get_jurisdictions('National')
-      }, 
-      {
-        title: 'Regional designations',
-        total: get_designation_total('Regional'),
-        has_jurisdiction: has_jurisdiction('Regional'),
-        jurisdictions: get_jurisdictions('Regional')
-      },
-      {
-        title: 'International designations',
-        total: get_designation_total('International'),
-        has_jurisdiction: has_jurisdiction('International'),
-        jurisdictions: get_jurisdictions('International')
-      }
-    ]
+    @presenter = RegionPresenter.new @region
 
     @iucn_categories = @region.protected_areas_per_iucn_category
 
     @governance_types = @region.protected_areas_per_governance
-
-    @marine_stats = {
-      pame_km2: 'XXXXX', ##TODO FERDI
-      pame_percentage: 'XXXXX', ##TODO FERDI
-      protected_km2: presenter.pa_marine_area.round(0),
-      protected_percentage: presenter.percentage_pa_marine_cover.round(2),
-      total_km2: presenter.marine_area.round(0)
-    }
 
     @sources = [
       {
@@ -45,17 +16,7 @@ class RegionController < ApplicationController
       }
     ]
 
-    @terrestrial_stats = {
-      pame_km2: 'XXXXX', ##TODO FERDI
-      pame_percentage: 'XXXXX', ##TODO FERDI
-      protected_km2: presenter.pa_land_area.round(0),
-      protected_percentage: presenter.percentage_pa_land_cover.round(2),
-      total_km2: presenter.land_area.round(0)
-    }
-
     @total_oecm = 0 ##TODO
-    @total_points_percentage = presenter.geometry_ratio[:points]
-    @total_polygons_percentage = presenter.geometry_ratio[:polygons] 
     @total_wdpa = @region.protected_areas.count
 
     @wdpa = get_wdpa
@@ -68,34 +29,6 @@ class RegionController < ApplicationController
     @region = Region.where(iso: params[:iso].upcase).first
     @region or raise_404
     @presenter = RegionPresenter.new @region
-  end
-
-  def designations ##FERDI I am assuming a lot of this will be moved as it is used across region and countries
-    designations_by_jurisdiction = @region.designations.group_by { |design|
-      design.jurisdiction.name rescue "Not Reported"
-    }
-  end
-
-  def has_jurisdiction type 
-    Jurisdiction.find_by_name(type)
-  end
-
-  def get_designation_total type
-    if designations.include? type then
-      designations[type].count
-    else
-      0
-    end
-  end
-
-  def get_jurisdictions type
-    jurisdiction = has_jurisdiction type
-    
-    if jurisdiction
-      @region.protected_areas_per_designation(jurisdiction)
-    else
-      []
-    end
   end
 
   def get_wdpa
--- app/presenters/country_presenter.rb
+++ app/presenters/country_presenter.rb
@@ -1,10 +1,35 @@
 class CountryPresenter
   def initialize country
     @country = country
+    @statistic = StatisticPresenter.new(country)
+    @designations_presenter = DesignationsPresenter.new(country)
   end
 
-  def marine_statistics
-    statistic = StatisticPresenter.new(country)
+  def designations
+    @designations_presenter.designations
+  end
+
+  def marine_stats
+    {
+      pame_km2: statistic.pame_statistic.pame_pa_marine_area,
+      pame_percentage: statistic.pame_statistic.pame_percentage_pa_marine_cover.round(2),
+      protected_km2: statistic.pa_marine_area.round(0),
+      protected_percentage: statistic.percentage_pa_marine_cover.round(2),
+      total_km2: statistic.marine_area.round(0)
+    }
+  end
+
+  def terrestrial_stats
+    {
+      pame_km2: statistic.pame_statistic.pame_pa_land_area,
+      pame_percentage: statistic.pame_statistic.pame_percentage_pa_land_cover.round(2),
+      protected_km2: statistic.pa_land_area.round(0),
+      protected_percentage: statistic.percentage_pa_land_cover.round(2),
+      total_km2: statistic.land_area.round(0)
+    }
+  end
+
+  def marine_page_statistics
     {
       name: country.name,
       iso: country.iso,
@@ -18,10 +43,22 @@ class CountryPresenter
     }
   end
 
+  def total_points_percentage
+    statistic.geometry_ratio[:points]
+  end
+
+  def total_polygons_percentage
+    statistic.geometry_ratio[:polygons]
+  end
+
   private
 
   def country
     @country
+  end
+
+  def statistic
+    @statistic
   end
 
   def overseas_territories_url
