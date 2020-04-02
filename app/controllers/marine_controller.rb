06ba967e58cbbc0d95f09529c6a4241d49bb85f3
stacytalbot <stacymtalbot@gmail.com>
2020-04-02 10:31:02 +0100

    Remove comments from controller

--- app/controllers/marine_controller.rb
+++ app/controllers/marine_controller.rb
@@ -153,12 +153,6 @@ class MarineController < ApplicationController
   end
 
   def growth
-    # @protectedAreasGrowth = [
-    #   {
-    #     id: 'a',
-    #     datapoints: [{x:0,y:0},{x:1,y:1},{x:2,y:2},{x:3,y:3},{x:4,y:4},{x:5,y:5},{x:6,y:6},{x:7,y:7}]
-    #   }
-    # ].to_json
     @protectedAreasGrowth = [
       {
         id: "Global",
