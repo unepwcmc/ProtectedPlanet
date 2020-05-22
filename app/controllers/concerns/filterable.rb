d172a778191ff5a6ca4c679f03d461524f2b7f87
stacytalbot <stacymtalbot@gmail.com>
2020-05-22 14:20:05 +0100

    Update Filter Groups in the backend

--- app/controllers/concerns/filterable.rb
+++ app/controllers/concerns/filterable.rb
@@ -16,24 +16,17 @@ module Concerns::Filterable
 
       @filter_groups = [
         {
-          title: I18n.t('search.view-by'),
-          filters: [
-            {
-              id: 'geo_type',
-              name: 'geo_type',
-              options: [
-                { id: 'all', title: I18n.t('search.view-group-geo-type.options')[0] },
-                { id: 'regions', title: I18n.t('search.view-group-geo-type.options')[1] },
-                { id: 'countries', title: I18n.t('search.view-group-geo-type.options')[2] },
-                { id: 'sites', title: I18n.t('global.area-types.wdpa') } ## OR I18n.t('global.area-types.oecm')
-              ],
-              type: 'radio'
-            }
-          ]
-        },
-        {
           title: I18n.t('search.filter-by'),
           filters: [
+            {
+              id: 'db_type',
+              name: 'db_type',
+              options: [
+                { id: 'oecm', title: I18n.t('search.filter-group-db.options')[0] },
+                { id: 'wdpa', title: I18n.t('search.filter-group-db.options')[1] },
+              ],
+              type: 'checkbox'
+            },
             {
               id: 'is_type',
               name: 'is_type',
@@ -43,7 +36,35 @@ module Concerns::Filterable
                 { id: 'marine', title: I18n.t('search.filter-group-type.options')[2] }
               ],
               title: I18n.t('search.filter-group-type.title'),
-              type: 'radio'
+              type: 'checkbox'
+            },
+            {
+              id: 'special_status',
+              name: 'special_status',
+              options: [
+                { id: 'green-list', title: I18n.t('search.filter-group-special-status.options')[0] },
+                { id: 'parcc', title: I18n.t('search.filter-group-special-status.options')[1] },
+                { id: 'irreplacibility', title: I18n.t('search.filter-group-special-status.options')[2] },
+              ],
+              title: I18n.t('search.filter-group-special-status.title'),
+              type: 'checkbox'
+            },
+            {
+              id: 'geo_type',
+              options: [
+                { 
+                  id: 'country', 
+                  title: I18n.t('search.filter-group-geo-type.options')[0],
+                  autocomplete: [ { id: 'whatever you need', title: 'United Kingdom' }]
+                },
+                { 
+                  id: 'region', 
+                  title: I18n.t('search.filter-group-geo-type.options')[1],
+                  autocomplete: [ { id: 'whatever you need', title: 'Europe' }]
+                }
+              ],
+              title: I18n.t('search.filter-group-geo-type.title'),
+              type: 'tabs-search'
             },
             {
               id: 'designation',
