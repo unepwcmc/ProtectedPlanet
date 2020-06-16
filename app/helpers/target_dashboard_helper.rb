47b02f4b301107289d3563f0610442a61cd6658f
stacytalbot <stacymtalbot@gmail.com>
2020-06-16 19:02:49 +0100

    Start replacing the agile carousel on the target 11 dashboard page with the flickity one

--- app/helpers/target_dashboard_helper.rb
+++ app/helpers/target_dashboard_helper.rb
@@ -7,31 +7,34 @@ module TargetDashboardHelper
 
   def get_config_carousel_global
     {
-      navButtons: false,
-      infinite: true,
-      responsive: [
-      {
-          breakpoint: 628,
-          settings: {
-            dots: true,
-            slidesToShow: 1,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            dots: true,
-            slidesToShow: 2,
-          }
-        },
-        {
-          breakpoint: 1024,
-          settings: {
-            dots: false,
-            slidesToShow: 5
-          }
-        }
-      ]
+      cellAlign: 'left',
+      prevNextButtons: false,
+      wrapAround: true
+      # navButtons: false,
+      # infinite: true,
+      # responsive: [
+      # {
+      #     breakpoint: 628,
+      #     settings: {
+      #       dots: true,
+      #       slidesToShow: 1,
+      #     }
+      #   },
+      #   {
+      #     breakpoint: 768,
+      #     settings: {
+      #       dots: true,
+      #       slidesToShow: 2,
+      #     }
+      #   },
+      #   {
+      #     breakpoint: 1024,
+      #     settings: {
+      #       dots: false,
+      #       slidesToShow: 5
+      #     }
+      #   }
+      # ]
     }.to_json
   end
 end
