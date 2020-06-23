534c456a48648343d6cd9f1e5a623b3a021b6727
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-06-23 12:01:29 +0100

    Update url param to redirect to filtered search

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,12 +5,14 @@ module HomeHelper
     home_yml[:pas][:categories].map do |category|
       #Make sure to remove any leading /
       slug = category[:slug].split('/').last
+      filter = Array.wrap(category[:filter])
       cms_page = Comfy::Cms::Page.find_by_slug(slug)
       {
         image: cms_fragment_render(:theme_image, cms_page),
         title: category[:title],
-        url: 'filtered wdpa page' ##TODO filtered WDPA results page 
+        url: search_areas_path(db_type: 'wdpa', filters: {is_type: filter })
       }
     end
   end
+
 end
