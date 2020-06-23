55bcbd9eb20d8fd09e326dd472bd175e7d85f471
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-06-23 13:35:41 +0100

    Fix filtered search url for greenlist

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,13 +5,20 @@ module HomeHelper
     home_yml[:pas][:categories].map do |category|
       #Make sure to remove any leading /
       slug = category[:slug].split('/').last
-      filter = Array.wrap(category[:filter])
       cms_page = Comfy::Cms::Page.find_by_slug(slug)
       {
         image: cms_fragment_render(:theme_image, cms_page),
         title: category[:title],
-        url: search_areas_path(db_type: 'wdpa', filters: {is_type: filter })
+        url: search_areas_path(db_type: 'wdpa', filters: get_filter(category[:filter]))
       }
+    end
+  end
+
+  def get_filter(filter)
+    if filter == 'is_green_list'
+      { special_status: [filter] }
+    else
+      { is_type: [filter] }
     end
   end
 
