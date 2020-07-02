bcbdfcf6b78c7bc2d04cc4476d829c4b55d46ef0
Jonathan Feist <jonathan.feist@unep-wcmc.org>
2020-07-02 14:43:26 +0100

    Add base for VMapFilters component

--- app/javascript/components/map/VMapFilters.vue
+++ app/javascript/components/map/VMapFilters.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="map--filters">
-    FILTERS
-    <slot />
+    <div class="header">
+      <div class="header--title">{{ title }}</div>
+      <div class="header--close-button"></div>
+    </div>
+    <div class="search">
+      <map-search />
+    </div>
+    <div class="overlays">
+      <div class="overlays--layer" v-for="(layer, index) in overlays" :key="index">
+        <v-map-filter-layer :layer="layer" />
+      </div>
+    </div>
+
+    <div class="disclaimer" v-if="disclaimer">
+      <slot name="disclaimer">
+        <div class="disclaimer--heading">{{ disclaimer.heading }}}</div>
+        <div class="disclaimer--body" v-html="disclaimer.body"></div>
+      </slot>
+    </div>
   </div>
 </template>
 
@@ -13,6 +30,21 @@ export default {
     return {
     }
   },
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    disclaimer: {
+      type: Object,
+      required: false,
+      validator: type => {
+        return type.hasOwnProperty('heading') && type.heading instanceof String
+          && type.hasOwnProperty('body') && type.heading instanceof String
+      }
+    }
+  }
 
 }
 </script>
