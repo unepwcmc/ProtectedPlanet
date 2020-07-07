a9a5646545e86c81e771abb3114a4d3695755959
Ben Tregenna <BenT@wcmclt02195smbp.lan>
2020-07-07 16:53:52 +0100

    Fixed a couple more cms factory test failures

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,6 +2,14 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
   test "should get index" do
+    seed_cms
+    # we need to add extra pages for pa categories on the home page
+    FactoryGirl.create(:cms_page, site: @site, layout: @layout, slug: 'marine-protected-areas')
+    FactoryGirl.create(:cms_page, site: @site, layout: @layout, slug: 'green-list')
+    # and the CTAs
+    FactoryGirl.create(:cms_cta, css_class: 'api')
+    FactoryGirl.create(:cms_cta, css_class: 'live-report')
+    
     get :index
     assert_response :success
   end
