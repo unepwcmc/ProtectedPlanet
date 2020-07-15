bc3452569f7a090b8f50a6419b04f9c9376e4f7a
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-15 17:08:49 +0100

    styled right panel

--- app/javascript/components/select/SelectEquity.vue
+++ app/javascript/components/select/SelectEquity.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="select-equity">
-    <div class="select-equity__content">
+  <div class="select--equity">
+    <div class="select--equity__content">
       <!-- <img :src="selected.image" :alt="'Visual results for ' + selected.title" /> -->
     </div>
 
-    <div class="select-equity__panel">
-      <h2>Results from study sites</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse recusandae deserunt saepe placeat ut architecto optio nostrum dolorem, laudantium ullam provident similique consectetur id eligendi praesentium labore quisquam atque vel?</p>
-      <select v-model="selected">
-        <option v-for="(option, index) in options" :key="index" :value="option">{{ option.label }}</option>
+    <div class="select--equity__panel">
+      <h2 class="select--equity__header">Results from study sites</h2>
+      <p class="select--equity__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse recusandae deserunt saepe placeat ut architecto optio nostrum dolorem, laudantium ullam provident similique consectetur id eligendi praesentium labore quisquam atque vel?</p>
+      <select class="select--equity__select" v-model="selected">
+        <option 
+          v-for="(option, index) in options" 
+          :key="index" 
+          :value="option"
+        >
+          {{ option.label }}
+        </option>
       </select>
-      <a :href="selected.url" title="View the protected area">View the protected area</a>
+      <a
+        :href="selected.url"
+        class="button--view-pa"
+        title="View the protected area"
+      >View Protected Area</a>
     </div>
   </div>
 </template>
