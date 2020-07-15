f17bfc260a6105d275e776139c681b89bef81911
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-15 12:18:45 +0100

    created a new component equity select

---
+++ app/javascript/components/select/EquitySelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="equity-select">
+    <div class="equity-select__content">
+      <img :src="selected.image" :alt="'Visual results for ' + selected.title" />
+    </div>
+
+    <div class="equity-select__panel">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse recusandae deserunt saepe placeat ut architecto optio nostrum dolorem, laudantium ullam provident similique consectetur id eligendi praesentium labore quisquam atque vel?</p>
+      <select v-model="selected">
+        <option v-for="(option, index) in options" :key="index" :value="option">{{ option.title }}</option>
+      </select>
+      <a :href="selected.url" title="View the protected area">View the protected area</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "equity-select",
+
+  props: {
+    options: {
+      type: Array
+    }
+  },
+
+  data() {
+    return {
+      selected: null
+    };
+  }
+};
+</script>
