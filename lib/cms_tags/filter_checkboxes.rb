72eb7b9a312c98d62bee360559581193c2092103
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-17 11:03:00 +0100

    custom filter checkboxes cms tag created

---
+++ lib/cms_tags/filter_checkboxes.rb
@@ -0,0 +1,47 @@
+class FilterCheckboxes < ComfortableMexicanSofa::Content::Tag::Fragment
+  def initialize(context:, params: [], source: nil)
+    super
+    @custom_categories = options['options'].split(',').map do |category|
+      category = category.strip
+    end
+
+    @custom_categories.each do |category|
+      class_eval { attr_accessor category }
+      instance_variable_set :"@#{category}", false
+    end
+  end
+
+  def content(category)
+    self.send(category)
+  end
+  
+
+  def form_field(object_name, view, index)
+    name = "#{object_name}[fragments_attributes][#{index}][boolean]"
+
+    
+    
+    input =   view.content_tag(:div, class: "form-check form-check-inline") do
+                @custom_categories.each do |category|
+          
+                  options = { 
+                      id: form_field_id, 
+                      class: "form-check-input"                
+                  } 
+
+                  view.concat view.hidden_field_tag(name, "0", id: nil)
+                  view.concat view.check_box_tag(name, "1", content(category), options)
+                  view.concat view.label_tag(category, nil, class: 'form-check-label pr-3')
+                end
+              end
+            
+
+    yield input
+  end
+
+end
+
+ComfortableMexicanSofa::Content::Renderer.register_tag(
+    :filter_checkboxes, FilterCheckboxes
+)
+  
