e1e9252e847ef7b7f17e14cbd7f0ed3125673987
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-20 21:37:00 +0100

    attributes and booleans stored in content before rendering

--- lib/cms_tags/filter_checkboxes.rb
+++ lib/cms_tags/filter_checkboxes.rb
@@ -11,15 +11,18 @@ class FilterCheckboxes < ComfortableMexicanSofa::Content::Tag::Fragment
     end
   end
 
-  def value_of_content(category)
-    self.send(category)
+  def store_in_content(category)
+    value = self.send(category)
+    fragment.content << " #{category} #{value} "
   end
   
   def form_field(object_name, view, index)
     name = "#{object_name}[fragments_attributes][#{index}][content]"
 
-    
-    
+    # Clearing the fragment's content before populating it with booleans stored
+    # as strings
+    fragment.content = ""
+
     input =   view.content_tag(:div, class: "form-check form-check-inline") do
                 @custom_categories.each do |category|
           
@@ -28,22 +31,27 @@ class FilterCheckboxes < ComfortableMexicanSofa::Content::Tag::Fragment
                       class: "form-check-input"               
                   } 
 
-                  view.concat view.hidden_field_tag(name, "#{category}", id: nil)
-                  view.concat view.check_box_tag(name, "1", parse_content(category), options)
+                  view.concat view.hidden_field_tag(name, category, id: nil)
+                  store_in_content(category)
+                  view.concat view.check_box_tag(name, "1", parse_content(category), options) 
                   view.concat view.label_tag(category, nil, class: 'form-check-label pr-3')
                 end
               end
             
-
+  
     yield input
   end
 
   private
 
-  def parse_content
-    return if self.content == ""
-    # TODO: Need to write a function to parse the content attribute of the fragment that is submitted via the form.
-    false
+  def parse_content(category)
+    return false if fragment.content == ""
+
+    stored_values = fragment.content.split(' ')
+    
+    # Making sure to grab the boolean (in string format) that follows every
+    # option name in the transmogrified fragment
+    ActiveModel::Type::Boolean.new.cast(stored_values[stored_values.find_index(category) + 1])
   end
 
 end
