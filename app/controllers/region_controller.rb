468b99ad2500b6bbe17417739aae08e2c6cd03a0
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-23 14:25:10 +0100

    added view all link for region pages

--- app/controllers/region_controller.rb
+++ app/controllers/region_controller.rb
@@ -25,6 +25,7 @@ class RegionController < ApplicationController
     @total_wdpa = @region.protected_areas.count
 
     @wdpa = pas_sample
+    @wdpaViewAllUrl = search_areas_path(filters: { location: { type: 'region', options: ["#{@region.name}"] } })
   end
 
   private
