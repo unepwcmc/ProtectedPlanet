ab312495782e1f94285e3e5855f7dd1d47799ffe
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2020-07-30 19:04:26 +0100

    Change layout of categories in the layout file so they are styled correctly

--- lib/cms_tags/categories.rb
+++ lib/cms_tags/categories.rb
@@ -8,8 +8,13 @@ class Categories < ComfortableMexicanSofa::Content::Tag::Fragment
   def form_field(object_name, view, index)
     options = { id: form_field_id, class: "form-control" }
   
-    input = view.send(:collection_check_boxes, :page, :page_category_ids, @categories, :id, :label, options)
-            
+    input = view.collection_check_boxes(:page, :page_category_ids, @categories, :id, :label, options) do |b|
+      view.content_tag(:div, class: "form-check form-check-inline") do
+        view.concat b.check_box(class: "form-check-input") 
+        view.concat b.label(class: "form-check-label")
+      end
+    end
+
     yield input
   end
 end
