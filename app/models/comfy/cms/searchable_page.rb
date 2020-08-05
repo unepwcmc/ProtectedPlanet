a5a085359f5f4a71c2e609dd92464062ec13b40b
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-08-05 18:35:42 +0100

    Index x only published pages

--- app/models/comfy/cms/searchable_page.rb
+++ app/models/comfy/cms/searchable_page.rb
@@ -6,6 +6,8 @@ class Comfy::Cms::SearchablePage < Comfy::Cms::Page
     class_name: 'Comfy::Cms::SearchableTranslation', foreign_key: 'page_id'
 
   def as_indexed_json
+    # index only published pages
+    return unless is_published
     self.as_json(
       only: [:id, :label],
       include: {
