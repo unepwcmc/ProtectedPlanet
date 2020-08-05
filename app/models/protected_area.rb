b212386fac45d5b5c7c73fd44e3882a4a3a8d5d0
Stanley Liu <sliu1993@hotmail.co.uk>
2020-08-05 15:46:55 +0100

    use of scopes instead of queries

--- app/models/protected_area.rb
+++ app/models/protected_area.rb
@@ -148,13 +148,13 @@ class ProtectedArea < ApplicationRecord
   end
 
   def self.global_terrestrial_oecm_coverage
-    land_oecms = ProtectedArea.where(is_oecm: true, marine: false).pluck(:reported_area)
+    land_oecms = ProtectedArea.oecms.marine_areas.pluck(:reported_area)
     total_oecm_area = land_oecms.inject(0) { |sum, area| sum + area.to_i }
     ((total_oecm_area.to_f / CountryStatistic.global_land_area.to_f) * 100).round(2)
   end
 
   def self.global_marine_oecm_coverage
-    marine_oecms = ProtectedArea.where(is_oecm: true, marine: true).pluck(:reported_marine_area)
+    marine_oecms = ProtectedArea.oecms.marine_areas.pluck(:reported_marine_area)
     total_oecm_area = marine_oecms.inject(0) { |sum, area| sum + area.to_i }
     ((total_oecm_area.to_f / CountryStatistic.global_marine_area.to_f) * 100).round(2)
   end
