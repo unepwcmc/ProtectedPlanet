91d9f0aef27f20a11bc0e7e9c398397075c6e171
Stanley Liu <sliu1993@hotmail.co.uk>
2020-08-05 16:27:14 +0100

    added test for dopa importer

---
+++ test/unit/wdpa/dopa_importer_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class TestDopaImporter < ActiveSupport::TestCase
+  test "#import DOPA sites" do
+    DOPA_LIST = "#{Rails.root}/lib/data/seeds/dopa4_pas_list.csv"
+
+    wdpa_ids = [1,2,3]
+    wdpa_ids.each do |wdpa_id|
+      FactoryGirl.create(:protected_area, wdpa_id: wdpa_id)
+    end
+
+    Wdpa::DopaImporter.import(DOPA_LIST)
+
+    assert_equal 3, ProtectedArea.where(is_dopa: true).count   
+  end
+
+  test "#skip DOPA sites which don't have a record in the DB" do 
+    DOPA_LIST = 
+
+    wdpa_ids = [1,2,3]
+    wdpa_ids.each do |wdpa_id|
+      FactoryGirl.create(:protected_area, wdpa_id: wdpa_id)
+    end
+
+    Wdpa::DopaImporter.import(DOPA_LIST)
+
+    assert_equal 3, ProtectedArea.where(is_dopa: true).count   
+  end
+
+end
