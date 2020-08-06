b3976a33d447214d81a7f05253be52f0af25cf6e
Stanley Liu <sliu1993@hotmail.co.uk>
2020-08-06 10:50:59 +0100

    fixed silly mistake in protected area

--- app/models/protected_area.rb
+++ app/models/protected_area.rb
@@ -26,6 +26,10 @@ class ProtectedArea < ApplicationRecord
   scope :oecms, -> { where(is_oecm: true) }
   scope :wdpas, -> { where(is_oecm: false) }
 
+  scope :terrestrial_areas, -> {
+    where(marine: false)
+  }
+
   scope :marine_areas, -> {
     where(marine: true)
   }
@@ -148,13 +152,13 @@ class ProtectedArea < ApplicationRecord
   end
 
   def self.global_terrestrial_oecm_coverage
-    land_oecms = ProtectedArea.oecms.marine_areas.pluck(:reported_area)
+    land_oecms = self.oecms.terrestrial_areas.pluck(:reported_area)
     total_oecm_area = land_oecms.inject(0) { |sum, area| sum + area.to_i }
     ((total_oecm_area.to_f / CountryStatistic.global_land_area.to_f) * 100).round(2)
   end
 
   def self.global_marine_oecm_coverage
-    marine_oecms = ProtectedArea.oecms.marine_areas.pluck(:reported_marine_area)
+    marine_oecms = self.oecms.marine_areas.pluck(:reported_marine_area)
     total_oecm_area = marine_oecms.inject(0) { |sum, area| sum + area.to_i }
     ((total_oecm_area.to_f / CountryStatistic.global_marine_area.to_f) * 100).round(2)
   end
