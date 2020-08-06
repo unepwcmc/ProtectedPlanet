aabc081081fed15dc4b099b99972fb4078fc1b3a
Stanley Liu <sliu1993@hotmail.co.uk>
2020-08-06 14:53:09 +0100

    new, much faster method of updating the db

--- lib/modules/wdpa/dopa_importer.rb
+++ lib/modules/wdpa/dopa_importer.rb
@@ -12,20 +12,19 @@ module Wdpa::DopaImporter
 
       # Read the CSV to get the WDPA IDs
       CSV.foreach(DOPA_LIST, headers: true) do |row| 
-        # Find the associated Protected Area with each WDPA ID mentioned and update the is_dopa column to true
-        dopa_pa = ProtectedArea.where('reported_area > 0.5e1')
-                  .or(ProtectedArea.where('reported_marine_area > 0.5e1'))
-                  .find_by_wdpa_id(row['wdpaid'])
-
-        if dopa_pa.nil? 
-          logger.info "DOPA site #{row['wdpaid']} not present in DB"
-          next
-        end
-
-       pas_to_update << ProtectedArea.find_by_wdpa_id(row['wdpaid'])
+       pas_to_update << row['wdpaid']
       end
+
       
-      ProtectedArea.where(id: pas_to_update.map(&:id)).update_all(is_dopa: true)
+      # Find all WDPA IDs (if they exist) in the CSV
+      areas = ProtectedArea.where(wdpa_id: pas_to_update)
+      
+      # Missing WDPA IDs
+      missing_pas = pas_to_update - areas.map(&:wdpa_id)
+      logger.info "Could not update #{missing_pas.join(', ')}"
+      
+      # Update all of them at once
+      areas.update_all(is_dopa: true)
     end
   end
 
