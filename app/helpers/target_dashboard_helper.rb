547cddabdfcc9594f7aafcecdd5cf9a965507e4e
Stanley Liu <sliu1993@hotmail.co.uk>
2020-08-18 18:25:21 +0100

    reworked yaml and helper to allow variations of text

--- app/helpers/target_dashboard_helper.rb
+++ app/helpers/target_dashboard_helper.rb
@@ -1,8 +1,18 @@
 module TargetDashboardHelper
-  def getTooltipText id
+  def getTooltipText(id)
     tooltip = t("thematic_area.target_11_dashboard.tooltips").select { |_tooltip| _tooltip[:id] == id.to_s }.first
+    tooltip ? t("thematic_area.target_11_dashboard.#{id}_text", geo_type: 'global') : ''
+  end
 
-    tooltip ? tooltip[:text] : ''
+  def region_and_country_tooltips
+    t("thematic_area.target_11_dashboard.tooltips").map do |tooltip|
+      id = tooltip[:id]
+      {
+        id: id,
+        title: tooltip[:title],
+        text: t("thematic_area.target_11_dashboard.#{id}_text", geo_type: 'the country/region\'s')
+      }
+    end.to_json
   end
 
   def get_config_carousel_t11
