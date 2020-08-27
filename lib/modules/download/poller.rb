cf37e5637c8617836516d40953db5c79c28b0489
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-08-27 09:55:10 +0100

    Make search download work even with no filters

--- lib/modules/download/poller.rb
+++ lib/modules/download/poller.rb
@@ -7,7 +7,7 @@ module Download::Poller
 
   def self.json_response(params)
     domain = params['domain']
-    filters = params['filters'] && Download::Utils.extract_filters(JSON.parse(params['filters']))
+    filters = get_filters(params)
     token = filters ? Download::Utils.search_token(search_term(params), filters) : params['token']
     format = params['format']
 
@@ -18,7 +18,8 @@ module Download::Poller
       'id' => computed_id(token, format),
       'title' => filename,
       'url' => is_ready ? Download.link_to(filename) : '',
-      'hasFailed' => Download.has_failed?(domain, token, format)
+      'hasFailed' => Download.has_failed?(domain, token, format),
+      'token' => token
     }
   end
 
@@ -28,5 +29,10 @@ module Download::Poller
 
   def self.search_term(params)
     params['search'].to_s
+  end
+
+  def self.get_filters(params)
+    return unless params['search']
+    params['filters'] ? Download::Utils.extract_filters(JSON.parse(params['filters'])) : {}
   end
 end
