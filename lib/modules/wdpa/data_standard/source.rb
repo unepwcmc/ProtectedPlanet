880b226e5302b54288e33f9583a7348876f3d710
Stanley Liu <sliu1993@hotmail.co.uk>
2020-09-15 15:25:00 +0100

    added update year to source importer and changed class methods to use update_yr

--- lib/modules/wdpa/data_standard/source.rb
+++ lib/modules/wdpa/data_standard/source.rb
@@ -1,16 +1,17 @@
 class Wdpa::DataStandard::Source < Wdpa::DataStandard
   STANDARD_ATTRIBUTES = {
-    :metadataid => {name: :metadataid, type: :integer},
-    :data_title => {name: :title, type: :string},
-    :resp_party => {name: :responsible_party, type: :string},
-    :resp_email => {name: :responsible_email, type: :string},
-    :year       => {name: :year, type: :year},
-    :char_set   => {name: :character_set, type: :string},
-    :ref_system => {name: :reference_system, type: :string},
-    :scale      => {name: :scale, type: :string},
-    :lineage    => {name: :lineage, type: :string},
-    :citation   => {name: :citation, type: :string},
-    :disclaimer => {name: :disclaimer, type: :string},
-    :language   => {name: :language, type: :string}
+    :metadataid  => {name: :metadataid, type: :integer},
+    :data_title  => {name: :title, type: :string},
+    :resp_party  => {name: :responsible_party, type: :string},
+    :resp_email  => {name: :responsible_email, type: :string},
+    :year        => {name: :year, type: :year},
+    :update_year => {name: :update_yr, type: :year},
+    :char_set    => {name: :character_set, type: :string},
+    :ref_system  => {name: :reference_system, type: :string},
+    :scale       => {name: :scale, type: :string},
+    :lineage     => {name: :lineage, type: :string},
+    :citation    => {name: :citation, type: :string},
+    :disclaimer  => {name: :disclaimer, type: :string},
+    :language    => {name: :language, type: :string}
   }
 end
