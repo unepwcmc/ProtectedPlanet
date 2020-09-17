0fbdb7bb06a094aedff6b5a9838dc51a41726a31
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2020-09-17 10:51:15 +0100

    Refactor conditional

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,14 +1,12 @@
 class AssetsController < ApplicationController
+  TYPES = %w(protected_area country region).freeze
+
   def tiles
-    if params[:type] == "protected_area"
-      image = AssetGenerator.protected_area_tile(protected_area)
-    elsif params[:type] == "country"
-      image = AssetGenerator.country_tile(country)
-    elsif params[:type] == "region"
-      image = AssetGenerator.region_tile(region)
-    else
-      raise_404
-    end
+    area_type = params[:type]
+    raise_404 unless TYPES.include?(area_type)
+
+    method_name = "#{area_type}_tile"
+    image = AssetGenerator.send(method_name, send(area_type))
 
     send_data image, type: 'image/png', disposition: 'inline'
   rescue AssetGenerator::AssetGenerationFailedError
