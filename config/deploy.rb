7fef9c70f38d6813aa14548b03e9d403a9e604c2
Stanley Liu <sliu1993@hotmail.co.uk>
2020-09-24 16:31:35 +0100

    reverted back to original staging names after merging refresh-copy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.11.0'
 
-set :application, 'ProtectedPlanet-copy'
+set :application, 'ProtectedPlanet'
 set :repo_url, 'git@github.com:unepwcmc/ProtectedPlanet.git'
 
 set :deploy_user, 'wcmc'
