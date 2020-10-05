bad7f20f3eab6fd9530de5b0b7bccf5b8456593c
Stanley Liu <sliu1993@hotmail.co.uk>
2020-10-05 10:25:36 +0100

    remove alternate locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,8 +36,7 @@ class ApplicationController < ActionController::Base
       'image:alt': t('meta.image_alt'),
       'image:height': t('meta.image_height'),
       'image:width': t('meta.image_width'),
-      'locale': 'en_GB',
-      'locale:alternate': "#{I18n.locale}_#{I18n.locale.upcase}"
+      'locale': 'en_GB'
     }
   end
 
