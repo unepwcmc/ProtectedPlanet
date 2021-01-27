0079ce8c285dce292abe24b54f1f71dd1fca900d
Stanley Liu <sliu1993@hotmail.co.uk>
2021-01-27 15:07:50 +0000

    revert: revert cap lock in deploy.rb to 3.11.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.14.1'
+lock '3.11.0'
 
 set :repo_url, 'git@github.com:unepwcmc/ProtectedPlanet.git'
 set :application, "ProtectedPlanet"
