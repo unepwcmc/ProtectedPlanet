9ac2797546e2c6edbad65aa463f785cfab9c29af
Joseph Burke <joseph_burke@outlook.com>
2021-04-28 16:13:56 +0100

    Change file_string to path_string

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -105,7 +105,7 @@ class CountryController < ApplicationController
   private 
 
   def flag_path country_name
-    file_string = country_name.downcase.gsub(' ', '-').gsub(',', '')
-    ActionController::Base.helpers.image_url("flags/#{file_string}.svg")
+    path_string = country_name.downcase.gsub(' ', '-').gsub(',', '')
+    ActionController::Base.helpers.image_url("flags/#{path_string}.svg")
   end
 end
