e7ce259d53c3e7bbbafb547a49f6e2a451efbb86
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2021-09-01 11:33:18 +0100

    Update commercial language for downloads

--- app/javascript/components/download/DownloadCommercial.vue
+++ app/javascript/components/download/DownloadCommercial.vue
@@ -13,23 +13,22 @@
       <h4 class="modal__title">{{ text.title }}</h4>
 
       <div class="modal__links">
-        <a 
-          class="modal__link-commercial"
-          href="https://ibat-alliance.org/"
-          target="_blank"
-        >
+        <div class="modal__link">
           <span class="modal__link-title">{{ text.commercialTitle }}</span>
-          <span>{{ text.commercialText }}</span>
-        </a>
+          <span class="modal__link-text" v-html="text.commercialText" />
+        </div>
 
         <span class="modal__divider" />
 
-        <div
-        class="modal__link-non-commercial"
-          @click="click"
-        >
+        <div class="modal__link">
           <span class="modal__link-title">{{ text.nonCommercialTitle }}</span>
-          <span>{{ text.nonCommercialText }}</span>
+          <span class="modal__link-text">{{ text.nonCommercialText }}</span>
+          <button
+            class="modal__link-button"
+            @click="click"
+          >
+            {{ text.nonCommercialButton }}
+          </button>
         </div>
       </div>
     </div>
@@ -46,7 +45,7 @@ export default {
     },
     text: {
       required: true,
-      type: Object //{ commercialText: String, commercialTitle: String, nonCommercialText: String, nonCommercialTitle: String, title: String }
+      type: Object //{ commercialText: String, commercialTitle: String, nonCommercialText: String, nonCommercialTitle: String, nonCommercialButton: String, title: String }
     },
   },
 
