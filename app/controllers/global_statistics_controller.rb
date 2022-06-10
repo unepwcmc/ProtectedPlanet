d5b99ca3fc60c940ed37ee42a85452fb514ed1c3
sergio <s.marrocoli@gmail.com>
2022-06-10 12:59:29 +0100

    feat: Moved latest file method to GlobalStatistic class

--- app/controllers/global_statistics_controller.rb
+++ app/controllers/global_statistics_controller.rb
@@ -1,14 +1,5 @@
 class GlobalStatisticsController < ApplicationController
   def show
-    send_file filename, type: 'text/csv', disposition: 'attachment'
-  end
-
-  private
-
-  def filename
-    directory = "#{Rails.root}/lib/data/seeds/"
-    global_statistics_csvs = Dir.glob("#{directory}/global_statistics*")
-    latest_global_statistics_file = global_statistics_csvs.sort.last
-    latest_global_statistics_file
+    send_file GlobalStatistic.latest_csv, type: 'text/csv', disposition: 'attachment'
   end
 end
