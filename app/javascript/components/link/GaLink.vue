478e0c896c957d04fd579825668fd7ae47220d44
Will Kocur <willkocur@gmail.com>
2022-10-12 15:26:34 +0100

    feat: track global stats download

---
+++ app/javascript/components/link/GaLink.vue
@@ -0,0 +1,34 @@
+<template>
+  <a 
+    :href="href" 
+    @click="click"
+  >
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    gaId: {
+      type: String,
+      default: ''
+    },
+
+    href: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    click () {
+      if(this.gaId) {
+        const eventLabel = `Link - ${this.gaId}`
+
+        this.$ga.event('Button', 'click', eventLabel)
+      }
+    }
+  }
+}
+</script>
