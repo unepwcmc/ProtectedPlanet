a318f499c1d563abe406c6a1193c8763b3432603
maxime <maxycle@hotmail.com>
2022-10-27 17:28:00 +0100

    feat: add 4 fields to CountryStatistic

--- lib/modules/wdpa/geometry_ratio_calculator.rb
+++ lib/modules/wdpa/geometry_ratio_calculator.rb
@@ -1,12 +1,10 @@
-module Wdpa::GeometryRatioCalculator
+ module Wdpa::GeometryRatioCalculator
   def self.calculate
     Country.pluck(:id).each do |country_id|
       geometry_counts = calculate_geometry_counts(country_id)
 
       statistics = CountryStatistic.find_or_initialize_by(country_id: country_id)
-      statistics.polygons_count = geometry_counts["polygons"]
-      statistics.points_count = geometry_counts["points"]
-
+      statistics.assign_attributes(geometry_counts)
       statistics.save
     end
   end
@@ -14,13 +12,17 @@ module Wdpa::GeometryRatioCalculator
   def self.calculate_geometry_counts country_id
     ActiveRecord::Base.connection.select_all("""
       SELECT
-        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' THEN 1 ELSE 0 END)) AS points,
-        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' THEN 1 ELSE 0 END)) AS polygons
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' THEN 1 ELSE 0 END)) AS points_count,
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' THEN 1 ELSE 0 END)) AS polygons_count,
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' AND pas.is_oecm = true THEN 1 ELSE 0 END)) AS oecm_polygons,
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' AND pas.is_oecm = true THEN 1 ELSE 0 END)) AS oecm_points,
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' AND pas.is_oecm = false THEN 1 ELSE 0 END)) AS protected_area_polygons,
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' AND pas.is_oecm = false THEN 1 ELSE 0 END)) AS protected_area_points
       FROM protected_areas pas
       INNER JOIN
         countries_protected_areas cpas
         ON cpas.protected_area_id = pas.id
         AND cpas.country_id = #{country_id}
-    """).first
+      """).first
   end
 end
