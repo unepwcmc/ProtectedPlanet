90ba6d786f0f81cbfe5e576904aca6233b918ef1
sergio <s.marrocoli@gmail.com>
2022-12-20 17:45:13 +0000

    feat: remove buffer query which might be too slow? possible fix for the SQL database connection issue on production

--- app/models/concerns/geometry_concern.rb
+++ app/models/concerns/geometry_concern.rb
@@ -25,13 +25,9 @@ module GeometryConcern
   def geojson_for_mapbox_uri geo_properties=nil
     # Returns a stringified geojson for making a request to the Mapbox API, to generate a preview thumbnail.
     # If the string is too long it will be rejected, so we then need to simplify it.
-    # First we can buffer it, which reduces complexity but retains the original shape well.
-    # Finally, if the string is still too long, we fall back on a convex transformation, which
-    # provides a very crude outline, but is a short string.
+    # We fall back on a convex transformation, which provides a very crude outline, but is a short string.
     if geojson_suitable_for_mapbox_url?((geojson = geojson_query(geo_properties)))
       geojson
-    elsif geojson_suitable_for_mapbox_url?((buffered_geojson = buffered_geojson_query(geo_properties)))
-      buffered_geojson
     else
       convex_geojson_query(geo_properties)
     end
@@ -46,20 +42,6 @@ module GeometryConcern
   end
 
   private
-
-  def buffered_geojson_query geo_properties
-    # We send a geojson to mapbox to create thumbnails. If the length of these exceeds a uri
-    # length of ~5000 characters, mapbox returns a 403. This method simplifies the geojson by
-    # first simplifying the geometry (to make the buffer faster), adding a buffer, and then
-    # simplifying again.
-    simplified_geojson = ActiveRecord::Base.connection.select_value("""
-    SELECT ST_AsGeoJSON(ST_SimplifyPreserveTopology(ST_Buffer(ST_SimplifyPreserveTopology(ST_MakeValid(#{main_geom_column}), 0.005), 0.005), 0.005), 3)
-      FROM #{self.class.table_name}
-      WHERE id = #{id}
-    """.squish)
-
-    simplified_geojson.present? ? to_uri(simplified_geojson, geo_properties) : nil
-  end
 
   def convex_geojson_query geo_properties
     # Very simple polygon, if other polygon thumbnail methods fail.
