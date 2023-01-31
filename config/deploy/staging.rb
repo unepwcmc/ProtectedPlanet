0cab47e9a3021ac170ff2e2a57da3c32adacb85b
maxime <maxycle@hotmail.com>
2023-01-31 11:39:42 +0000

    fix: set staging branch back to develop

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, :staging
-set :branch, "fix/297/disclaimer-update"
+set :branch, "develop"
 
 server 'new-web.pp-staging.linode.protectedplanet.net', user: 'wcmc', roles: %w{web app db}
 
