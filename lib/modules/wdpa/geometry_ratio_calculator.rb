72ebfb0d5bb572c827c7384f31165a12b8fa4fea
sergiomarrocoli <s.marrocoli@gmail.com>
2023-03-07 15:25:02 +0000

    feat: update new country_statistic column names to show they are counts

--- lib/modules/wdpa/geometry_ratio_calculator.rb
+++ lib/modules/wdpa/geometry_ratio_calculator.rb
@@ -1,4 +1,7 @@
 module Wdpa::GeometryRatioCalculator
+  # This class does not appear to calculate ratios, rather it counts the number of points and polygons for each country.
+  # It is run during the WDPA import process and updates the fields according to the new WPDA data.
+  # It has been modified to also count the number of PA and OECM points and polygons for each country.
   def self.calculate
     Country.pluck(:id).each do |country_id|
       geometry_counts = calculate_geometry_counts(country_id)
@@ -14,10 +17,10 @@ module Wdpa::GeometryRatioCalculator
       SELECT
         SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' THEN 1 ELSE 0 END)) AS points_count,
         SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' THEN 1 ELSE 0 END)) AS polygons_count,
-        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' AND pas.is_oecm = true THEN 1 ELSE 0 END)) AS oecm_polygons,
-        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' AND pas.is_oecm = true THEN 1 ELSE 0 END)) AS oecm_points,
-        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' AND pas.is_oecm = false THEN 1 ELSE 0 END)) AS protected_area_polygons,
-        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' AND pas.is_oecm = false THEN 1 ELSE 0 END)) AS protected_area_points
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' AND pas.is_oecm = true THEN 1 ELSE 0 END)) AS oecm_polygon_count,
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' AND pas.is_oecm = true THEN 1 ELSE 0 END)) AS oecm_point_count,
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOLYGON' AND pas.is_oecm = false THEN 1 ELSE 0 END)) AS protected_area_polygon_count,
+        SUM((CASE WHEN GeometryType(the_geom) = 'MULTIPOINT' AND pas.is_oecm = false THEN 1 ELSE 0 END)) AS protected_area_point_count
       FROM protected_areas pas
       INNER JOIN
         countries_protected_areas cpas
