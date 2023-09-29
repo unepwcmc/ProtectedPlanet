37ed48839bf27d946b8739cb9f80a6a4a5909f3b
warren-unep-wcmc <134701195+warren-unep-wcmc@users.noreply.github.com>
2023-09-29 10:41:45 +0100

    Delete data-management-console/sql/common_reference/international_criteria.sql
    Project rearrangement means this is no longer here

--- data-management-console/sql/common_reference/international_criteria.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM stg_international_criteria
-DROP SEQUENCE IF EXISTS stg_int_crit_seq
-CREATE SEQUENCE stg_int_crit_seq AS INT START WITH 1
-
-CREATE OR REPLACE FUNCTION stg_international_criteria_id() RETURNS TRIGGER LANGUAGE PLPGSQL AS $$ BEGIN IF NEW.id is NULL THEN NEW.id = nextval('stg_int_crit_seq'); END IF; RETURN NEW; END; $$
-DROP TRIGGER IF EXISTS trig_stg_international_criteria_id ON stg_international_criteria
-CREATE TRIGGER trig_stg_international_criteria_id BEFORE INSERT ON stg_international_criteria FOR EACH ROW EXECUTE procedure stg_international_criteria_id();
-
-INSERT INTO stg_INTERNATIONAL_CRITERIA(DESCRIPTION, ORIGINATOR_ID) SELECT DISTINCT int_crit, 10000 FROM WDPADATA_POLY_MAY2023 UNION SELECT DISTINCT int_crit, 10000 FROM WDPADATA_POLY_MAY2023 UNION SELECT DISTINCT int_crit, 10000 FROM WDPADATA_POLY_MAY2023
