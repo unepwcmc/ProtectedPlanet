738b56049038710254dd0d38abfe0e139481ad64
warren-unep-wcmc <134701195+warren-unep-wcmc@users.noreply.github.com>
2023-10-03 15:36:23 +0100

    Support serialization of dates and datetimes (e.g. ICCA submission date)

--- data-management-console/website/jsonexporter.py
+++ data-management-console/website/jsonexporter.py
@@ -1,14 +1,21 @@
 # Small helper class for API endpoints
 import json
 import time
+from datetime import datetime, date
 
 
 class JsonExporter:
 
     @staticmethod
+    def json_serialize(arg_to_encode):
+        if isinstance(arg_to_encode, (datetime, date)):
+            return arg_to_encode.isoformat()
+        raise TypeError(f"Type {type(arg_to_encode)} is not serializable")
+
+    @staticmethod
     def export(chain_of_objects: dict) -> str:
         start_time = time.time()
-        output_json = json.dumps(chain_of_objects)
+        output_json = json.dumps(chain_of_objects, default=JsonExporter.json_serialize)
         print(f"Length of output JSON is {len(output_json)}")
         # pprint.pprint(chain_of_objects)
         duration = time.time() - start_time
