81dcba97e8e4249a468d465f1593d9f6b818ad40
Yueeeeee <yue-long.he@unep-wcmc.org>
2025-02-03 10:39:01 +0800

    chore: remove devise settings

--- test/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    sequence(:email) { |n| "user#{n}@example.com" }
-    password 'whatapassword123'
-    password_confirmation 'whatapassword123'
-  end
-end
