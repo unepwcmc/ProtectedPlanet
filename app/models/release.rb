3803a2b2689fd12b856cf5cd17d437c440f0002e
Yueeeeee <yue-long.he@unep-wcmc.org>
2025-11-21 10:01:31 +0000

    prevent running some release related. functions during migration to cause errors

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 class Release < ApplicationRecord
+  # Prevent ActiveRecord from trying to introspect the table if it doesn't exist
+  # This handles the case where migrations haven't been run yet
+  def self.table_exists?
+    @_table_exists ||= begin
+      connection.table_exists?(table_name)
+    rescue ActiveRecord::StatementInvalid, PG::UndefinedTable
+      false
+    end
+  end
+
+  # Override columns to return empty array if table doesn't exist
+  # def self.columns
+  #   return [] unless table_exists?
+  #   super
+  # rescue ActiveRecord::StatementInvalid, PG::UndefinedTable
+  #   []
+  # end
+
   has_many :release_events, dependent: :delete_all
 
   STATES = %w[started preflight_ok importing validating swapped rolled_back aborted failed succeeded].freeze
@@ -11,8 +29,12 @@ class Release < ApplicationRecord
   validates :is_current, uniqueness: true, if: :is_current?
 
   # Returns the current active release
+  # Safe to call even if table doesn't exist yet
   def self.current_release
+    return nil unless table_exists?
     find_by(is_current: true)
+  rescue ActiveRecord::StatementInvalid, PG::UndefinedTable
+    nil
   end
 
   # Returns the label of the current active release
