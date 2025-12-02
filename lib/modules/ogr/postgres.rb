c6fced12574547a38a1a32a2ce26708cca085858
Yueeeeee <yue-long.he@unep-wcmc.org>
2025-12-02 16:36:14 +0000

    Revert "fix: use alternative way to send sql query"
    This reverts commit 99e332e71313c609cd7301f62a4549e84c3c3b44.

--- lib/modules/ogr/postgres.rb
+++ lib/modules/ogr/postgres.rb
@@ -28,8 +28,11 @@ class Ogr::Postgres
     # The name of the feature, e.g. WDPA_poly_MmmYYYY, WDPA_point_MmmYYYY
     feature_name = get_feature_name(file_name, geom_type)
     command = ogr_command(template, binding)
-    # Rails.logger.info "[OGR::Postgres.export] file_type=#{file_type}, geom_type=#{geom_type}, command=#{command}"
-
+    Rails.logger.info "[OGR::Postgres.export] file_type=#{file_type}, geom_type=#{geom_type}, command=#{command}"
+    
+    # Write command to tmp file for debugging on staging
+    log_command_to_file(command, file_type, geom_type)
+    
     # Execute command and check for errors
     # system returns: true (success), false (failed), nil (couldn't execute)
     system(command)
@@ -44,6 +47,22 @@ class Ogr::Postgres
   def self.ogr_command template, context
     compiled_template = ERB.new(template).result context
     compiled_template.squish
+  end
+
+  def self.log_command_to_file(command, file_type, geom_type)
+    log_file = File.join(Rails.root, 'tmp', 'ogr_command_file.txt')
+    timestamp = Time.current.iso8601
+    log_entry = <<~LOG
+      [#{timestamp}] file_type=#{file_type}, geom_type=#{geom_type}
+      #{command}
+      ---
+    LOG
+    
+    File.open(log_file, 'a') do |f|
+      f.write(log_entry)
+    end
+  rescue StandardError => e
+    Rails.logger.warn "Failed to write OGR command to log file: #{e.message}"
   end
 
   # The filename convention should be as follows:
