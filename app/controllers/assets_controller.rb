06c62a9b23197abcc31bf74af903cde690e89e72
Yueeeeee <yue-long.he@unep-wcmc.org>
2025-12-18 09:29:41 +0000

    feat: cache tiles

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -5,7 +5,27 @@ class AssetsController < ApplicationController
     area_type = params[:type]
     raise_404 unless TYPES.include?(area_type)
     method_name = "#{area_type}_tile"
-    image = AssetGenerator.send(method_name, send(area_type))
+    record = send(area_type)
+    raise_404 if record.nil?
+
+    cache_key = [
+      'tiles',
+      'image',
+      area_type,
+      params[:id].to_s,
+      (record.respond_to?(:updated_at) && record.updated_at ? record.updated_at.to_i : 'na')
+    ].join(':')
+
+    image = Rails.cache.fetch(cache_key, expires_in: 20.days) do
+      AssetGenerator.send(method_name, record)
+    end
+
+    if image.blank?
+      redirect_to ActionController::Base.helpers.asset_path('search-placeholder-country.png', type: :image)
+      return
+    end
+
+    expires_in 5.days, public: true
 
     send_data image, type: 'image/png', disposition: 'inline'
   rescue AssetGenerator::AssetGenerationFailedError
