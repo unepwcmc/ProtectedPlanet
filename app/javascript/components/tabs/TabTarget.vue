b1669970ce84b2ffd0ac91115d9035e6ad8ba11d
Yueeeeee <yue-long.he@unep-wcmc.org>
2026-07-01 08:52:25 +0100

    fix: resize map when clicking on different tabs

--- app/javascript/components/tabs/TabTarget.vue
+++ app/javascript/components/tabs/TabTarget.vue
@@ -19,6 +19,29 @@ export default {
     isActive () {
       return this.id == this.$attrs['selected-id'] //need to do this way for ie11
     }
+  },
+
+  watch: {
+    isActive(isActive) {
+      if (isActive) {
+        this.resizeMapsInTab()
+      }
+    }
+  },
+
+  mounted() {
+    if (this.isActive) {
+      this.resizeMapsInTab()
+    }
+  },
+
+  methods: {
+    resizeMapsInTab() {
+      // Mapbox initialises at 0×0 inside display:none tabs; resize once visible.
+      this.$nextTick(() => {
+        this.$eventHub.$emit('map:resize')
+      })
+    }
   }
 }
 </script>
