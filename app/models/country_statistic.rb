294218c6076ad480c9b2f789beb7a488fcc4372b
Yueeeeee <yue-long.he@unep-wcmc.org>
2026-07-01 08:55:42 +0100

    chore: remove dead code for aichi

--- app/models/country_statistic.rb
+++ app/models/country_statistic.rb
@@ -1,8 +1,6 @@
 class CountryStatistic < ApplicationRecord
   belongs_to :country
 
-  scope :top_marine_coverage, -> { order('percentage_pa_marine_cover DESC NULLS LAST').limit(6) }
-
   def total_marine_area
     marine_area + overseas_total_marine_area
   end
@@ -29,23 +27,5 @@ class CountryStatistic < ApplicationRecord
 
   def overseas_percentage
     (overseas_total_protected_marine_area / overseas_total_marine_area) * 100
-  end
-
-  [:pa, :oecms_pa].each do |pa_type|
-    [:land, :marine].each do |type|
-      field_name = "#{type}_area"
-      define_singleton_method("global_#{pa_type}_#{field_name}") do
-        attr = "#{pa_type}_#{field_name}"
-        Stats::Global.calculate_stats_for(self, attr)
-      end
-
-      define_singleton_method("global_#{field_name}") do
-        Stats::Global.calculate_stats_for(self, field_name)
-      end
-
-      define_singleton_method("global_percentage_#{pa_type}_#{type}_cover") do
-        (public_send("global_#{pa_type}_#{field_name}") / public_send("global_#{field_name}") * 100).round(2)
-      end
-    end
   end
 end
