2a8bc72dd7bd3a6645404e07cf6e777fc5b87300
Daniyal Akhlaq <daniyalakhlaq1@gmail.com>
2026-07-27 10:22:48 +0100

    feat: config.load_defaults 6.1

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module ProtectedPlanet
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Opted out of one 6.0 default. Every belongs_to foreign key in this schema is
     # nullable, so nothing at the database level backs a presence validation, and
