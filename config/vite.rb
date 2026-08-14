86e8274fc9d0bc313605602e6ac8fec57d789a46
Yueeeeee <yue-long.he@unep-wcmc.org>
2026-08-14 15:49:32 +0100

    feat: adjusted for frontend backend integration

--- config/vite.rb
+++ config/vite.rb
@@ -6,5 +6,5 @@ require_relative 'application'
 # them and Vite exposes them to client code via import.meta.env — see
 # https://vite-ruby.netlify.app/guide/plugins.html#environment
 # If you add any envs here, also add a type to app/frontend/composables/useEnvs.ts
-ViteRuby.env['VITE_MAPBOX_TOKEN'] = Rails.application.secrets.mapbox[:access_token]
+ViteRuby.env['VITE_MAPBOX_TOKEN'] = Rails.application.config_for(:app_secrets).mapbox[:access_token]
 ViteRuby.env['VITE_RAILS_ENV'] = Rails.env
